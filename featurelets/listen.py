2a0a35c5ea9810d62b77289d8b438e219771dc6d
Rob Miller <rob@kalistra.com>
2006-06-27 22:36:18 +0000

    * removing the unused config view from the listen featurelet
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/ra_listen_flet@797 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- featurelets/listen.py
+++ featurelets/listen.py
@@ -22,7 +22,7 @@ class ListenFeaturelet(BaseFeaturelet):
 
     id = "listen"
     title = "Mailing Lists"
-    config_view = "listen_config"
+    #config_view = "listen_config"
     installed_marker = IListenFeatureletInstalled
     
     _info = {'content': ({'id': 'lists', 'title': 'Mailing Lists',
