0aa7add5c91e249577467ce1fee8a2c9a8754a8a
Rob Miller <rob@kalistra.com>
2006-06-27 02:30:41 +0000

    * add view to support delete list confirmation
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/ra_listen_flet@793 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- featurelets/browser/listen.py
+++ featurelets/browser/listen.py
@@ -2,9 +2,13 @@ from Products.Five import BrowserView
 
 from Products.listen.content.mailinglist import MailingList
 
+from zope.i18nmessageid import MessageFactory
+
 from topp.featurelets.interfaces import IFeatureletSupporter
 
 from opencore.featurelets.listen import ListenFeaturelet
+
+_ = MessageFactory("opencore")
 
 class ListenConfigView(BrowserView):
     """
@@ -26,3 +30,27 @@ class ListenConfigView(BrowserView):
         container = self.context._getOb(descriptor['content'][0]['id'])
         lists = container.objectValues(spec=MailingList.meta_type)
         return lists
+
+class DeleteListView(BrowserView):
+    """
+    View object that handles the list delete confirmation logic.
+    """
+    def __init__(self, context, request):
+        self._context = (context,)
+        self.request = request
+
+    @property
+    def context(self):
+        return self._context[0]
+
+    def confirmDeleteList(self):
+        form = self.request.form
+        psm = _("Mailing list deletion cancelled.")
+        list_id = form.get('list_id', '')
+        confirm = form.get('%s_confirm_delete' % list_id, 'false')
+        if confirm == 'true':
+            self.context.manage_delObjects(ids=[list_id])
+            psm = _("Mailing list deleted.")
+        url = "%s?portal_status_message=%s" % (self.context.absolute_url(),
+                                               psm)
+        self.request.RESPONSE.redirect(url)
