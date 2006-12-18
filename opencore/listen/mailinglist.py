8b9d59b115fdef11f0e4a6b724c5352cfff8c37e
Rob Marianski <rob@marianski.com>
2006-12-18 22:03:55 +0000

    updated listen install since listen now removed the factory type information structure and went to generic setup
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/setuptools-listen-new-install@2134 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/mailinglist.py
+++ opencore/listen/mailinglist.py
@@ -1,15 +1,79 @@
 from Products.listen.content.mailinglist import MailingList
-from Products.listen.content.mailinglist import factory_type_information \
-     as base_fti
 from Products.listen.permissions import InviteSubscribers
 from Products.OpenPlans.config import PROJECTNAME
+
+from Products.CMFCore import permissions as CMFPermissions
+from Products.listen import permissions
 
 from zope.interface import implements
 
 from interfaces import IOpenMailingList
 from fieldproperty import ListNameFieldProperty
 
-fti = base_fti[0].copy()
+PKG_NAME = 'listen'
+
+factory_type_information = ( {
+    'id'             : 'Mailing List',
+    'icon'           : 'mailboxer_icon.png',
+    'meta_type'      : 'MailingList',
+    'description'    : "A mailing list manages user subscriptions and "\
+                       "processes incoming mail",
+    'product'        : PKG_NAME,
+    'factory'        : 'addMailingList',
+    'immediate_view' : 'edit',
+    'aliases'        : {'(Default)'     :'mailinglist_view',
+                        'index.html'    :'mailinglist_view',
+                        'view'          :'mailinglist_view',
+                        'sharing'       :'folder_localrole_form',
+                        'subscribers'   :'@@editAllSubscribers',
+                        'edit'          :'@@edit'},
+    'actions'        : (
+                        {
+                        'id': 'view',
+                        'name': 'View',
+                        'action': 'string:${object_url}',
+                        'permissions': (CMFPermissions.View,)
+                        },
+                        {
+                        'id': 'edit',
+                        'name': 'Edit',
+                        'action': 'string:${object_url}/edit',
+                        'permissions': (permissions.EditMailingList,)
+                        },
+                        {
+                        'id': 'archive',
+                        'name': 'archive',
+                        'action': 'string:${object_url}/archive',
+                        'permissions': (CMFPermissions.View,)
+                        },
+# TODO: Moderation and bounced/disabled address management TTW
+#                         {
+#                         'id': 'moderate',
+#                         'name': 'Moderate',
+#                         'action': 'string:${object_url}/mqueue',
+#                         'permissions': (permissions.ManageSubscriptions,)
+#                         },
+#                         {
+#                         'id': 'managelist',
+#                         'name': 'Manage List',
+#                         'action': 'string:${object_url}/manage-list',
+#                         'permissions': (permissions.ManageSubscriptions,)
+#                         },
+                        {
+                        'id': 'subscribers',
+                        'name': 'Subscribers',
+                        'action': 'string:${object_url}/subscribers',
+                        'permissions': (permissions.ManageSubscriptions,)
+                        },
+                       ),
+    'filter_content_types' : True,
+    'allowed_content_types' : (),
+    'global_allow' : True,
+    'allow_discussion' : False,
+  },
+)
+
+fti = factory_type_information[0].copy()
 fti['id'] = 'Open Mailing List'
 fti['meta_type'] = "OpenMailingList"
 fti['product'] = PROJECTNAME
