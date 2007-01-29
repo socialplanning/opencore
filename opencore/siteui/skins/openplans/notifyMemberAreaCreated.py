36c29163d819f2cd3ac8917acad5c70238faa3a6
Whit Morriss <d.w.morriss@gmail.com>
2007-01-29 19:58:01 +0000

    adding skins from Products.OpenPlans
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@2624 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/siteui/skins/openplans/notifyMemberAreaCreated.py
@@ -0,0 +1,27 @@
+## Script (Python) "notifyMemberAreaCreated"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=Create the member home page
+
+from Products.CMFCore.utils import getToolByName
+mtool = getToolByName(context, 'portal_membership')
+#member_id = mtool.getAuthenticatedMember().getId()
+#folder = mtool.getHomeFolder(member_id)
+#page_id = "%s-home" % member_id
+#title = "%s Home" % member_id
+#folder.invokeFactory('Document', page_id, title=title)
+#folder.setDefaultPage(page_id)
+#
+#page = getattr(folder, page_id)
+#page_text = context.member_index(member_id=member_id)
+#page.setText(page_text)
+
+
+member = mtool.getAuthenticatedMember()
+
+from Products.OpenPlans.browser.profile import fireFirstLoginEvent
+fireFirstLoginEvent(member)
