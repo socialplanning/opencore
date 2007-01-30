74dd09d164e874cd868f6c6d1c43a68ee8857e02
Whit Morriss <d.w.morriss@gmail.com>
2007-01-30 04:11:49 +0000

    remove unnecessary file
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@2665 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/siteui/FirstLoginEvent.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.utils import getToolByName 
-from zope.interface import implements
-
-class FirstLoginEvent():
-    implements(IFirstLoginEvent)
-    def __init__(self, member):
-        self.member = member
