d01cd45e28a337569cbeab09de9c242f387abddb
Rob Miller <rob@kalistra.com>
2007-03-01 21:46:58 +0000

    * use formlib-based add view
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/setuptools-listen-new-install@3294 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/listen/mailinglist_views.py
@@ -0,0 +1,20 @@
+from zope.formlib import form
+from zope.app.form.browser import ASCIIWidget
+
+from Products.listen.browser.mailinglist_views import DescriptionWidget
+from Products.listen.browser.mailinglist_views import MailingListAddForm
+
+from interfaces import IOpenMailingList
+from widgets import OpenListNameWidget
+
+class MailingListAddForm(MailingListAddForm):
+    """
+    A form for adding OpenMailingList objects.
+    """
+    form_fields = form.FormFields(IOpenMailingList)
+    form_fields['description'].custom_widget = DescriptionWidget
+    form_fields['mailto'].custom_widget = OpenListNameWidget
+    form_fields['admin_email'].custom_widget = ASCIIWidget
+
+    portal_type = 'Open Mailing List'
+        
