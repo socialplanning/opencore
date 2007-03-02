a98dd8e32605270a98688f5f74e52ad34b676162
Rob Miller <rob@kalistra.com>
2007-03-02 18:24:09 +0000

    * remove unused browser:editform tag * clarify formlib form inheritance structure
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/setuptools-listen-new-install@3309 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/mailinglist_views.py
+++ opencore/listen/mailinglist_views.py
@@ -2,12 +2,16 @@ from zope.formlib import form
 from zope.app.form.browser import ASCIIWidget
 
 from Products.listen.browser.mailinglist_views import DescriptionWidget
-from Products.listen.browser.mailinglist_views import MailingListAddForm
+from Products.listen.browser.mailinglist_views import MailingListAddForm \
+     as BaseAddForm
+from Products.listen.browser.mailinglist_views import MailingListEditForm \
+     as BaseEditForm
 
 from interfaces import IOpenMailingList
 from widgets import OpenListNameWidget
 
-class MailingListAddForm(MailingListAddForm):
+
+class MailingListAddForm(BaseAddForm):
     """
     A form for adding OpenMailingList objects.
     """
@@ -16,4 +20,12 @@ class MailingListAddForm(MailingListAddForm):
     form_fields['mailto'].custom_widget = OpenListNameWidget
 
     portal_type = 'Open Mailing List'
-        
+
+
+class MailingListEditForm(BaseEditForm):
+    """A form for editing MailingList objects.
+
+    """
+    form_fields = form.FormFields(IOpenMailingList)
+    form_fields['description'].custom_widget = DescriptionWidget
+    form_fields['mailto'].custom_widget = OpenListNameWidget
