1234ac9edc23d54465eca6d820df3b10fbd09350
Chris Abraham <cabraham@openplans.org>
2007-03-08 17:13:27 +0000

    some ui tweaks to the add/edit forms
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/setuptools-listen-new-install@3426 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/mailinglist_views.py
+++ opencore/listen/mailinglist_views.py
@@ -10,6 +10,10 @@ from Products.listen.browser.mailinglist_views import MailingListEditForm \
 from interfaces import IOpenMailingList
 from widgets import OpenListNameWidget
 
+from Products.listen.browser.mailinglist_views import ArchiveListWidget
+from Products.listen.browser.mailinglist_views import ListTypeListWidget
+
+
 
 class MailingListAddForm(BaseAddForm):
     """
@@ -18,7 +22,8 @@ class MailingListAddForm(BaseAddForm):
     form_fields = form.FormFields(IOpenMailingList)
     form_fields['description'].custom_widget = DescriptionWidget
     form_fields['mailto'].custom_widget = OpenListNameWidget
-
+    form_fields['archived'].custom_widget = ArchiveListWidget
+    form_fields['list_type'].custom_widget = ListTypeListWidget
     portal_type = 'Open Mailing List'
 
 
@@ -29,3 +34,5 @@ class MailingListEditForm(BaseEditForm):
     form_fields = form.FormFields(IOpenMailingList)
     form_fields['description'].custom_widget = DescriptionWidget
     form_fields['mailto'].custom_widget = OpenListNameWidget
+    form_fields['archived'].custom_widget = ArchiveListWidget
+    form_fields['list_type'].custom_widget = ListTypeListWidget
