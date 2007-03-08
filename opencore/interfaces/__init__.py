f2903e3949cc77d218185224f8ab16224de62828
Whit Morriss <d.w.morriss@gmail.com>
2007-03-08 16:48:59 +0000

    add more interface imports
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@3422 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/interfaces/__init__.py
+++ opencore/interfaces/__init__.py
@@ -1,6 +1,9 @@
 #
 try:
     from Products.OpenPlans.interfaces import IProject
+    from Products.OpenPlans.interfaces import IAddProject, IEditProject
+    from Products.OpenPlans.interfaces import IWriteWorkflowPolicySupport
+    from Products.OpenPlans.interfaces import IReadWorkflowPolicySupport
 except ImportError:
     from zope.interface import Interface
     class IProject(Interface):
