4dbaefdfad27f003e12ea30855fcef02d365b9d2
Whit Morriss <d.w.morriss@gmail.com>
2007-03-08 18:17:45 +0000

    add subproject creation view
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@3436 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- content/project.py
+++ content/project.py
@@ -2,11 +2,21 @@ import os.path
 import Products.OpenPlans.content.project as project
 from Products.Fate.meta.view import AddView as BaseAddView
 from opencore import redirect
+try:
+    from Products.OpenPlans.browser.projectlisting import ProjectListingView
+except ImportError:
+    from opencore.siteui.projectlisting import ProjectListingView
+from memojito import memoizedproperty
+from Products.CMFCore.utils import getToolByName
+from Products.Five.traversable import Traversable
+from zope.interface import implements
+from opencore.interfaces import IAddSubProject
 
 OpenProject=project.OpenProject
 HOME_PAGE_ID = 'project-home'
 HOME_PAGE_TITLE = 'Project Home'
 HOME_PAGE_FILE = 'project_index.html'
+
 
 class ProjectAddView(BaseAddView):
     """
@@ -42,3 +52,34 @@ class ProjectAddView(BaseAddView):
         instance = self.handle_environ(instance)
 
         return instance
+
+    
+class SubProjectAddView(ProjectAddView):
+    """add view for redirected areas
+    """
+    def handle_environ(self, instance):
+        theme_parent = self.request.environ.get(redirect.PARENT_KEY, False)
+        if theme_parent:
+            parent = self.unrestrictedTraverse(theme_parent)
+            redirect.handle_child_created(instance, parent)
+
+
+class SubProjectListingView(ProjectListingView, Traversable): 
+    """mandatory docstring
+    """
+    implements(IAddSubProject)
+
+    @memoizedproperty
+    def redirect_info(self): 
+        return redirect.get_info(self.context)
+
+    @property
+    def project_paths(self):
+        return list(self.redirect_info.values())
+
+    @memoizedproperty
+    def portal_catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+        
+    def allprojects(self): 
+        return self.portal_catalog(path=self.project_paths, sort_on='sortable_title')
