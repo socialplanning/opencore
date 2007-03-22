5aa9c3cc236e17dc3e5ab9c6540b367436b2855b
Rob Miller <rob@kalistra.com>
2007-03-22 23:54:13 +0000

    * fix things up to use IAdding correctly
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@3746 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- featurelets/interfaces.py
+++ featurelets/interfaces.py
@@ -1,12 +1,11 @@
 from zope.interface import Interface
-from zope.app.container.interfaces import IAdding
 
 class IListenFeatureletInstalled(Interface):
     """
     Marks an object as having the listen featurelet installed.
     """
 
-class IListenContainer(IAdding):
+class IListenContainer(Interface):
     """
     Marks an object as a mailing list container for the listen
     featurelet.
