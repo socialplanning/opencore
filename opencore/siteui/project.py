878ba864fb063e42ca3b1b063609c2da18e532ef
Whit Morriss <d.w.morriss@gmail.com>
2007-03-29 20:30:23 +0000

    remove commented debris, defaulting traverser, add post creation subscribers back here
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/whit-hook-refactor@3892 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/siteui/project.py
+++ opencore/siteui/project.py
@@ -4,7 +4,7 @@ import cgi
 from Acquisition import aq_parent
 
 from zope import event
-from zope.interface import implements
+from zope.interface import implements, Interface
 from zope.component import adapts
 
 from Products.Five.traversable import Traversable
@@ -19,6 +19,10 @@ from opencore import redirect
 from opencore.interfaces import IAddSubProject, IAddProject, IProject
 from opencore.interfaces.event import AfterProjectAddedEvent, \
      AfterSubProjectAddedEvent
+from zope.app.traversing.interfaces import ITraversable
+from Products.Five.traversable import FiveTraversable
+from opencore.interfaces.event import IAfterProjectAddedEvent, \
+     IAfterSubProjectAddedEvent
 
 HOME_PAGE_ID = 'project-home'
 HOME_PAGE_TITLE = 'Project Home'
@@ -96,13 +100,17 @@ class SubProjectAddView(ProjectAddView):
         return instance 
 
 # == other views == #
+class ISubProjectListingView(IAddSubProject):
+    """unique interface for this unique situation"""
+
 
 class SubProjectListingView(ProjectListingView, Traversable): 
     """mandatory docstring
     """
     implements(IAddSubProject)
 
-    def __init__(self, context, request): 
+    def __init__(self, context, request):
+        import pdb;pdb.set_trace()
         ProjectListingView.__init__(self, context, request)
         # XXX this may be a horrendous hack, but it appears 
         # to be what Five expects to find during traversal... ?!
@@ -126,19 +134,46 @@ class SubProjectListingView(ProjectListingView, Traversable):
                                    sort_on='sortable_title')
     @memoize
     def blocked_content(self):
-        return getattr(self.aq_parent, self.__name__, False)
+        name = self.__name__
+        parent = self.aq_parent
+        if name in parent.objectIds():
+            return getattr(parent, name)
+        return False
 
-    def __call__(self, *args, **kwargs):
-        content = self.blocked_content()
-        if content is not False:
-            return content()
-        return self.index(self, *args, **kw)
+# ==  post creation handlers == #
 
-# default redirection 
+@adapter(IAfterProjectAddedEvent)
+def handle_postcreation(event):
+    instance = event.project
 
-class DefaultProjectRedirectTraverser(redirect.DefaultingRedirectTraverser): 
-    """
-    a traverser that redirects non-IRedirected IProjects to a 
-    default host / path
-    """
-    adapts(IProject)
+    # add the 'project home' menu item before any others
+    instance._initProjectHomeMenuItem()
+
+    # Fetch the values from request and store them.
+    instance.processForm()
+
+    # We don't need this here
+    instance._initializeProject(event.request)
+
+    # ugh... roster might have been created by an event before a
+    # team was associated (in _initializeProject), need to fix up
+    roster_id = instance.objectIds(spec='OpenRoster')
+    if roster_id:
+        roster = instance._getOb(roster_id[0])
+        if not roster.getTeams():
+            roster.setTeams(instance.getTeams())
+
+@adapter(IAfterSubProjectAddedEvent)
+def handle_subproject_redirection(event):
+    instance = event.project
+    request = event.request
+    parent = event.parent 
+    _handle_parent_child_association(parent, instance)
+
+def _handle_parent_child_association(parent, child):
+    child_id = child.getId()
+    parent_info = redirect.get_info(parent)
+    parent_path = redirect.pathstr(child)
+    parent_info[child_id] = parent_path
+    child_url = "%s/%s" %(parent_info.url, child_id) 
+    redirect.activate(child, url=child_url, parent=parent_path)
