85b07456da376378044b2a0a1135ef0c7b159852
Whit Morriss <d.w.morriss@gmail.com>
2007-03-29 22:51:09 +0000

    move content code back for now. add saveFeaturelet to handlers
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/whit-hook-refactor@3920 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/handler.py
+++ opencore/project/handler.py
@@ -1,6 +1,8 @@
-""" post creation handlers """
 from opencore.interfaces.event import IAfterProjectAddedEvent, IAfterSubProjectAddedEvent
+from topp.featurelets.interfaces import IFeatureletSupporter
+from topp.featurelets.interfaces import IFeatureletRegistry
 from zope.component import adapter
+
 
 @adapter(IAfterProjectAddedEvent)
 def handle_postcreation(event):
@@ -39,3 +41,38 @@ def _handle_parent_child_association(parent, child):
     parent_info[child_id] = parent_path
     child_url = "%s/%s" %(parent_info.url, child_id) 
     redirect.activate(child, url=child_url, parent=parent_path)
+
+
+def saveFeaturelets(obj, event):
+    """
+    IObjectModified event subscriber that installs the appropriate
+    featurelets.
+    """
+    req = obj.REQUEST
+    if req.get('set_flets') is None:
+        # don't do anything unless we're actually coming from the
+        # project edit screen
+        return
+
+    # XXX there must be a better way... :-|
+    if req.get('flet_recurse_flag') is not None:
+        return
+    req.set('flet_recurse_flag', True)
+    registry = getUtility(IFeatureletRegistry)
+    supporter = IFeatureletSupporter(obj)
+
+    desired = req.get('featurelets')
+    if desired is None:
+        desired = tuple()
+    desired = set(desired)
+    installed = set(supporter.getInstalledFeatureletIds())
+
+    needed = desired.difference(installed)
+    for flet_id in needed:
+        flet = registry.getFeaturelet(flet_id)
+        supporter.installFeaturelet(flet)
+
+    removed = installed.difference(desired)
+    for flet_id in removed:
+        flet = registry.getFeaturelet(flet_id)
+        supporter.removeFeaturelet(flet)
