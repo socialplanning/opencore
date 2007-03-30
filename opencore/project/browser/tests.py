50c30d710670864d92654b935588fd8c2f0b7113
Whit Morriss <d.w.morriss@gmail.com>
2007-03-30 15:26:23 +0000

    adding test runner
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/whit-hook-refactor@3922 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/project/browser/tests.py
@@ -0,0 +1,40 @@
+import os, sys, unittest
+from zope.testing import doctest
+from Testing import ZopeTestCase
+from Testing.ZopeTestCase import PortalTestCase 
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Products.OpenPlans.tests.openplanstestcase import OpenPlansLayer
+
+optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
+
+
+def test_suite():
+    from Testing.ZopeTestCase import FunctionalDocFileSuite, installProduct
+    from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
+    from Products.PloneTestCase import setup
+    from opencore.testing import create_test_content
+    from zope.interface import alsoProvides
+    from Products.Five.utilities.marker import erase as noLongerProvides
+    from opencore import redirect
+
+    setup.setupPloneSite()
+    def readme_setup(tc):
+        create_test_content(tc.portal)
+        tc._refreshSkinData()
+
+    globs = locals()
+    readme = FunctionalDocFileSuite("README.txt",
+                                    optionflags=optionflags,
+                                    package='opencore.siteui',
+                                    test_class=FunctionalTestCase,
+                                    globs = globs,
+                                    setUp=readme_setup
+                                    )
+
+    readme.layer = OpenPlansLayer
+
+    return unittest.TestSuite((readme,))
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
