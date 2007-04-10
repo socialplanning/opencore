a6eb395c46197adb6f6fc96e15bd4c62f07f352f
Whit Morriss <d.w.morriss@gmail.com>
2007-04-10 15:02:14 +0000

    sketch of project subscribers for vhoster
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/whit-hook-refactor@4098 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/project/deliver.py
@@ -0,0 +1,44 @@
+# handlers for managing a deliverance vhoster
+from OFS.interfaces import IObjectWillBeRemovedEvent, IObjectWillBeMovedEvent
+from cStringIO import StringIO
+from opencore.interfaces import IProject
+from opencore.interfaces.event import IAfterProjectAddedEvent, IAfterSubProjectAddedEvent
+from opencore.redirect import get_info
+from opencore.redirect.interfaces import IHostInfo
+from restclient import GET, POST, PUT, DELETE
+from zope.app.container.interfaces import IObjectMovedEvent
+from zope.app.event import IObjectCreatedEvent
+from zope.app.event.interfaces import IObjectCreatedEvent
+from zope.component import adapter, getUtility
+from zope.interface import Interface
+import simplejson
+
+def service_url(project):
+    hostinfo = getUtility(IHostInfo)
+    return "http://%s.%s/.deliverance" %(project.getId(), hostinfo.host)
+
+@adapter(IAfterProjectAddedEvent)
+def init_parent_vhoster(event):
+    # @@ don't know what this suppose to do
+    url = service_url(event.project)
+    ##
+
+@adapter(IAfterSubProjectAddedEvent)
+def init_subproject_vhoster(event):
+    url = service_url(event.project)
+    out = StringIO()
+    simplejson.dump(out)
+    #@@ httplib?
+    POST("%s/remote_uris" %url, params=dict(add=out.getvalue()))
+
+@adapter(IProject, IObjectRemovedEvent)
+def remove_project(obj, event):
+    pass
+
+@adapter(IProject, IObjectWillBeMovedEvent)
+def move_project(obj, event):
+    pass
+    
+# PUT http://project.openplans.org/.deliverance/remote_uris
+# content=[..., {'path': '/subproject', 'headers': {'X-Project-Name':
+# 'subproject'}}]
