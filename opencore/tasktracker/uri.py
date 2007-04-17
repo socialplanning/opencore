cd4eb3227ea5656b94a50e1dca33e4ae947a338e
Whit Morriss <d.w.morriss@gmail.com>
2007-04-17 16:06:00 +0000

    add meta handlers
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/tt-completion@4254 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/tasktracker/uri.py
+++ opencore/tasktracker/uri.py
@@ -11,3 +11,15 @@ class TaskTrackerURI(object):
         self.uri = uri
 
 _tt_info = TaskTrackerURI()
+
+def set_tt_info(uri):
+    _tt_info.host=uri
+
+def configure_tt_info(_context, uri):
+    _context.action(
+        # if more than one DH is registered, will raise conflict
+        # warning. can be overridden
+        discriminator = 'opencore.tasktracker.tt_info already registered',
+        callable = set_tt_info,
+        args = (host, path, vhost)
+        )
