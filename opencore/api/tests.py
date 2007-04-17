5f3240016367b275e44b81ccedeb2af0cd357bf6
Whit Morriss <d.w.morriss@gmail.com>
2007-04-17 23:40:33 +0000

    adding some functional tests to opencore.api. somebody owes me a beer....
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/tt-completion@4314 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/api/tests.py
@@ -0,0 +1,33 @@
+import os, sys, unittest
+from zope.testing import doctest
+from collective.testing import utils
+from zope.testing import doctestunit
+from zope.testing import doctest
+from zope.component import testing
+from Testing import ZopeTestCase as ztc
+from Products.Five import zcml
+from zope.interface import alsoProvides
+from zope.testing.cleanup import cleanUp
+from opencore.testing.layer import SiteSetupLayer, OpenCoreContent
+
+import warnings; warnings.filterwarnings("ignore")
+
+optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
+
+def test_suite():
+    from zope.component import getMultiAdapter, getUtility
+    from zope.interface import alsoProvides
+    readme = ztc.FunctionalDocFileSuite('README.txt',
+                                        package='opencore.api',
+                                        optionflags=optionflags,
+                                        globs=locals())
+    
+    zcml_suites = (readme,)
+    for suite in zcml_suites:
+        suite.layer = OpenCoreContent
+        
+    suites = zcml_suites
+    return unittest.TestSuite(suites)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
