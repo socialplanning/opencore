dbe8d0ea33b3f4bdaa8c04ddb35d6881c84dca2f
Whit Morriss <d.w.morriss@gmail.com>
2007-04-17 16:00:30 +0000

    tt uri utility
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/tt-completion@4250 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/tasktracker/uri.py
+++ opencore/tasktracker/uri.py
@@ -2,3 +2,12 @@
 utility, directive and helper functions for locating the active
 tasktracker instance
 """
+from opencore.tasktracker.interfaces import ITaskTrackerInfo
+from zope.interface import implements
+
+class TaskTrackerURI(object):
+    implements(ITaskTrackerInfo)
+    def __init__(self, uri=None):
+        self.uri = uri
+
+_tt_info = TaskTrackerURI()
