bc740c72af7189ebf98d89cdecd6947f12305bfb
Whit Morriss <d.w.morriss@gmail.com>
2007-04-23 19:11:47 +0000

    * testing bits for utility
    * added mockhttp layer
    
    * added function for explicit http reg
    
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@4483 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/testing/__init__.py
+++ opencore/testing/__init__.py
@@ -4,3 +4,5 @@ try:
     from zope.interface import noLongerProvides
 except ImportError:
     from Products.Five.utilities.marker import erase as noLongerProvides
+
+from opencore.testing.utility import mock_utility
