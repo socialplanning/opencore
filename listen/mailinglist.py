15624958470f2864aaa5b05a007d096ec4d4077b
Rob Marianski <rob@marianski.com>
2007-04-25 19:22:22 +0000

    merge listen changes: -r3837:4549 https://svn.openplans.org/svn/opencore/branches/production
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@4584 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- listen/mailinglist.py
+++ listen/mailinglist.py
@@ -1,27 +1,79 @@
 from Products.listen.content.mailinglist import MailingList
-from Products.listen.content.mailinglist import factory_type_information \
-     as base_fti
 from Products.listen.permissions import InviteSubscribers
 from Products.OpenPlans.config import PROJECTNAME
+
+from Products.CMFCore import permissions as CMFPermissions
+from Products.listen import permissions
 
 from zope.interface import implements
 
 from interfaces import IOpenMailingList
 from fieldproperty import ListNameFieldProperty
 
-fti = base_fti[0].copy()
-fti['id'] = 'Open Mailing List'
-fti['meta_type'] = "OpenMailingList"
-fti['product'] = PROJECTNAME
-fti['factory'] = 'addOpenMailingList'
-# Add subscription invitation
-fti['aliases']['invite'] = '@@inviteMembers'
-fti['actions'] = fti['actions'] + (
-    {'id': 'invite_members',
-     'name': 'Invite Members',
-     'action': 'string:${object_url}/invite',
-     'permissions': (InviteSubscribers,)
-     },)
+PKG_NAME = 'listen'
+
+factory_type_information = ( {
+    'id'             : 'Open Mailing List',
+    'icon'           : 'mailboxer_icon.png',
+    'meta_type'      : 'OpenMailingList',
+    'description'    : "A mailing list manages user subscriptions and "\
+                       "processes incoming mail",
+    'product'        : PROJECTNAME,
+    'factory'        : 'addOpenMailingList',
+    'immediate_view' : 'edit',
+    'aliases'        : {'(Default)'     :'mailinglist_view',
+                        'index.html'    :'mailinglist_view',
+                        'view'          :'mailinglist_view',
+                        'sharing'       :'folder_localrole_form',
+                        'subscribers'   :'@@editAllSubscribers',
+                        'edit'          :'@@edit'},
+    'actions'        : (
+                        {
+                        'id': 'view',
+                        'name': 'View',
+                        'action': 'string:${object_url}',
+                        'permissions': (CMFPermissions.View,)
+                        },
+                        {
+                        'id': 'edit',
+                        'name': 'Edit',
+                        'action': 'string:${object_url}/edit',
+                        'permissions': (permissions.EditMailingList,)
+                        },
+                        {
+                        'id': 'search_archive',
+                        'name': 'Search',
+                        'action': 'string:${object_url}/archive/search_archive',
+                        'permissions': (CMFPermissions.View,)
+                        },
+                        {
+                        'id': 'archive',
+                        'name': 'Archive',
+                        'action': 'string:${object_url}/archive',
+                        'permissions': (CMFPermissions.View,)
+                        },
+                        {
+                        'id': 'membership',
+                        'name': 'Membership',
+                        'action': 'string:${object_url}/manage_membership',
+                        'permissions': (permissions.EditMailingList,)
+                        },
+                        {
+                        'id': 'moderation',
+                        'name': 'Moderation',
+                        'action': 'string:${object_url}/moderation',
+                        'permissions': (permissions.EditMailingList,)
+                        },
+                       ),
+    'filter_content_types' : True,
+    'allowed_content_types' : (),
+    'global_allow' : True,
+    'allow_discussion' : False,
+  },
+)
+
+fti = factory_type_information[0].copy()
+
 
 def addOpenMailingList(self, id, title=u''):
     """ Add an OpenMailingList """
