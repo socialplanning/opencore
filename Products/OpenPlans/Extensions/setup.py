db592e98a146c7c1bb39244f82b0063e78ad28c6
Rob Marianski <rob@marianski.com>
2007-05-01 18:08:35 +0000

    remove duplicate setCookieDomain item
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@4856 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- Products/OpenPlans/Extensions/setup.py
+++ Products/OpenPlans/Extensions/setup.py
@@ -100,7 +100,6 @@ functions = dict(
     createMemIndexes = convertFunc(createMemIndexes),
     setCookieDomain = convertFunc(setCookieDomain),
     migrate_listen_member_lookup=migrate_listen_member_lookup,
-    setCookieDomain = convertFunc(setCookieDomain)
     )
 
 class TOPPSetup(SetupWidget):
