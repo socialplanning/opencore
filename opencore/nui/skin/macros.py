1d88f77faca567c0bb077f515f924d6015060e5b
Whit Morriss <d.w.morriss@gmail.com>
2007-05-07 20:01:22 +0000

    whitespace
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5031 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/skin/macros.py
+++ opencore/nui/skin/macros.py
@@ -2,12 +2,14 @@ from Products.Five import BrowserView
 from Products.Five.skin.standardmacros import StandardMacros as BaseMacros
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
+
 class NuiMacros(BrowserView):
     """"""
     template = ZopeTwoPageTemplateFile('main_template.pt')
 
     def __getitem__(self, key):
         return self.template.macros[key]
+
 
 class StandardMacros(BaseMacros):
     macro_pages = ('nui_macros', )
