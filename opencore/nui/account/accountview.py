c6831b271ebd3a6c831cbda0f22e7ffd609ac755
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-05-10 21:26:18 +0000

    started overriding default password reset form
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5179 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/account/accountview.py
+++ opencore/nui/account/accountview.py
@@ -43,10 +43,17 @@ class ForgotLoginView(OpencoreView):
         portal_reg = getToolByName(self.context, 'portal_registration')
         portal_reg.mailPassword(userid, self.request)
         
-        return "An email has been sent to you, %s" % userid
+        return "An email has been sent to you, %s" % userid  # XXX rollie?
         return self.index(*args, **kw) # XXX not really right
 
 class PasswordResetView(OpencoreView):
-    def __call__(self, *args, **kw):        
-        self.request.get('key', None)
-        return "foo"
+    def __call__(self, *args, **kw):
+        key = self.request.get('key')
+        pw_tool = getToolByName(self.context, "portal_password_reset")
+        try:
+            pw_tool.verifyKey(key)
+        except "InvalidRequestError": # XXX rollie?
+            return "You fool! The Internet Police have already been notified of this incident. Your IP has been confiscated."
+        except "ExpiredRequestError": # XXX rollie?
+            return "YOU HAVE EXPIRED."
+        return self.index(*args, **kw)
