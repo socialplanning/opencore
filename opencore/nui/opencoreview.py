bc028bab779627fdb5ada891b976b3cbb354ea2f
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-05-10 19:10:53 +0000

    smaller reorganization
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/login-join@5166 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/opencoreview.py
+++ opencore/nui/opencoreview.py
@@ -220,20 +220,6 @@ class OpencoreView(BrowserView):
             return self.user_exists(username)
         return False
 
-class LoginView(OpencoreView):
-    @property
-    def came_from(self):
-        return self.request.get('came_from')
-
-    @property
-    def login(self):
-        return self.loggedin()
-
-    def __call__(self, *args, **kw):
-        if self.login:
-            return self.request.RESPONSE.redirect(self.came_from or self.siteURL)
-        return self.index(*args, **kw)
-
 class ProjectsView(OpencoreView):
 
     template = ZopeTwoPageTemplateFile('pages/projects.pt')
