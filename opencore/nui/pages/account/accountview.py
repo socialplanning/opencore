36814bad469a0e9ab1a3ce723cad3a94e2e1b43a
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-05-10 19:07:01 +0000

    * basic username/email lookup for password retrieval  * massive reorganization
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/login-join@5165 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/nui/pages/account/accountview.py
@@ -0,0 +1,44 @@
+"""
+AccountView: the view for some of opencore's new zope3 views.
+"""
+from Products.CMFCore.utils import getToolByName
+from opencore.nui.opencoreview import OpencoreView
+
+class JoinView(OpencoreView):
+    pass
+
+class LoginView(OpencoreView):
+    @property
+    def came_from(self):
+        return self.request.get('came_from')
+
+    @property
+    def login(self):
+        return self.loggedin()
+
+    def __call__(self, *args, **kw):
+        if self.login:
+            return self.request.RESPONSE.redirect(self.came_from or self.siteURL)
+        return self.index(*args, **kw)
+
+class ForgotLoginView(OpencoreView):
+    def __call__(self, *args, **kw):
+        user_lookup = self.request.get("__ac_name")
+        if not user_lookup:
+            return self.index(*args, **kw)
+
+        brains = self.membranetool(getId=user_lookup) or self.membranetool(getEmail=user_lookup)
+        if not len(brains):
+            plone_utils = getToolByName(self.context, 'plone_utils')
+            from Products.CMFPlone import PloneMessageFactory
+            plone_utils.addPortalMessage(PloneMessageFactory(u'You do not exist'))
+            return self.index(*args, **kw)
+
+        brain = brains[0]
+        email = brain.getEmail
+        
+        #sendEmail(to=user) XXX ask Ra
+        
+        kw['email'] = email
+        return self.index(*args, **kw) # XXX not really right
+
