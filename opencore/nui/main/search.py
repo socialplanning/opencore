c70f223c4c615ad8a0d7042ffab017027322294b
Chris Abraham <cabraham@openplans.org>
2007-05-15 19:10:44 +0000

    got the people search working properly
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5324 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/main/search.py
+++ opencore/nui/main/search.py
@@ -1,10 +1,12 @@
 import datetime
 from time import strptime
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import Batch
 from Products.AdvancedQuery import Eq, RankByQueries_Sum
 from topp.utils.pretty_date import prettyDate
  
 from opencore.nui.opencoreview import OpencoreView
+
 
 class SearchView(OpencoreView):
 
@@ -122,6 +124,9 @@ class ProjectsSearchView(SearchView):
 
 
 class PeopleSearchView(SearchView):
+    def __init__(self, context, request):
+        SearchView.__init__(self, context, request)
+        self.membrane_tool = getToolByName(context, 'membrane_tool')
 
     def __call__(self):
         personname = self.request.get('personname', None)
@@ -147,9 +152,8 @@ class PeopleSearchView(SearchView):
 
     def search_for_person_by_letter(self, letter, sort_by=None):
         letter = letter.lower()
-        query = dict(portal_type="OpenMember",
-                     Title=letter + '*')
-        people_brains = self.catalogtool(**query)
+        query = dict(Title=letter + '*')
+        people_brains = self.membrane_tool(**query)
 
         people_brains = sorted(people_brains, key=self.create_sort_fn(sort_by))
 
@@ -158,20 +162,20 @@ class PeopleSearchView(SearchView):
     def search_for_person(self, person, sort_by=None):
         person = person.lower()
 
-        proj_query = person
-        if not proj_query.endswith('*'):
-            proj_query = proj_query + '*'
+        person_query = person
+        if not person_query.endswith('*'):
+            person_query = person_query + '*'
 
         if not sort_by or sort_by == 'relevancy':
-            rs = (RankByQueries_Sum((Eq('Title', proj_query),32), (Eq('getFull_name', proj_query),16)),)
+            rs = (RankByQueries_Sum((Eq('Title', person_query),32), (Eq('getId', person_query),16)),)
         else:
             # we can't sort by title
             if sort_by == 'Title':
-                sort_by = 'sortable_title'
+                sort_by = 'exact_getFullname'
             rs = ((sort_by, 'desc'),)
 
-        people_brains = self.catalogtool.evalAdvancedQuery(
-            Eq('portal_type', 'OpenMember') & Eq('SearchableText', proj_query),
+        people_brains = self.membrane_tool.evalAdvancedQuery(
+            Eq('RosterSearchableText', person_query),
             rs,
             )
         return people_brains
@@ -179,7 +183,7 @@ class PeopleSearchView(SearchView):
 
 class HomeView(SearchView):
     def __init__(self, context, request):
-        OpencoreView.__init__(self, context, request)
+        SearchView.__init__(self, context, request)
         self.projects_search = ProjectsSearchView(context, request)
 
     def recently_updated_projects(self):
@@ -197,3 +201,4 @@ class HomeView(SearchView):
         # we get object for number of project members
         projects = (x.getObject() for x in project_brains)
         return projects
+
