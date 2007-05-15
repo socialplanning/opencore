d308010d479950dd68c229cec3e1592a8c2f4df1
David Turner <novalis@novalis.org>
2007-05-15 20:05:27 +0000

    oops again
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5331 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/project/project.py
+++ nui/project/project.py
@@ -4,3 +4,9 @@ from Products.CMFCore.utils import getToolByName
 
 class ProjectView(OpencoreView):
     project_preferences = ZopeTwoPageTemplateFile('project-preferences.pt')
+
+    def updateProject(self):
+        return self.project_preferences()
+
+    def createProject(self):
+        return self.project_preferences()
