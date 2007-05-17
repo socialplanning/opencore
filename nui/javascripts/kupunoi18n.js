cdaf91639486f2c7ee7b31ab8c710b5544948b59
Joshua Bronson <jabronson@gmail.com>
2007-05-17 21:07:16 +0000

    add kupu js to nui javascript dir
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5455 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ nui/javascripts/kupunoi18n.js
@@ -0,0 +1,14 @@
+
+/* - kupunoi18n.js - */
+window._ = function(msgid, interpolations) {
+    /* dummy _ function for systems that don't want to use i18n */
+    if (interpolations) {
+        for (var id in interpolations) {
+            var value = interpolations[id];
+            var reg = new RegExp('\\\$\\\{' + id + '\\\}', 'g');
+            msgid = msgid.replace(reg, ""+value);
+        };
+    };
+    return msgid;
+};
+
