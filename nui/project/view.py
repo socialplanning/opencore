d8b4f068e14ccf3f55991e2a2ee5bdcba088905a
Joshua Bronson <jabronson@gmail.com>
2007-05-18 20:46:49 +0000

    project-create is almost hooked up, i think
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5512 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/project/view.py
+++ nui/project/view.py
@@ -1,19 +1,36 @@
 from opencore.nui.opencoreview import OpencoreView
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.CMFCore.utils import getToolByName
+from zope import event
+from opencore.interfaces.event import AfterProjectAddedEvent #, AfterSubProjectAddedEvent
 
 class ProjectView(OpencoreView):
     project_preferences = ZopeTwoPageTemplateFile('project-preferences.pt')
     project_create = ZopeTwoPageTemplateFile('project-create.pt')
 
-    def updateProject(self):
+    def renderUpdateForm(self):
         return self.project_preferences()
 
-    def createProject(self):
+    def renderCreateForm(self):
         return self.project_create()
 
-    def doUpdateProject(self):
-        proj = self.context
-        proj.setFull_name(self.request.get('fullname'))
-        proj.setDescription(self.request.get('mission'))
+    def createProject(self):
+        import pdb; pdb.set_trace()
+        putils = getToolByName(self.context, 'plone_utils')
+        self.request.set('__initialize_project__', True)
+        self.errors = {}
+        self.context.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)
+        if self.errors:
+            self.portal_status_message = 'Please correct the indicated errors.'
+            return super(ProjectView, self).__call__(errors=self.errors)
+
+        self.context.processForm(values=self.request)
+        projid = self.request.form.get('id')
+        projid = putils.normalizeString(projid)
+        projects.invokeFactory('OpenProject', projid)
+        proj = self.portal.projects._getOb(projid)
+        event.notify(AfterProjectAddedEvent(self.context, self.request))
         self.request.response.redirect(proj.absolute_url())
+
+    def updateProject(self):
+        pass
