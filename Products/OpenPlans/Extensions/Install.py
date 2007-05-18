c5b3ed32a8e882f2f13ca8064ff4bd2d10ae4851
Chris Abraham <cabraham@openplans.org>
2007-05-18 20:20:49 +0000

    added code for installing the news folder
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5506 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- Products/OpenPlans/Extensions/Install.py
+++ Products/OpenPlans/Extensions/Install.py
@@ -622,6 +622,16 @@ def installCookieAuth(portal, out):
         print >> out, ("Move signed cookie auth to be top priority challenge "
                        "plugin")
 
+def installDefaultContent(portal, out):
+    print >> out, ("Creating '%s' content" % 'sitenews')
+    existing_item = getattr(portal.aq_base, 'news', None)
+    if existing_item is not None and existing_item.Type() != 'Folder':
+        portal.manage_delObjects([existing_item.getId()])
+        
+    if getattr(portal.aq_base, 'news', None) is None:
+        portal.invokeFactory('Folder', 'news', title='OpenPlans News')
+
+
 def install(self, migrate_atdoc_to_openpage=True):
     out = StringIO()
     portal = getToolByName(self, 'portal_url').getPortalObject()
@@ -656,5 +666,6 @@ def install(self, migrate_atdoc_to_openpage=True):
     createGreyEditTab(portal, out)
     createIndexes(portal, out)
     createMemIndexes(portal, out)
+    installDefaultContent(portal, out)
     print >> out, "Successfully installed %s." % config.PROJECTNAME
     return out.getvalue()
