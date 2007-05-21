f25cb93ad0a1ff6a12d9f14e09327b97b5b14803
Joshua Bronson <jabronson@gmail.com>
2007-05-21 01:17:57 +0000

    create project works!
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5534 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/project/view.py
+++ nui/project/view.py
@@ -14,22 +14,24 @@ class ProjectView(OpencoreView):
     def renderCreateForm(self):
         return self.project_create()
 
-    def createProject(self):
+    def handleCreate(self):
         putils = getToolByName(self.context, 'plone_utils')
         self.request.set('__initialize_project__', True)
+
         self.errors = {}
         self.context.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)
         if self.errors:
             self.portal_status_message = ['Please correct the indicated errors.']
             return self.renderCreateForm()
+        # TODO this is not enforcing non-empty project full names
 
-        self.context.processForm(values=self.request)
-        projid = self.request.form.get('id')
+        projid = self.request.form.get('projid')
         projid = putils.normalizeString(projid)
-        projects.invokeFactory('OpenProject', projid)
+        self.portal.projects.projects.invokeFactory('OpenProject', projid)
         proj = self.portal.projects._getOb(projid)
-        event.notify(AfterProjectAddedEvent(self.context, self.request))
+
+        event.notify(AfterProjectAddedEvent(proj, self.request))
         self.request.response.redirect(proj.absolute_url())
 
-    def updateProject(self):
+    def handleUpdate(self):
         pass
