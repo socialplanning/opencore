f38c875e96a972652d41fd5a90570d33029b0609
David Turner <novalis@novalis.org>
2007-05-21 21:25:58 +0000

    better code, same error
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5556 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/project/view.py
+++ nui/project/view.py
@@ -18,31 +18,31 @@ class ProjectView(OpencoreView):
         putils = getToolByName(self.context, 'plone_utils')
         self.request.set('__initialize_project__', True)
 
-        id = self.request.get('projid')
-        if not id:
-            self.errors = {'projid' : 'You need to enter a short name for the project'}
-            self.portal_status_message = ['Please correct the indicated errors.', "%s" % self.errors]
-            return self.renderCreateForm()
-        
-        stub = self.context.restrictedTraverse('portal_factory/OpenProject/' + id)
-        
-        transaction_note('Initiated creation of %s with id %s in %s' % (stub.getTypeInfo().getId(), id, context.absolute_url()))
-        
-        
         self.errors = {}
-        stub.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)
         if not self.request.get('full_name'):
             self.errors['full_name'] = 'Please add a full name'
-        if self.errors:
-            self.portal_status_message = ['Please correct the indicated errors.', "%s" % self.errors]
-            return self.renderCreateForm()
-        # TODO this is not enforcing non-empty project full names
 
         projid = self.request.form.get('projid')
         projid = putils.normalizeString(projid)
+        if not projid:
+            self.errors['projid'] = 'You need to enter a short name for the project'
+
+        if self.errors:
+            self.portal_status_message = ['Please correct the indicated errors.', "%s" % self.errors]
+            return self.renderCreateForm()
+
+
+
         self.portal.projects.projects.invokeFactory('OpenProject', projid)
         proj = self.portal.projects._getOb(projid)
+        proj.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)
+        if self.errors:
+            self.portal_status_message = ['Please correct the indicated errors.', "%s" % self.errors]
+            return self.renderCreateForm()
 
+
+        self.context.portal_factory.doCreate(self.context, projid)
+        
         event.notify(AfterProjectAddedEvent(proj, self.request))
         self.request.response.redirect(proj.absolute_url())
 
