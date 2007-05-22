21adef8c3e6ad17497c92e5bd46a5bee636b1966
Rob Marianski <rob@marianski.com>
2007-05-22 20:12:35 +0000

    getting brain for member was raising an AttributeError in some cases
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5592 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- Products/OpenPlans/Extensions/find_duplicate_emails.py
+++ Products/OpenPlans/Extensions/find_duplicate_emails.py
@@ -18,9 +18,12 @@ def find_duplicate_emails(self):
     for email in duplicate_emails:
 	if not email: continue
         brains = mb(getEmail=email)
-        mems = (m.getObject() for m in brains)
         out.write('%s\n' % email)
-        for mem in mems:
+        for brain in brains:
+            try:
+                mem = brain.getObject()
+            except AttributeError:
+                continue
             id = mem.getId()
             login_time = mem.login_time
             projects = mem.getProjectListing()
