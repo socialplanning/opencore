b55a19abb741ffe54f925137c5e0497370ac6812
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-05-24 20:15:35 +0000

    use MockMailHost for testing so SMTP server is not required
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5710 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/account/tests.py
+++ opencore/nui/account/tests.py
@@ -3,6 +3,8 @@ from zope.testing import doctest
 from Testing import ZopeTestCase
 from Testing.ZopeTestCase import PortalTestCase 
 from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Products.PasswordResetTool.tests.test_doctests import MockMailHostTestCase
+
 from opencore.testing.layer import OpencoreContent
 
 optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
@@ -12,7 +14,6 @@ import warnings; warnings.filterwarnings("ignore")
 def test_suite():
     from Products.Five.utilities.marker import erase as noLongerProvides
     from Products.PloneTestCase import setup
-    from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
     from Testing.ZopeTestCase import FunctionalDocFileSuite, installProduct
     from pprint import pprint
     from zope.interface import alsoProvides
@@ -25,7 +26,7 @@ def test_suite():
     readme = FunctionalDocFileSuite("README.txt",
                                     optionflags=optionflags,
                                     package='opencore.nui.account',
-                                    test_class=FunctionalTestCase,
+                                    test_class=MockMailHostTestCase,
                                     globs = globs,
                                     setUp=readme_setup
                                     )
