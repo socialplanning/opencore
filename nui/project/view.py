1908a0eee5c94d840dd1a211e365cbd8e25a77d7
David Turner <novalis@novalis.org>
2007-05-24 15:08:25 +0000

    set_flets
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5663 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/project/view.py
+++ nui/project/view.py
@@ -50,33 +50,8 @@ class ProjectView(OpencoreView):
 
 
         self.context.portal_factory.doCreate(self.context, title)
-        self.handleFeaturelets(proj)
         event.notify(AfterProjectAddedEvent(proj, self.request))
         self.request.response.redirect(proj.absolute_url())
-
-    def handleFeaturelets(self, proj):
-        if self.request.get('flet_recurse_flag') is not None:
-            return
-        self.request.set('flet_recurse_flag', True)
-        registry = getUtility(IFeatureletRegistry)
-        supporter = IFeatureletSupporter(proj)
-        
-        desired = self.request.get('featurelets')
-        if desired is None:
-            desired = tuple()
-        desired = set(desired)
-        installed = set(supporter.getInstalledFeatureletIds())
-
-        needed = desired.difference(installed)
-        for flet_id in needed:
-            flet = registry.getFeaturelet(flet_id)
-            supporter.installFeaturelet(flet)
-
-        removed = installed.difference(desired)
-        for flet_id in removed:
-            flet = registry.getFeaturelet(flet_id)
-            supporter.removeFeaturelet(flet)
-
 
     def handlePrefs(self):
         self.context.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)        
