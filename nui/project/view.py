335d60d84f30a6699d59cbdb39911c1b1e905d4e
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-05-25 20:06:00 +0000

    cleanup and adding portal status msgs correctly
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5769 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/project/view.py
+++ nui/project/view.py
@@ -34,24 +34,21 @@ class ProjectAddView(BaseView):
         id_ = putils.normalizeString(title)
         if self.context.has_key(id_):
             self.errors = {'title' : 'The requested short name is already taken.'}
-            self.portal_status_message = ['Please correct the indicated errors.']          
+            self.addPortalStatusMessage(u'Please correct the indicated errors.')
 
         if self.errors:
-            self.portal_status_message = ['Please correct the indicated errors.']
+            self.addPortalStatusMessage(u'Please correct the indicated errors.')
             return 
 
         proj = self.context.restrictedTraverse('portal_factory/OpenProject/%s' %id_)
         proj.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)
         if self.errors:
             transaction_note('Started creation of project: %s' %title)
-            self.portal_status_message = ['Please correct the indicated errors.']
+            self.addPortalStatusMessage(u'Please correct the indicated errors.')
             return 
 
         self.context.portal_factory.doCreate(proj, id_)
         proj = self.context._getOb(id_)
         event.notify(AfterProjectAddedEvent(proj, self.request))
         transaction_note('Finished creation of project: %s' %title)
-        self.request.RESPONSE.redirect(proj.absolute_url())
-
-
-
+        self.redirect(proj.absolute_url())
