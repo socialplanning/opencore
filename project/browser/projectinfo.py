4fd1560be4ee944c45c9c1ee7f3ee3e88665b5fb
Whit Morriss <d.w.morriss@gmail.com>
2007-05-25 19:49:26 +0000

    adding missing import
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@5766 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- project/browser/projectinfo.py
+++ project/browser/projectinfo.py
@@ -9,7 +9,7 @@ from Products.Five.browser.TrustedExpression import getEngine
 from Products.OpenPlans.interfaces import IProject, IOpenTeam
 from memojito import memoizedproperty
 from interfaces import IProjectInfo
-
+from topp.featurelets.interfaces import IFeatureletSupporter
 
 class ProjectInfoView(BrowserView):
     implements(IProjectInfo)
