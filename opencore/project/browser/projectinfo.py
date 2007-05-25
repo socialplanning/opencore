7917f6f0f1e87c778e2e2025a74bd6547298f4d9
Whit Morriss <d.w.morriss@gmail.com>
2007-05-25 22:04:56 +0000

    add memoization, abstract featurelet retrieval to a function
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5784 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/projectinfo.py
+++ opencore/project/browser/projectinfo.py
@@ -10,6 +10,14 @@ from Products.OpenPlans.interfaces import IProject, IOpenTeam
 from memojito import memoizedproperty
 from interfaces import IProjectInfo
 from topp.featurelets.interfaces import IFeatureletSupporter
+from opencore.project.utils import get_featurelets
+from plone.memoize import instance, view
+
+view.memoizedproperty = lambda func: property(view.memoize(func))
+
+# assumption here is that all instances of a piv in a request will be
+# for the same project. if this changes, we will memoize differently
+view.mcproperty = lambda func: property(view.memoize_contextless(func))
 
 class ProjectInfoView(BrowserView):
     implements(IProjectInfo)
@@ -19,7 +27,7 @@ class ProjectInfoView(BrowserView):
         self._context = (context,)
         self.request = request
 
-    @memoizedproperty
+    @view.mcproperty
     def project(self):
         if IOpenTeam.providedBy(self.context):
             # get the related project
@@ -30,14 +38,14 @@ class ProjectInfoView(BrowserView):
             if IProject.providedBy(item):
                 return item
 
-    @memoizedproperty
+    @property
     def inProject(self):
         inside = self.project is not None
         # XXX is this needed any more?
         self.request.set('inProject', inside)
         return inside
 
-    @memoizedproperty
+    @view.mcproperty
     def projectMembership(self):
         pm = getToolByName(self.context, 'portal_membership')
         if pm.isAnonymousUser():
@@ -52,19 +60,16 @@ class ProjectInfoView(BrowserView):
             
         return False
 
-    @memoizedproperty
+    @view.mcproperty
     def featurelets(self):
         flets = []
         if self.project is not None:
-            supporter = IFeatureletSupporter(self.project)
-            flet_ids = supporter.getInstalledFeatureletIds()
-            getfletdesc = supporter.getFeatureletDescriptor
-            flets = [{'name': id,
-                      'url' : getfletdesc(id)['content'][0]['id']}
-                     for id in flet_ids]
+            flets = get_featurelets(self.project)
         return flets
 
 
 engine = getEngine()
 evaluate = lambda text, ec: engine.compile(text)(ec)
 getContext = lambda data: engine.getContext(data)
+
+
