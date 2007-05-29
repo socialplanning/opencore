dc0f2874de8696e199ff282ba4a139e98c9ccef1
Whit Morriss <d.w.morriss@gmail.com>
2007-05-29 23:24:33 +0000

    remove import
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@5828 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/opencoreview.py
+++ opencore/nui/opencoreview.py
@@ -2,4 +2,4 @@
 OpencoreView: the base view for all of opencore's new zope3 views.
 """
 from opencore.nui.base import BaseView as OpencoreView
-from opencore.nui.member import YourProjectsView
+#from opencore.nui.member import MemberPreferences as YourProjectsView
