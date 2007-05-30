9ecdad8e61a1fbee94dc67088dc89a5e3693d215
Rob Miller <rob@kalistra.com>
2007-05-30 20:01:24 +0000

    merge -r5855:5856 from trunk * when not in a project or member area, only show the edit tabs if user has   edit privs
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/production@5858 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- Products/OpenPlans/skins/openplans_patches/showEditableBorder.py
+++ Products/OpenPlans/skins/openplans_patches/showEditableBorder.py
@@ -8,13 +8,26 @@
 ##title=returns whether or not current template displays *editable* border
 ##
 
-# turn border on all the time
-
-REQUEST=context.REQUEST
+# turn border on nearly all the time
+from Products.CMFCore.utils import getToolByName
+request = context.REQUEST
 
 if actions is None:
     raise AttributeError, 'You must pass in the filtered actions'
     
-if REQUEST.has_key('disable_border'): #short circuit
+if request.has_key('disable_border'): #short circuit
     return 0
+else:
+    outside = False
+
+    proj_info = context.restrictedTraverse('@@project_info')
+    mem_info = context.restrictedTraverse('@@member_info')
+    if not proj_info.inProject and not mem_info.inMemberArea:
+        outside = True
+
+    if outside:
+        mtool = getToolByName(context, 'portal_membership')
+        if not mtool.checkPermission('Modify portal content', context):
+            return 0
+    
 return 1
