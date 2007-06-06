6cae63c12d1cd35d6ca72f3ef6c5158f19b15b29
Rob Miller <rob@kalistra.com>
2007-06-06 18:24:38 +0000

    * added dynamic generation infrastructure for user menu * renamed 'subcontext' property to 'contextmenu' in TopNavView
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@6085 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/topnav/topnav.py
+++ opencore/nui/topnav/topnav.py
@@ -12,11 +12,19 @@ class TopNavView(OpencoreView):
     Provides req'd information for rendering top nav in any context.
     """
     @memoizedproperty
-    def subcontext(self):
+    def contextmenu(self):
         if self.piv.inProject:
             viewname = 'topnav-project-menu'
         elif self.inmember:
             viewname = 'topnav-member-menu'
         else:
             viewname = 'topnav-default-menu'
+        return self.get_view(viewname)
+
+    @memoizedproperty
+    def usermenu(self):
+        if self.loggedin:
+            viewname = 'topnav-auth-user-menu'
+        else:
+            viewname = 'topnav-anon-user-menu'
         return self.get_view(viewname)
