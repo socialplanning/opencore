f60d9adb5297dee5be1386cef3d5d1cb14895146
Rob Miller <rob@kalistra.com>
2007-06-07 00:05:23 +0000

    * add MemberMenuView, refactor template to get all of the menu information from   the view
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@6104 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/topnav/topnav.py
+++ nui/topnav/topnav.py
@@ -7,6 +7,7 @@ memoizedproperty = lambda func: property(view.memoize(func))
 
 from opencore.nui.base import BaseView as OpencoreView
 
+
 class TopNavView(OpencoreView):
     """
     Provides req'd information for rendering top nav in any context.
@@ -28,3 +29,49 @@ class TopNavView(OpencoreView):
         else:
             viewname = 'topnav-anon-user-menu'
         return self.get_view(viewname)
+
+
+class MemberMenuView(OpencoreView):
+    """
+    Contains the information req'd by the topnav's member context menu
+    """
+    @memoizedproperty
+    def profile_url(self):
+        return '%s/profile' % self.areaURL
+
+    @memoizedproperty
+    def contact_url(self):
+        return '%s/contact' % self.areaURL
+
+    @memoizedproperty
+    def atMemberHome(self):
+        result = False
+        memfolder = self.miv.member_folder
+        if memfolder is not None:
+            homepage = memfolder._getOb(memfolder.getDefaultPage())
+            if self.context == homepage and \
+               self.request.ACTUAL_URL not in (self.contact_url,
+                                               self.profile_url):
+                result = True
+        return result
+
+    @memoizedproperty
+    def menudata(self):
+        menudata = (
+            {'content': 'Home',
+             'href': self.areaURL,
+             'selected': self.atMemberHome,
+             },
+
+            {'content': 'Profile',
+             'href': self.profile_url,
+             'selected': self.request.ACTUAL_URL == self.profile_url,
+             },
+
+            {'content': 'Contact',
+             'href': self.contact_url,
+             'selected': self.request.ACTUAL_URL == self.contact_url,
+             },
+            )
+
+        return menudata
