e7caf45884193db4888e026ce5760722bb59974d
Joshua Bronson <jabronson@gmail.com>
2007-07-03 21:04:25 +0000

    add nuified date view
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@6965 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/listen/view.py
+++ nui/listen/view.py
@@ -1,6 +1,6 @@
 from opencore.listen.mailinglist_views import MailingListAddForm, MailingListEditForm
 from Products.listen.browser.mailinglist_views import MailingListView
-from Products.listen.browser.mail_archive_views import ArchiveForumView
+from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView
 from opencore.nui.base import BaseView
 
 class NuiMailingListView(BaseView, MailingListView):
@@ -25,4 +25,10 @@ class NuiArchiveForumView(BaseView, ArchiveForumView):
     def __init__(self, context, request):
         BaseView.__init__(self, context, request)
         ArchiveForumView.__init__(self, context, request)
+        self.errors = ()
+
+class NuiArchiveDateView(BaseView, ArchiveDateView):
+    def __init__(self, context, request):
+        BaseView.__init__(self, context, request)
+        ArchiveDateView.__init__(self, context, request)
         self.errors = ()
