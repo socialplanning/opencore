9ded24476d2ee0e77292e2b472499cb3fbc81433
Joshua Bronson <jabronson@gmail.com>
2007-07-03 20:07:12 +0000

    nuified archive view. add nui prefix to nuified listen views
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@6959 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/listen/view.py
+++ nui/listen/view.py
@@ -1,21 +1,28 @@
 from opencore.listen.mailinglist_views import MailingListAddForm, MailingListEditForm
 from Products.listen.browser.mailinglist_views import MailingListView
+from Products.listen.browser.mail_archive_views import ArchiveForumView
 from opencore.nui.base import BaseView
 
-class OpenMailingListView(BaseView, MailingListView):
+class NuiMailingListView(BaseView, MailingListView):
     def __init__(self, context, request):
         BaseView.__init__(self, context, request)
         MailingListView.__init__(self, context, request)
         self.errors = ()
 
-class MailingListAddView(BaseView, MailingListAddForm):
+class NuiMailingListAddView(BaseView, MailingListAddForm):
     def __init__(self, context, request):
         BaseView.__init__(self, context, request)
         MailingListAddForm.__init__(self, context, request)
         self.errors = ()
 
-class MailingListEditView(BaseView, MailingListEditForm):
+class NuiMailingListEditView(BaseView, MailingListEditForm):
     def __init__(self, context, request):
         BaseView.__init__(self, context, request)
         MailingListEditForm.__init__(self, context, request)
+        self.errors = ()
+
+class NuiArchiveForumView(BaseView, ArchiveForumView):
+    def __init__(self, context, request):
+        BaseView.__init__(self, context, request)
+        ArchiveForumView.__init__(self, context, request)
         self.errors = ()
