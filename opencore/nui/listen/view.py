0478dbdbd1ca1db1c68671690bf41fa30fd645f3
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-07-06 21:03:21 +0000

    svn merge -r 7067:HEAD https://svn.openplans.org/svn/opencore/trunk .
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/octopus@7086 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/listen/view.py
+++ opencore/nui/listen/view.py
@@ -1,16 +1,19 @@
 from opencore.listen.mailinglist_views import MailingListAddForm, MailingListEditForm, MailingListView
-from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView, ArchiveNewTopicView
+from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView, \
+                                                       ArchiveNewTopicView, SubFolderDateView
+from Products.listen.browser.mail_message_views import ForumMailMessageView
 from opencore.nui.base import BaseView
 
 
-def make_nui_listen_view_class(cls, set_errors=False):
-    class NuiListenView(BaseView, cls):
+def make_nui_listen_view_class(ListenClass, set_errors=False):
+    class NuiListenView(BaseView, ListenClass):
         def __init__(self, context, request):
             BaseView.__init__(self, context, request)
-            cls.__init__(self, context, request)
+            ListenClass.__init__(self, context, request)
             if set_errors:
                 self.errors = ()
     return NuiListenView
+
 
 NuiMailingListView      = make_nui_listen_view_class(MailingListView)
 NuiMailingListAddView   = make_nui_listen_view_class(MailingListAddForm, set_errors=True)
@@ -18,3 +21,21 @@ NuiMailingListEditView  = make_nui_listen_view_class(MailingListEditForm, set_er
 NuiArchiveForumView     = make_nui_listen_view_class(ArchiveForumView)
 NuiArchiveDateView      = make_nui_listen_view_class(ArchiveDateView)
 NuiArchiveNewTopicView  = make_nui_listen_view_class(ArchiveNewTopicView)
+NuiSubFolderDateView    = make_nui_listen_view_class(SubFolderDateView)
+
+class NuiForumMailMessageView(BaseView, ForumMailMessageView):
+    def __init__(self, context, request):
+        BaseView.__init__(self, context, request)
+        ForumMailMessageView.__init__(self, context, request)
+
+        # hack around alecm's "five acquisition nightmare" hack
+        # see listen/browser/mail_message_views.py line 70
+        self.kontext = None
+
+    def setKontext(self, value):
+        self.kontext = value
+
+    def getKontext(self):
+        return self.kontext
+
+    context = property(getKontext, setKontext)
