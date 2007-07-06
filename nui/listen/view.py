d6e881fcfb8eedf302ae778b937bb1b21118016d
Joshua Bronson <jabronson@gmail.com>
2007-07-06 19:55:16 +0000

    get forum_message_view hooked up initially (still need to deal with some metal) *requried hacking around alecms "nasty Five acquisition nightmare" hack in listen/browser/mail_message_views.py line 70
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@7071 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/listen/view.py
+++ nui/listen/view.py
@@ -1,15 +1,29 @@
 from opencore.listen.mailinglist_views import MailingListAddForm, MailingListEditForm, MailingListView
 from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView, ArchiveNewTopicView
+from Products.listen.browser.mail_message_views import ForumMailMessageView
 from opencore.nui.base import BaseView
 
 
-def make_nui_listen_view_class(cls, set_errors=False):
+def make_nui_listen_view_class(cls, set_errors=False, set_context=False):
     class NuiListenView(BaseView, cls):
         def __init__(self, context, request):
             BaseView.__init__(self, context, request)
             cls.__init__(self, context, request)
             if set_errors:
                 self.errors = ()
+
+            # hack around alecm's "five acquisition nightmare" hack
+            # see listen/browser/mail_message_views.py line 70
+            self.kontext = None
+
+        def setKontext(self, value):
+            self.kontext = value
+
+        def getKontext(self):
+            return self.kontext
+
+        context = property(getKontext, setKontext)
+
     return NuiListenView
 
 NuiMailingListView      = make_nui_listen_view_class(MailingListView)
@@ -18,3 +32,4 @@ NuiMailingListEditView  = make_nui_listen_view_class(MailingListEditForm, set_er
 NuiArchiveForumView     = make_nui_listen_view_class(ArchiveForumView)
 NuiArchiveDateView      = make_nui_listen_view_class(ArchiveDateView)
 NuiArchiveNewTopicView  = make_nui_listen_view_class(ArchiveNewTopicView)
+NuiForumMailMessageView = make_nui_listen_view_class(ForumMailMessageView)
