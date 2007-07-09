34ea1adef13c21cdadbe6f0f3a21ceab1c437b74
Joshua Bronson <jabronson@gmail.com>
2007-07-09 15:00:20 +0000

    * move the workaround for some listen classes' Five hack into nui listen class factory * stub out threaded_message.pt
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@7124 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/listen/view.py
+++ opencore/nui/listen/view.py
@@ -1,35 +1,29 @@
 from opencore.listen.mailinglist_views import MailingListAddForm, MailingListEditForm, MailingListView
 from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView, \
                                                        ArchiveNewTopicView, SubFolderDateView
-from Products.listen.browser.mail_message_views import ForumMailMessageView
+from Products.listen.browser.mail_message_views import ForumMailMessageView, ThreadedMailMessageView
 from opencore.nui.base import BaseView
 
 
 def make_nui_listen_view_class(ListenClass, set_errors=False):
     class NuiListenView(BaseView, ListenClass):
+        # mask the property defined in any listen views
+        context = None
         def __init__(self, context, request):
-            BaseView.__init__(self, context, request)
+            self.context = context # MUST set context before calling the ListenClass constructor
             ListenClass.__init__(self, context, request)
+            BaseView.__init__(self, context, request)
             if set_errors:
                 self.errors = ()
     return NuiListenView
 
 
-NuiMailingListView      = make_nui_listen_view_class(MailingListView)
-NuiMailingListAddView   = make_nui_listen_view_class(MailingListAddForm, set_errors=True)
-NuiMailingListEditView  = make_nui_listen_view_class(MailingListEditForm, set_errors=True)
-NuiArchiveForumView     = make_nui_listen_view_class(ArchiveForumView)
-NuiArchiveDateView      = make_nui_listen_view_class(ArchiveDateView)
-NuiArchiveNewTopicView  = make_nui_listen_view_class(ArchiveNewTopicView)
-NuiSubFolderDateView    = make_nui_listen_view_class(SubFolderDateView)
-
-class NuiForumMailMessageView(BaseView, ForumMailMessageView):
-    # mask the property defined in the listen views
-    context = None
-
-    def __init__(self, context, request):
-        # MUST set context before calling the listen base class
-        # constructors
-        self.context = context
-        ForumMailMessageView.__init__(self, context, request)
-        BaseView.__init__(self, context, request)
+NuiMailingListView         = make_nui_listen_view_class(MailingListView)
+NuiMailingListAddView      = make_nui_listen_view_class(MailingListAddForm, set_errors=True)
+NuiMailingListEditView     = make_nui_listen_view_class(MailingListEditForm, set_errors=True)
+NuiArchiveForumView        = make_nui_listen_view_class(ArchiveForumView)
+NuiArchiveDateView         = make_nui_listen_view_class(ArchiveDateView)
+NuiArchiveNewTopicView     = make_nui_listen_view_class(ArchiveNewTopicView)
+NuiSubFolderDateView       = make_nui_listen_view_class(SubFolderDateView)
+NuiThreadedMailMessageView = make_nui_listen_view_class(ThreadedMailMessageView)
+NuiForumMailMessageView    = make_nui_listen_view_class(ForumMailMessageView)
