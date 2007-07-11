2a307c89e9f5fe3e9211c93d2646cc670f182c18
David Turner <novalis@novalis.org>
2007-07-11 21:15:28 +0000

    initial work on csrf-prevention
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@7269 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/base.py
+++ nui/base.py
@@ -409,6 +409,11 @@ class BaseView(BrowserView):
         else:
             return ''
 
+    def authenticator(self):
+        return self.get_tool('browser_id_manager').getBrowserId(create=True)
+
+    def authenticator_input(self):
+        return '<input type="hidden" name="authenticator" value="%s" />' % self.authenticator()
 
 def aq_iface(obj, iface):
     obj = aq_inner(obj)
