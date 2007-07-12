d7935df3d7389b4e3e3e40f973dd2c5c0d8bc306
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-07-12 17:19:32 +0000

    * make attachment uploads work  * make attachment renames work  * begin to octopize the wiki edit view    * begin to merge wiki edit with attachment handling view    * use octopus syntax for attachment renames  * tests to be fixed in a separate commit
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@7298 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/wiki/view.py
+++ nui/wiki/view.py
@@ -1,11 +1,14 @@
 from opencore.nui.base import BaseView
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from opencore.nui.formhandler import button
+from opencore.nui.formhandler import button, OctopoLite, action
 
-class WikiEdit(BaseView):
+class WikiEdit(BaseView, OctopoLite):
 
-    @button('save')
-    def handle_request(self):
+    template = ZopeTwoPageTemplateFile("wiki-edit.pt")
+
+    @action('save')
+    def handle_save(self, target=None, fields=None):
+        """ todo: make this check for attachments too """
         self.errors = {}
         self.context.validate(REQUEST=self.request, errors=self.errors, data=1, metadata=0)
         if self.errors:
@@ -17,7 +20,107 @@ class WikiEdit(BaseView):
         repo = self.context.portal.portal_repository
         repo.save(self.context, comment = self.request.form.get('comment', ''))
         self.addPortalStatusMessage(u'Changes saved.')
+
+        self.template = None
         self.redirect(self.context.absolute_url())
+
+    def _handle_createAtt(self):
+        attachmentTitle = self.request.form.get('attachmentTitle')
+        attachmentFile = self.request.form.get('attachmentFile')
+        
+        if not attachmentFile:
+            self.errors = {'attachmentFile' : 'you forgot to upload something'}
+            return None 
+        
+        # Make sure we have a unique file name
+        fileName = attachmentFile.filename
+        
+        imageId = ''
+
+        if fileName:
+            fileName = fileName.split('/')[-1]
+            fileName = fileName.split('\\')[-1]
+            fileName = fileName.split(':')[-1]
+            plone_utils = self.get_tool('plone_utils')
+            imageId = plone_utils.normalizeString(fileName)
+            
+        if not imageId:
+            imageId = plone_utils.normalizeString(attachmentTitle)
+            
+        imageId = self._findUniqueId(imageId)
+        
+        newImageId = self.context.invokeFactory(id = imageId, type_name = 'FileAttachment')
+        if newImageId is not None and newImageId != '':
+            imageId = newImageId
+            
+        object = self.context._getOb(imageId, None)
+        object.setTitle(self._findUniqueTitle(attachmentTitle or imageId))
+        object.setFile(attachmentFile)
+        object.reindexObject()
+        return object
+
+    def _findUniqueTitle(self, title):
+        titles = [self.context._getOb(i).Title() for i in self.context.objectIds()]
+ 
+        def getVersion(title, number):
+            """returns the version string of a title"""
+            delimiter = ' v'
+            try:
+                version = int(title.rsplit(delimiter, 1)[1])
+                title = title.rsplit(delimiter,1)[0]
+            except (IndexError, ValueError):
+                pass
+            return delimiter.join((title, str(number)))
+
+        idx = 0
+        while True:
+            if not title in titles:
+                return title
+            title = getVersion(title, idx)
+            idx += 1
+
+    def _findUniqueId(self, id):
+        contextIds = self.context.objectIds()
+
+        if id not in contextIds:
+            return id
+
+        dotDelimited = id.split('.')
+
+        ext = dotDelimited[-1]
+        name = '.'.join(dotDelimited[:-1])
+
+        idx = 0
+        while ('%s.%s.%s' % (name, str(idx), ext)) in contextIds:
+            idx += 1
+
+        return ('%s.%s.%s' % (name, str(idx), ext))
+    
+    @action('upload-attachment')
+    def create_attachment(self, target=None, fields=None):
+        new_attachment = self._handle_createAtt()
+
+        if new_attachment is None:
+            return "Failed snippet"
+        self.new_attachment = lambda: new_attachment
+
+        return "Foo"
+    
+    def _handle_updateAtt(self, attach_id, title):
+        attachment = self.context._getOb(attach_id)
+        attachment.setTitle(title)
+        attachment.reindexObject('Title')
+        return attachment
+
+    @action('update-attachment')
+    def update_attachment(self, target=None, fields=None):
+        ### 
+        # do not assign directly because this will implicitly
+        # wrap the attachment in the view.
+        for obj_id, data in zip(target, fields):
+            new_attachment = self._handle_updateAtt(obj_id, data.get('title', obj_id))
+        self.new_attachment = lambda: new_attachment
+        return "attachment snippet"
 
 
 class AttachmentView(BaseView):
