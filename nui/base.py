632f16ad450f30324a07d345180469fbbfcc5da2
Whit Morriss <d.w.morriss@gmail.com>
2007-07-13 16:47:15 +0000

    append '_thumb' to image url
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk/opencore@7348 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- nui/base.py
+++ nui/base.py
@@ -229,7 +229,7 @@ class BaseView(BrowserView):
         result['portrait_url'] = self.defaultPortraitURL
         portrait = member.getProperty('portrait', None)
         if portrait:
-            result['portrait_url'] = portrait.absolute_url()
+            result['portrait_url'] = "%s_thumb" %portrait.absolute_url()
 
         return result
 
