334fb321df83955e14a03244426cf30d853d932b
Whit Morriss <d.w.morriss@gmail.com>
2007-07-16 20:27:43 +0000

    add img to globs
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@7414 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/member/tests.py
+++ opencore/nui/member/tests.py
@@ -23,6 +23,9 @@ def test_suite():
     from opencore.nui.member.interfaces import ITransientMessage
     from opencore.nui.project.interfaces import IEmailInvites
 
+    img = os.path.join(os.path.dirname(__file__), 'default-portrait.jpg')
+    portrait = open(img)
+
     setup.setupPloneSite()
     def readme_setup(tc):
         tc._refreshSkinData()
