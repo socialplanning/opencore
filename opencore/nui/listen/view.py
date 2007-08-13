cf3dcbc55cc867d72847ea564f22220af5bb72b3
Chris Abraham <cabraham@openplans.org>
2007-08-13 16:07:37 +0000

    fixed PSMs in listen (#1095)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@8198 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/listen/view.py
+++ opencore/nui/listen/view.py
@@ -10,44 +10,73 @@ from Products.listen.browser.moderation import ModerationView
 from opencore.nui.base import BaseView
 from Products.listen.interfaces import IMailingList
 from plone.memoize.view import memoize as req_memoize
+import cgi
+
+
+class ListenBaseView(BaseView):
+    @req_memoize
+    def list_url(self):
+        obj = self.context
+        while not IMailingList.providedBy(obj):
+            try:
+                obj = obj.aq_parent
+            except AttributeError:
+                return ''
+        return obj.absolute_url()            
+
+    @req_memoize
+    def list_title(self):
+        obj = self.context
+        while not IMailingList.providedBy(obj):
+            try:
+                obj = obj.aq_parent
+            except AttributeError:
+                return ''
+        return obj.Title()            
+        
+    @property
+    def portal_status_message(self):
+        if hasattr(self, '_redirected'):
+            return []
+        plone_utils = self.get_tool('plone_utils')
+        msgs = plone_utils.showPortalMessages()
+        if msgs:
+            msgs = [msg.message for msg in msgs]
+        else:
+            msgs = []
+        req_psm = self.request.form.get("portal_status_message")
+        req_psm2 = self.request.get("portal_status_message")
+        if req_psm:
+            req_psm = cgi.escape(req_psm)
+            msgs.append(req_psm)
+        elif req_psm2:
+            req_psm2 = cgi.escape(req_psm2)
+            msgs.append(req_psm2)
+
+        return msgs
+
 
 def make_nui_listen_view_class(ListenClass, set_errors=False, add_update=False):
-    class NuiListenView(BaseView, ListenClass):
+    class NuiListenView(ListenBaseView, ListenClass):
         # mask the property defined in any listen views
         context = None
         def __init__(self, context, request):
             self.context = context # MUST set context before calling the ListenClass constructor
             ListenClass.__init__(self, context, request)
             BaseView.__init__(self, context, request)
+
+            psm = self.request.get('portal_status_message')
+            
             if set_errors:
                 self.errors = ()
 
-        @req_memoize
-        def list_url(self):
-            obj = self.context
-            while not IMailingList.providedBy(obj):
-                try:
-                    obj = obj.aq_parent
-                except AttributeError:
-                    return ''
-            return obj.absolute_url()            
-
-        @req_memoize
-        def list_title(self):
-            obj = self.context
-            while not IMailingList.providedBy(obj):
-                try:
-                    obj = obj.aq_parent
-                except AttributeError:
-                    return ''
-            return obj.Title()            
-        
         if add_update:
             def update(self):
                 result = super(NuiListenView, self).update()
                 if self.status and self.errors:
                     self.addPortalStatusMessage(self.status)
                 return result
+
 
     return NuiListenView
 
