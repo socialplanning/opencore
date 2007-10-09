9a95401f117a5dee20a18ce1b2311554d41b905c
Chris Abraham <cabraham@openplans.org>
2007-10-09 18:35:54 +0000

    got the view working
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/stats@9371 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/stats/stats.py
+++ opencore/stats/stats.py
@@ -8,16 +8,18 @@ import DateTime
 
 class StatsView(BrowserView):
 
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
 
-    def get_projects(self)
-        catalog = getToolByName(app.openplans, 'portal_catalog')
+    def get_projects(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
         query = dict(portal_type='OpenProject')
         brains = catalog(**query)
+        return brains
 
     def get_active_projects(self):    
         # "active" is defined as having been modified in the last 30 days
-
         projects = self.get_projects()
-        filtered_projects = [project for project in projects if project.modified > DateTime.now()-.1]
-
+        filtered_projects = [project for project in projects if project.modified > DateTime.now()-30]
         return filtered_projects
