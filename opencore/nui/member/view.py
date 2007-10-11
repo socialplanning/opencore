627339cc51ca3e22d1de658b9f6912f6de4ad341
Rob Marianski <rob@marianski.com>
2007-10-11 22:21:33 +0000

    remove the default anonymous email setting for anonymailer from view #1522
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@9471 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/member/view.py
+++ opencore/nui/member/view.py
@@ -572,12 +572,6 @@ class MemberAccountView(BaseView, OctopoLite):
             self.addPortalStatusMessage(msg)
             return
 
-        mem_hide_email = mem.getUseAnonByDefault()
-        if mem_hide_email != hide_email:
-            mem.setUseAnonByDefault(hide_email)
-            setting = hide_email and 'anonymous' or 'not anonymous'
-            self.addPortalStatusMessage('Default email is %s' % setting)
-
         if mem.getEmail() == email:
             return
 
