41ba85cbc93603c91bac1ac83fef88cfe7f2f832
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-10-13 00:51:25 +0000

    adding needed wordpress.events file
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/wordpress-sandbox@9527 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/wordpress/events.py
@@ -0,0 +1,21 @@
+import urllib
+import hmac, sha
+from opencore.wordpress.uri import get as uri_get
+
+def notify_wordpress_user_created(mem, event):
+    uri = "%s/openplans-create-user.php" % uri_get()
+    params = {}
+    
+    params['username'] = username = mem.id
+    params['email'] = mem.getEmail()
+    
+    auth = mem.acl_users.credentials_signed_cookie_auth
+    secret = auth.secret
+    sig = hmac.new(secret, username, sha).digest()
+    params['signature'] = sig = sig.encode('base64').strip()
+    params = urllib.urlencode(params)
+    
+    response = urllib.urlopen(uri, params)
+    response = response.read()
+    if not response.startswith("Creating user"):
+        raise AssertionError("Failed to update wordpress with newly created user. %s" % response)
