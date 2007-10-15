23054620e8ac34e06df045ce1b80f44ced74d070
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-10-15 20:54:04 +0000

    added site-admin-only view for triggering oc->wp user sync. needs refactoring (holding off until i know where i want to put things) and tests (can't do until i figure out why httplib2 isn't working for wordpress and can start using mock http objects)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/wordpress-sandbox@9562 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/wordpress/views.py
@@ -0,0 +1,28 @@
+import hmac
+import sha
+import urllib
+
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+
+from opencore.content.membership import OpenMembership
+
+from opencore.wordpress import uri as wp_uri
+
+class SyncUsersView(BrowserView):
+    def sync(self):
+        base_uri = wp_uri.get()
+        uri = "%s/openplans-sync-users.php" % base_uri
+        params = {}
+
+        # XXX should be moved out somewhere.. wordpress.utils? or is it more general?
+        auth = self.context.acl_users.credentials_signed_cookie_auth
+        secret = auth.secret
+        sig = hmac.new(secret, "admin", sha).digest() # XXX use real user, don't fake it
+        params['signature'] = sig = sig.encode('base64').strip()
+
+        params = urllib.urlencode(params)
+        response = urllib.urlopen(uri, params)
+        
+        return "OK, now have a blast!"
+        
