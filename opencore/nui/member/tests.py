4851b49ee203691e621d0436c27b66c4c682f411
Rob Miller <rob@kalistra.com>
2007-10-25 22:54:16 +0000

    removing unnecessary imports
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@9945 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/member/tests.py
+++ opencore/nui/member/tests.py
@@ -1,9 +1,9 @@
-import os, sys, unittest
+import os
+import unittest
 from zope.testing import doctest
-from Testing import ZopeTestCase
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 from opencore.testing.layer import OpencoreContent as test_layer
-from zope.app.component.site import setSite
+from Products.OpenPlans.tests.openplanstestcase import OpenPlansTestCase
 
 #optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
 optionflags = doctest.ELLIPSIS
@@ -11,12 +11,8 @@ optionflags = doctest.ELLIPSIS
 import warnings; warnings.filterwarnings("ignore")
 
 def test_suite():
-    from Products.Five.utilities.marker import erase as noLongerProvides
-    from Products.PloneTestCase import setup
-    from Products.OpenPlans.tests.openplanstestcase import OpenPlansTestCase
+    # these imports are needed inside the doctests
     from Products.CMFCore.utils import getToolByName
-    from Testing.ZopeTestCase import FunctionalDocFileSuite, installProduct
-    from pprint import pprint
     from zope.interface import alsoProvides
     from zope.component import getUtility
     from opencore.interfaces import IMemberFolder
@@ -26,20 +22,12 @@ def test_suite():
     img = os.path.join(os.path.dirname(__file__), 'test-portrait.jpg')
     portrait = open(img)
 
-    setup.setupPloneSite()
-    def readme_setup(tc):
-        tc._refreshSkinData()
-
-        # we also need to set the site for the local utility
-        setSite(tc.portal)
-
     globs = locals()
     readme = FunctionalDocFileSuite("README.txt",
                                     optionflags=optionflags,
                                     package='opencore.nui.member',
                                     test_class=OpenPlansTestCase,
                                     globs = globs,
-                                    setUp=readme_setup
                                     )
 
     readme.layer = test_layer
@@ -49,7 +37,6 @@ def test_suite():
                                  package='opencore.nui.member',
                                  test_class=OpenPlansTestCase,
                                  globs=globs,
-                                 setUp=readme_setup
                                  )
 
     return unittest.TestSuite((readme, transient))
