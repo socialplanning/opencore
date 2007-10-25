459b9ecb7990610e5e73fb8eef62d7a098d35bf2
Jeff Hammel <jhammel@mozilla.com>
2007-10-25 21:40:42 +0000

    make the default mailing address from greetings@localhost
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/bugday-10-25@9938 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/configuration/__init__.py
+++ opencore/configuration/__init__.py
@@ -17,7 +17,7 @@ COOKIE_DOMAIN = '.openplans.org'
 
 SITE_FROM_ADDRESS = product_config('site-from-address',
                                    namespace='opencore',
-                                   default='greetings@openplans.org')
+                                   default='greetings@localhost')
 
 PROHIBITED_MEMBER_PREFIXES = ['openplans', 'topp', 'anon', 'admin',
                               'manager', 'webmaster', 'help', 'support']
