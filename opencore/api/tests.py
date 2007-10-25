d5339c71df0d1450fa54e747e1fad71d18ba4ddc
Rob Miller <rob@kalistra.com>
2007-10-25 21:03:21 +0000

    clean up the api tests, get rid of unused imports, etc
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/test-content-wtf@9934 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/api/tests.py
+++ opencore/api/tests.py
@@ -1,42 +1,37 @@
-import os, sys, unittest
+import unittest
 from zope.testing import doctest
-from collective.testing import utils
-from zope.testing import doctestunit
-from zope.testing import doctest
-from zope.component import testing
 from Testing import ZopeTestCase as ztc
-from Products.Five import zcml
-from zope.interface import alsoProvides
-from zope.testing.cleanup import cleanUp
-from opencore.testing.layer import SiteSetupLayer, OpenCoreContent
+from opencore.testing.layer import OpencoreContent as test_layer
 
 import warnings; warnings.filterwarnings("ignore")
 
 optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
 
 def test_suite():
-    from zope.component import getMultiAdapter, getUtility
-    from zope.interface import alsoProvides
     from Products.OpenPlans.tests.openplanstestcase import OpenPlansTestCase
+    from zope.component import getUtility
+                   
+    globs = locals()
 
     readme = ztc.FunctionalDocFileSuite('README.txt',
                                         package='opencore.api',
                                         optionflags=optionflags,
                                         test_class=OpenPlansTestCase,
-                                        globs=locals(),
+                                        globs=globs,
                                         )
+    readme.layer = test_layer
+    
     member_api = ztc.FunctionalDocFileSuite('member_api.txt',
                                             package='opencore.api',
                                             optionflags=optionflags,
                                             test_class=OpenPlansTestCase,
-                                            globs=locals(),
+                                            globs=globs,
                                             )
     
-    zcml_suites = (readme,member_api)
-    for suite in zcml_suites:
-        suite.layer = OpenCoreContent
+    suites = (readme, member_api)
+    for suite in suites:
+        suite.layer = test_layer
         
-    suites = zcml_suites
     return unittest.TestSuite(suites)
 
 if __name__ == '__main__':
