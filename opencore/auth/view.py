462974152f5306bb4825151d880df719f3b3a43c
Rob Miller <rob@kalistra.com>
2007-10-26 02:00:48 +0000

    exclude remote auth plugins from being used when remote auth API is used
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/remote-auth@9952 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/auth/view.py
+++ opencore/auth/view.py
@@ -1,6 +1,16 @@
+import logging
+
 from Acquisition import aq_inner
+
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
+from Products.PluggableAuthService.PluggableAuthService import \
+     _SWALLOWABLE_PLUGIN_EXCEPTIONS
+
+from remoteauthplugin import RemoteOpenCoreAuth
+
+logger = logging.getLogger('opencore')
 
 class RemoteAuthView(BrowserView):
     """
@@ -13,15 +23,41 @@ class RemoteAuthView(BrowserView):
     def authenticate(self):
         """
         Extract username and password from request form and return
-        simply 'True' or 'False'.
+        simply 'True' or 'False'.  We iterate through the auth
+        plug-ins by hand, so we can exclude OpenCore remote auth
+        plug-ins, to prevent infinite auth loops when 2 sites point to
+        each other.
         """
         context = aq_inner(self.context)
-        uf = getToolByName(context, 'acl_users')
+        pas = getToolByName(context, 'acl_users')
+        plugins = pas.plugins
+        try:
+            authenticators = plugins.listPlugins(IAuthenticationPlugin)
+        except _SWALLOWABLE_PLUGIN_EXCEPTIONS:
+            logger.info("PluggableAuthService: Plugin listing error",
+                        exc_info=1)
+            authenticators = tuple()
+        
         form = self.request.form
-        result = uf.authenticate(form.get('username'),
-                                 form.get('password'),
-                                 self.request)
-        if result is None:
-            return 'False'
-        else:
+        credentials = {'login': form.get('username'),
+                       'password': form.get('password'),
+                       }
+
+        user_id = None
+        for authenticator_id, auth in authenticators:
+            if auth.meta_type == RemoteOpenCoreAuth.meta_type:
+                continue
+            try:
+                uid_and_name = auth.authenticateCredentials(credentials)
+                if uid_and_name is None or uid_and_name == (None,None):
+                    continue
+                user_id, name = uid_and_name
+            except _SWALLOWABLE_PLUGIN_EXCEPTIONS:
+                logger.info('PluggableAuthService: AuthenticationPlugin %s error',
+                            authenticator_id, exc_info=1)
+                continue
+
+        if user_id:
             return 'True'
+        else:
+            return 'False'
