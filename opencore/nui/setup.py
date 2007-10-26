07399c3e435b2195bcce10ad1abd9aeade1d3128
Rob Miller <rob@kalistra.com>
2007-10-26 19:32:50 +0000

    added setup widget for opencore remote auth plugin
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/remote-auth@9969 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/setup.py
+++ opencore/nui/setup.py
@@ -24,7 +24,8 @@ from Products.OpenPlans.Extensions.Install import createMemIndexes, \
      installColumns, createValidationMember, \
      install_local_transient_message_utility, install_email_invites_utility
 from Products.OpenPlans.Extensions.Install import setCaseInsensitiveLogins, \
-     setSiteEmailAddresses, updateWorkflowRoleMappings, \
+     setSiteEmailAddresses, updateWorkflowRoleMappings
+from Products.OpenPlans.Extensions.Install import install_remote_auth_plugin, \
      install_team_placeful_workflow_policies, addCatalogQueue
 from Products.OpenPlans.Extensions.utils import reinstallSubskins
 from Products.OpenPlans import config as op_config
@@ -313,7 +314,8 @@ nui_functions['annotate last modified author'] = annotate_last_modified_author
 nui_functions["Propagate workflow security settings"] = \
                          convertFunc(updateWorkflowRoleMappings)
 nui_functions['markNewsItems'] = markNewsItems
-
+nui_functions['Install OpenCore Remote Auth Plugin'] = \
+                       convertFunc(install_remote_auth_plugin)
 
 def run_nui_setup(portal):
     pm = portal.portal_migration
