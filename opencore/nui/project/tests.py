9dc989cbd5a82c5ffc759df9ca6323c16c5e7124
Whit Morriss <d.w.morriss@gmail.com>
2007-11-01 09:55:28 +0000

    - adding delete project test
    - clean up a bit, use opencore.testing.dtfactory
    
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@10212 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/project/tests.py
+++ opencore/nui/project/tests.py
@@ -1,16 +1,17 @@
 import os, sys, unittest
 from zope.testing import doctest
 from Testing import ZopeTestCase
-from Testing.ZopeTestCase import PortalTestCase 
-from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Testing.ZopeTestCase import PortalTestCase
 from opencore.testing.layer import OpencoreContent
 from Products.OpenPlans.tests.openplanstestcase import OpenPlansTestCase
 from zope.interface import alsoProvides
 from opencore.featurelets.interfaces import IListenContainer
 from opencore.testing.layer import MockHTTPWithContent
+from opencore.testing import setup as oc_setup
 from zope.app.component.hooks import setSite
 from Products.Five.site.localsite import enableLocalSiteHook
 from zope.app.component.hooks import setSite, setHooks
+from opencore.testing import dtfactory as dtf
 
 #optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
 optionflags = doctest.ELLIPSIS
@@ -20,15 +21,26 @@ import warnings; warnings.filterwarnings("ignore")
 def test_suite():
     from Products.Five.utilities.marker import erase as noLongerProvides
     from Products.PloneTestCase import setup
-    from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
     from Products.CMFCore.utils import getToolByName
-    from Testing.ZopeTestCase import FunctionalDocFileSuite, installProduct
+    from Testing.ZopeTestCase import installProduct
+    from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
     from pprint import pprint
     from zope.interface import alsoProvides
     from zope.component import getUtility
     from opencore.interfaces.workflow import IReadWorkflowPolicySupport
     from opencore.testing import utils
     from opencore.nui.indexing import authenticated_memberid
+
+    # @@ bah... crappy irregular import scheme
+    from opencore.tasktracker.featurelet import TaskTrackerFeaturelet
+    from opencore.featurelets.listen import ListenFeaturelet
+
+    # for delet-project
+    from topp.featurelets.interfaces import IFeatureletSupporter
+    from topp.clockqueue.interfaces import IClockQueue
+    import pdb
+        
+    setup.setupPloneSite()
 
     def contents_content(tc):
         tc.loginAsPortalOwner()
@@ -46,82 +58,87 @@ def test_suite():
         setHooks()
         proj.lists.invokeFactory('Open Mailing List', 'list1', title=u'new list')
         
-    def readme_setup(tc):
-        tc._refreshSkinData()
-
     def metadata_setup(tc):
         tc.project = tc.portal.projects.p1
         tc.page = getattr(tc.project, 'project-home')
 
-    def manage_team_setup(tc):
-        from zope.app.component.site import setSite
-        setSite(tc.portal)
-
     globs = locals()
-    readme = FunctionalDocFileSuite("README.txt", 
+    readme = dtf.FunctionalDocFileSuite("README.txt", 
                                     optionflags=optionflags,
                                     package='opencore.nui.project',
                                     test_class=FunctionalTestCase,
                                     globs = globs,
-                                    setUp=readme_setup
+                                    setUp=oc_setup.fresh_skin,
+                                    layer = MockHTTPWithContent                                       
                                     )
-    
-    metadata = FunctionalDocFileSuite("metadata.txt", 
-                                    optionflags=optionflags,
-                                    package='opencore.nui.project',
-                                    test_class=FunctionalTestCase,
-                                    globs = globs,
-                                    setUp=metadata_setup
-                                    )
-    
-    contents = FunctionalDocFileSuite("contents.txt",
+
+    delete = dtf.FunctionalDocFileSuite("delete-project.txt",
                                     optionflags=optionflags,
                                     package='opencore.nui.project',
                                     test_class=OpenPlansTestCase,
                                     globs = globs,
-                                    setUp=contents_content, 
+                                    setUp=oc_setup.extended_tt_setup,
+                                    layer = MockHTTPWithContent                                       
+                                    )
+    
+    metadata = dtf.FunctionalDocFileSuite("metadata.txt", 
+                                    optionflags=optionflags,
+                                    package='opencore.nui.project',
+                                    test_class=FunctionalTestCase,
+                                    globs = globs,
+                                    setUp=metadata_setup,
+                                    layer=OpencoreContent     
+                                    )
+    
+    contents = dtf.FunctionalDocFileSuite("contents.txt",
+                                    optionflags=optionflags,
+                                    package='opencore.nui.project',
+                                    test_class=OpenPlansTestCase,
+                                    globs = globs,
+                                    setUp=contents_content,
+                                    layer=OpencoreContent                                              
                                     )
 
-    manage_team = FunctionalDocFileSuite("manage-team.txt",
+    manage_team = dtf.FunctionalDocFileSuite("manage-team.txt",
                                          optionflags=optionflags,
                                          package='opencore.nui.project',
                                          test_class=OpenPlansTestCase,
                                          globs = globs, 
-                                         setUp=manage_team_setup
+                                         setUp=oc_setup.set_portal_as_site,
+                                         layer=OpencoreContent                                                 
                                          )
 
-    request_membership = FunctionalDocFileSuite("request-membership.txt",
+    request_membership = dtf.FunctionalDocFileSuite("request-membership.txt",
                                                 optionflags=optionflags,
                                                 package='opencore.nui.project',
                                                 test_class=OpenPlansTestCase,
                                                 globs = globs, 
-                                                setUp=manage_team_setup,
+                                                setUp=oc_setup.set_portal_as_site,
+                                                layer=OpencoreContent                                                        
                                                 )
 
-    homepage = FunctionalDocFileSuite("homepage.txt",
+    homepage = dtf.FunctionalDocFileSuite("homepage.txt",
                                                 optionflags=optionflags,
                                                 package='opencore.nui.project',
                                                 test_class=OpenPlansTestCase,
                                                 globs = globs, 
-                                                setUp=manage_team_setup,
+                                                setUp=oc_setup.set_portal_as_site,
+                                                layer=OpencoreContent     
                                                 )
 
-#    preferences = FunctionalDocFileSuite("preferences.txt",
-#                                         optionflags=optionflags,
-#                                         package='opencore.nui.project',
-#                                         test_class=OpenPlansTestCase,
-#                                         globs = globs, 
-#                                         setUp=manage_team_setup,
-#                                         )    
-#
-#    suites = (contents, metadata, manage_team, request_membership, preferences)
+##     preferences = FunctionalDocFileSuite("preferences.txt",
+##                                          optionflags=optionflags,
+##                                          package='opencore.nui.project',
+##                                          test_class=OpenPlansTestCase,
+##                                          globs = globs, 
+##                                          setUp=manage_team_setup,
+##                                          )    
+
+##     suites = (contents, metadata, manage_team, request_membership, preferences)
     suites = (contents, metadata, manage_team, request_membership, homepage)
-    for suite in suites:
-        suite.layer = OpencoreContent
-    readme.layer = MockHTTPWithContent
     unit = doctest.DocTestSuite('opencore.nui.project.view',
                                 optionflags=optionflags)
-    return unittest.TestSuite(suites + (readme, unit))
+    return unittest.TestSuite(suites + (readme, unit, delete))
 
 
 if __name__ == '__main__':
