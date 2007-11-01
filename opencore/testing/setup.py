f0cba0b9137c27f20728f9063808957a427c493a
Whit Morriss <d.w.morriss@gmail.com>
2007-11-01 09:50:51 +0000

    add a new module for general setUps for doctests in opencore
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@10209 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/testing/setup.py
@@ -0,0 +1,31 @@
+"""
+some basic opencore setups
+"""
+from opencore.testing import utils
+
+def simple_setup(tc):
+    tc._refreshSkinData()
+    tc.request = tc.app.REQUEST
+    tc.response = tc.request.RESPONSE
+    tc.homepage = getattr(tc.portal, 'site-home')
+    tc.projects = tc.portal.projects
+
+def base_tt_setup(tc):
+    tc.new_request = utils.new_request()
+    import opencore.tasktracker
+    from Products.Five import zcml
+    zcml.load_config('test-directive.zcml', opencore.tasktracker)
+    from zope.app.annotation.interfaces import IAttributeAnnotatable
+    from zope.testing.loggingsupport import InstalledHandler
+    tc.log = InstalledHandler(opencore.tasktracker.LOG)
+
+def extended_tt_setup(tc):
+    base_tt_setup(tc)
+    simple_setup(tc)
+
+def fresh_skin(tc):
+    tc._refreshSkinData()
+
+def set_portal_as_site(tc):
+    from zope.app.component.site import setSite
+    setSite(tc.portal)
