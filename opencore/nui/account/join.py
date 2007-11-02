1ce34446d7634c7d7bcce1bb3ba8198c7baea43c
Whit Morriss <d.w.morriss@gmail.com>
2007-11-02 23:46:36 +0000

    first pass at ui
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/whit-invite-usablity@10320 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/account/join.py
+++ opencore/nui/account/join.py
@@ -8,11 +8,14 @@ from opencore.nui.formhandler import action, post_only
 from opencore.nui.base import view
 from opencore.nui.project.interfaces import IEmailInvites
 from zope.component import getUtility
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
 
 class InviteJoinView(accountview.JoinView):
     """a preconfirmed join view that also introspects any invitation a
     perspective member has"""
+
+    template = ZopeTwoPageTemplateFile('invite-join.pt')
 
     def do_project_joins(self, member, project_ids):
         for proj_id in self.project_ids:
@@ -28,7 +31,11 @@ class InviteJoinView(accountview.JoinView):
 
     @view.memoizedproperty
     def invites(self):
-        return self.invite_util.getInvitesByEmailAddress(self.email)
+        return sorted(self.invite_util.getInvitesByEmailAddress(self.email))
+
+    def invite_map(self):
+        return [dict(id=invite, title=self.proj_title(invite)) \
+                for invite in invites]
 
     @action('join', apply=post_only(raise_=False))
     def create_member(self, targets=None, fields=None):
@@ -37,5 +44,8 @@ class InviteJoinView(accountview.JoinView):
         member = super(InviteJoinView, self)._create_member(targets, fields, confirmed=True)
         self.do_project_joins(member, self.request.get('invited_projects'))        
 
-
-
+    def proj_title(self, invite):
+        proj_obj = self.context.projects.get(invite)
+        if proj_obj is not None:
+            return proj_obj.Title()
+        return None
