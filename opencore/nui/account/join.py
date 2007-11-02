a74db70f5587c50011a24a2b85b95304de7d63a4
Whit Morriss <d.w.morriss@gmail.com>
2007-11-02 19:54:43 +0000

    eliminate now uneeded import
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/whit-invite-usablity@10275 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/account/join.py
+++ opencore/nui/account/join.py
@@ -4,9 +4,10 @@ separate pre-confirmed view for folks already invited to a project
 # @@ join should move in here too
 
 from opencore.nui.account import accountview
-from opencore.nui.formhandler import action
-from opencore.nui.base import memoizedproperty
-from topp.clockqueue import AnnotationQueue
+from opencore.nui.formhandler import action, post_only
+from opencore.nui.base import view
+from opencore.nui.project.interfaces import IEmailInvites
+from zope.component import getUtility
 import time
 
 class InviteJoinView(accountview.JoinView):
@@ -16,35 +17,29 @@ class InviteJoinView(accountview.JoinView):
     def _get_confirmation_email(self):
         pass
 
-    def _get_invites(self):
-        pass
-
     def do_project_joins(self, member, project_ids):
-        pass
+        for proj_id in self.project_ids:
+            continue
     
-    @memoizedproperty
+    @view.memoizedproperty
+    @staticmethod
+    def invite_util():
+        return getUtility(IEmailInvites)
+    
+    @view.memoizedproperty
     def email(self):
-        return self._get_confirmation_email(self.request.get('__k'))
+        return self.request('email')
 
-    @memoizedproperty
+    @view.memoizedproperty
     def invites(self):
-        return self._get_invites(self.email)
-    
+        return self.invite_util.getInvitesByEmailAddress(self.email)
+
     @action('join', apply=post_only(raise_=False))
     def create_member(self, targets=None, fields=None):
-        email = self.confirm_confirmation(self.request.get('__k'))
-        if not email:
+        if not int(request.get('__k')) == self.invites.key:
             raise ValueError('Bad confirmation key')
-        member = super(InviteJoinView, self).create_member(targets, fields, confirmed=True)
+        member = super(InviteJoinView, self)._create_member(targets, fields, confirmed=True)
         self.do_project_joins(member, self.request.get('invited_projects'))        
 
 
-class InviteKeys(AnnotationQueue):
-    key = "opencore.account.invitejoin"
 
-    def new_key(self, email, project_id):
-        return hash((email, project_id, InviteKeys))
-        
-
-    def append_invite(self, email, project_id):
-        pass
