df64e04ac783737c2e1b16e634b9bfa4ccf8866e
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-11-05 17:03:13 +0000

    email sender adapts siteroot and sends email  * email sender object  * tests it's dumb and in the wrong place - it's actually specific to opencore.nui.project.mship_messages. may move to appropriate module but will not do much to improve it for now, can revisit when anything else uses it
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/streamline-project-join@10476 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/utility/tests.py
+++ opencore/utility/tests.py
@@ -10,6 +10,8 @@ from zope.interface import alsoProvides
 from zope.testing.cleanup import cleanUp
 #from opencore.testing.layer import SiteSetupLayer, OpenCoreContent, ZCML
 from collective.testing.layer import ZCMLLayer as ZCML
+from Products.OpenPlans.tests.openplanstestcase import OpenPlansTestCase
+from opencore.testing.layer import OpencoreContent as test_layer
 
 import warnings; warnings.filterwarnings("ignore")
 
@@ -17,6 +19,7 @@ optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
 
 def test_suite():
     from zope.component import getMultiAdapter, getUtility
+    from Products.CMFCore.utils import getToolByName
     from opencore.testing import *
     import pdb; st = pdb.set_trace
     readme = ztc.FunctionalDocFileSuite('README.txt',
@@ -27,9 +30,17 @@ def test_suite():
     zcml_suites = (readme,)
     for suite in zcml_suites:
         suite.layer = ZCML
-        
+    
+    email_sender = ztc.FunctionalDocFileSuite("email-sender.txt",
+                                              optionflags=optionflags,
+                                              package='opencore.utility',
+                                              test_class=OpenPlansTestCase,
+                                              globs = locals()
+                                              )
+    email_sender.layer = test_layer
+    
     suites = zcml_suites
-    return unittest.TestSuite(suites)
+    return unittest.TestSuite(suites + (email_sender,))
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
