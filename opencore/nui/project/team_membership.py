461a777adeeee3006805397b3f2e4731e135506c
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-11-05 17:19:33 +0000

    store siteroot on RequestMembership (silly?)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/streamline-project-join@10481 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/project/team_membership.py
+++ opencore/nui/project/team_membership.py
@@ -1,8 +1,11 @@
+from Acquisition import aq_parent
 from zope.interface import implements
 from zope.component import adapts
 
 from opencore.interfaces import IOpenTeam
+from opencore.interfaces import IOpenSiteRoot
 from opencore.interfaces.pending_requests import IRequestMembership
+from opencore.utility.interfaces import IEmailSender
 
 class RequestMembershipWithEmail(object):
     """
@@ -15,11 +18,20 @@ class RequestMembershipWithEmail(object):
 
     def __init__(self, context):
         self.context = context
+        portal = context
+        
+        # XXX this is no good at all
+        while portal is not None:
+            portal = aq_parent(portal)
+            if IOpenSiteRoot.providedBy(portal):
+                break
+        self.portal = portal
 
     def join(self):
         context = self.context
         joined = context.join()
         if not joined:
             return False
-        # send email
+        email_sender = IEmailSender(self.portal)
+        #email_sender.sendMail()
         return True
