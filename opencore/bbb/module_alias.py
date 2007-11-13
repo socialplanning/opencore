d590cc3ab6c94b76503a892a5b4fb0d3cea7ec86
Whit Morriss <d.w.morriss@gmail.com>
2007-11-13 23:53:38 +0000

    update module aliases
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@10985 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/bbb/module_alias.py
+++ opencore/bbb/module_alias.py
@@ -1,17 +1,21 @@
 # BBB
 import sys
+import opencore
 import opencore.member
-#import opencore.member.browser
+import opencore.member.transient_messages
+import opencore.member.browser
 import opencore.interfaces.member
 import opencore.interfaces.bbb
 import opencore.browser
+import opencore.project
 import opencore.project.browser
 sys.modules['opencore.siteui'] = opencore.browser
 #sys.modules['opencore.nui'] = opencore.browser
 sys.modules['opencore.nui.project'] = opencore.project.browser
-#sys.modules['opencore.nui.member'] = opencore.member.browser
+sys.modules['opencore.nui.member'] = opencore.member.browser
+sys.modules['opencore.nui.member.transient_messages'] = opencore.member.transient_messages
 sys.modules['opencore.nui.member.interfaces'] = opencore.interfaces.bbb
 sys.modules['opencore.nui.project.interfaces'] = opencore.interfaces.bbb
 sys.modules['opencore.siteui.interfaces'] = opencore.interfaces.member
 
-del sys
+
