8b714dca9661512372e3d729c410c6f8b7b3f2b6
Rob Miller <rob@kalistra.com>
2007-11-14 01:58:36 +0000

    adding a view to check if a given email address is in use
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/tmp-openplans.org-remote-auth@10994 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/member/view.py
+++ opencore/nui/member/view.py
@@ -1,8 +1,11 @@
+import urllib
+
 from zope import event
 from zope.component import getUtility
 
 from zExceptions import BadRequest
 from zExceptions import Redirect
+from zExceptions import NotFound
 
 from plone.memoize.view import memoize as req_memoize
 
@@ -602,3 +605,33 @@ class MemberAccountView(BaseView, OctopoLite):
     def pretty_role(self, role):
         role = self.role_map.get(role, role)
         return role
+
+
+class EmailExistsView(BaseView):
+    """
+    Simple view that provides a RESTful API for determining whether a
+    given email address is in use on the site.
+    """
+    def email_exists(self):
+        """
+        Expects an 'email' entry in the request form.  Returns a 200
+        response if the email is being used, a 404 otherwise.
+        """
+        email = self.request.form.get('email')
+        response = self.request.response
+        response.setHeader('Content-Type', 'text/xml')
+        if email is not None:
+            email = urllib.unquote(email)
+            mbtool = self.get_tool('membrane_tool')
+            if len(mbtool.unrestrictedSearchResults(getEmail=email)) > 0:
+                # we found a match
+                content = "<email>%s</email>" % email
+                response.setHeader('Content-Length', len(content))
+                return content
+
+        # no match
+        response.setStatus(404)
+        content = "<email />"
+        response.setHeader('Content-Length', len(content))
+        return content
+        
