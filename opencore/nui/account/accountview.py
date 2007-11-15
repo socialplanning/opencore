6151be6855ed27771e43be83aa3ff3c46c439ac1
Paul Winkler <slinkp@gmail.com>
2007-11-15 23:38:13 +0000

    trivial cleanup.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11058 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/account/accountview.py
+++ opencore/nui/account/accountview.py
@@ -21,6 +21,7 @@ import urllib
 
 
 logger = logging.getLogger("opencore.nui.accountview")
+marker = object()
 
 class AccountView(BaseView):
     """
@@ -250,21 +251,19 @@ class ConfirmAccountView(AccountView):
     
     @instance.memoizedproperty
     def member(self):
-        member = None
         try:
             UID, confirmation_key = self.key.split('confirm')
         except ValueError: # if there is no 'confirm' (or too many?)
             return None
-    
+
         # we need to do an unrestrictedSearch because a default search
         # will filter results by user permissions
         matches = self.membranetool.unrestrictedSearchResults(UID=UID)
         if matches:
             member = matches[0].getObject()
-            if member._confirmation_key != confirmation_key:
-                return None
-
-        return member
+            if member._confirmation_key == confirmation_key:
+                return member
+        return None
 
     def confirm(self, member):
         """Move member into the confirmed workflow state"""
@@ -276,7 +275,7 @@ class ConfirmAccountView(AccountView):
 
         member.confirm()
         return True
-        
+
     def handle_confirmation(self, *args, **kw):
         member = self.member
         if member is None:
