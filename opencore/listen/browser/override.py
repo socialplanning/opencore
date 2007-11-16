9aa5c99d60b19fc573c6f6ac02c259acec581475
Whit Morriss <d.w.morriss@gmail.com>
2007-11-16 22:30:21 +0000

    fix up inheritance (seems to be working now, needs tests)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11076 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/browser/override.py
+++ opencore/listen/browser/override.py
@@ -1,18 +1,20 @@
-from opencore.listen.browser.view import ListenBaseView
+from opencore.listen.browser.view import NuiMailingListView as BaseView
 from Acquisition import aq_inner
 
 
-class ArchiveWrapper(ListenBaseView):
+class ArchiveWrapper(BaseView):
     """wraps archive's default view so it can be used as the index for
     the archive's container"""
-
-    def __init__(self, context, request):
-        self.context = context
-
+        
     def get_forum_view(self):
         archive = aq_inner(self.context.archive)
         return archive.restrictedTraverse('forum_view')
 
+    def index(self):
+        pass
+
     def __call__(self):
+        import pdb;pdb.set_trace()
+        BaseView.__call__(self)
         forum_view = self.get_forum_view()
         return forum_view()
