f72b3baaeef4657ff184a59822551ad78d921176
Chris Abraham <cabraham@openplans.org>
2007-11-19 20:07:53 +0000

    updated the i18n files and tests
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11107 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/events.py
+++ opencore/listen/events.py
@@ -86,7 +86,7 @@ def listen_featurelet_installed(proj, event):
     ms_tool = getToolByName(proj, 'portal_membership')
     cur_mem_id = unicode(ms_tool.getAuthenticatedMember().getId())
     ml.managers = (cur_mem_id,)
-    ml.setDescription(_(u'Discussion list for this project, consisting of all project members.'))
+    ml.setDescription(_(u'discussion_list_desc', u'Discussion list for this project, consisting of all project members.'))
     notify(ObjectCreatedEvent(ml))
 
     memlist = IWriteMembershipList(ml)
