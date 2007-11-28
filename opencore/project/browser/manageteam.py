18558110480526737bbcb5689412f1bb279844e7
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-11-28 16:00:37 +0000

    i18nify PSM from r11301. wow that took a while. this i18n setup doesn't actually make any sense to me, i just pretend it does sometimes
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11304 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/manageteam.py
+++ opencore/project/browser/manageteam.py
@@ -362,11 +362,11 @@ class ManageTeamView(TeamRelatedView, formhandler.OctopoLite):
             sender.sendEmail(mem_id, msg_id='remind_invitee', **msg_vars)
 
         if not mem_ids:
-            msg = "Please select at least one person to send a reminder to."
+            self.addPortalStatusMessage(_(u"remind_invite_none_selected"))
         else:
             plural = len(mem_ids) != 1
             msg = "Reminder%s sent: %s" % (plural and 's' or '', ", ".join(mem_ids))
-        self.add_status_message(msg)
+            self.add_status_message(msg)
 
 
     ##################
