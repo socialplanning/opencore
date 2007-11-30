a14b21186cae5c040d7f0cf8517d8352f04aeaee
Rob Marianski <rob@marianski.com>
2007-11-30 03:58:41 +0000

    more reorganization
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/viewlet_menuitems@11446 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/browser/topnav/manager.py
+++ opencore/browser/topnav/manager.py
@@ -1,92 +1,12 @@
 from Acquisition import aq_base
 from opencore.browser.topnav.viewlet import BaseMenuItem
-from opencore.interfaces import IOpenPage
-from opencore.interfaces.adding import IAddProject
-from opencore.interfaces.adding import IAmAPeopleFolder
-from Products.CMFCore.utils import getToolByName
+from opencore.browser.topnav.viewlet import contained_item_url
+from opencore.browser.topnav.viewlet import nofilter
+from opencore.browser.topnav.viewlet import default_css
+from opencore.browser.topnav.viewlet import if_request_starts_with_url
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.Five.viewlet.manager import ViewletManagerBase
 from Products.Five.viewlet.metaconfigure import viewletDirective
-from topp.utils import zutils
-from zope.interface import Interface
-
-def contained_within(viewlet):
-    return zutils.aq_iface(viewlet.context, viewlet.container)
-
-def nofilter(viewlet):
-    return True
-
-def contained_item_url(viewlet):
-    item = contained_within(viewlet)
-    if item is None:
-        url = viewlet.context.absolute_url()
-    else:
-        url = item.absolute_url()
-    return '%s/%s' % (url, viewlet.item_url)
-
-def portal_and_item_url(viewlet):
-    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
-    return '%s/%s' % (portal.absolute_url(), viewlet.item_url)
-
-def people_url(viewlet):
-    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
-    return '%s/people' % portal.absolute_url()
-
-def projects_url(viewlet):
-    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
-    return '%s/projects' % portal.absolute_url()
-
-def project_create_url(viewlet):
-    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
-    return '%s/projects/create' % portal.absolute_url()
-
-def member_wiki_url(viewlet):
-    mstool = getToolByName(viewlet.context, 'portal_membership')
-    home_folder = mstool.getHomeFolder()
-    id_ = home_folder.getId()
-    return '%s/%s-home' % (home_folder.absolute_url(), id_)
-
-def project_wiki_url(viewlet):
-    proj = contained_within(viewlet)
-    return '%s/project-home' % proj.absolute_url()
-
-def if_request_starts_with_url(viewlet):
-    return viewlet.request.ACTUAL_URL.startswith(viewlet.url())
-
-def if_portal_people_or_projects(viewlet):
-    context = viewlet.context
-    for iface in IPloneSiteRoot, IAddProject, IAmAPeopleFolder:
-        if iface.providedBy(context):
-            return True
-    return False
-
-def if_projects_selected(viewlet):
-    return (IAddProject.providedBy(viewlet.context)
-            and not viewlet.context.request.ACTUAL_URL.endswith('/create'))
-
-def openpage_provided(viewlet):
-    return IOpenPage.providedBy(viewlet.context)
-
-def default_css(viewlet):
-    return viewlet.selected() and 'oc-topnav-selected' or None
-
-def join_css(viewlet):
-    return viewlet.selected() and 'oc-topnav-selected' or 'oc-topnav-join'
-
-def not_part_of_project(viewlet):
-    proj = contained_within(viewlet)
-    if proj is None:
-        return False
-    mstool = getToolByName(viewlet.context, 'portal_membership')
-    if mstool.isAnonymousUser():
-        return True
-    mem = mstool.getAuthenticatedMember()
-    team = proj.getTeams()[0]
-    filter_states = tuple(team.getActiveStates()) + ('pending',)
-    if mem.getId() in team.getMemberIdsByStates(filter_states):
-        return False
-    return True
-
 
 class TopnavManager(ViewletManagerBase):
     """custom menu manager for opencore topnavigation"""
--- opencore/browser/topnav/viewlet.py
+++ opencore/browser/topnav/viewlet.py
@@ -1,5 +1,93 @@
+from opencore.interfaces import IOpenPage
+from opencore.interfaces.adding import IAddProject
+from opencore.interfaces.adding import IAmAPeopleFolder
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.Five.viewlet.viewlet import ViewletBase
+from topp.utils import zutils
 
 class BaseMenuItem(ViewletBase):
     render = ZopeTwoPageTemplateFile('menuitem.pt')
+
+def contained_within(viewlet):
+    return zutils.aq_iface(viewlet.context, viewlet.container)
+
+def nofilter(viewlet):
+    return True
+
+# all viewlet methods used for the menuitem registration
+# can be found below
+# these control the various permutations required
+# for displaying all menu items
+
+def contained_item_url(viewlet):
+    item = contained_within(viewlet)
+    if item is None:
+        url = viewlet.context.absolute_url()
+    else:
+        url = item.absolute_url()
+    return '%s/%s' % (url, viewlet.item_url)
+
+def portal_and_item_url(viewlet):
+    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
+    return '%s/%s' % (portal.absolute_url(), viewlet.item_url)
+
+def people_url(viewlet):
+    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
+    return '%s/people' % portal.absolute_url()
+
+def projects_url(viewlet):
+    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
+    return '%s/projects' % portal.absolute_url()
+
+def project_create_url(viewlet):
+    portal = getToolByName(viewlet.context, 'portal_url').getPortalObject()
+    return '%s/projects/create' % portal.absolute_url()
+
+def member_wiki_url(viewlet):
+    mstool = getToolByName(viewlet.context, 'portal_membership')
+    home_folder = mstool.getHomeFolder()
+    id_ = home_folder.getId()
+    return '%s/%s-home' % (home_folder.absolute_url(), id_)
+
+def project_wiki_url(viewlet):
+    proj = contained_within(viewlet)
+    return '%s/project-home' % proj.absolute_url()
+
+def if_request_starts_with_url(viewlet):
+    return viewlet.request.ACTUAL_URL.startswith(viewlet.url())
+
+def if_portal_people_or_projects(viewlet):
+    context = viewlet.context
+    for iface in IPloneSiteRoot, IAddProject, IAmAPeopleFolder:
+        if iface.providedBy(context):
+            return True
+    return False
+
+def if_projects_selected(viewlet):
+    return (IAddProject.providedBy(viewlet.context)
+            and not viewlet.context.request.ACTUAL_URL.endswith('/create'))
+
+def openpage_provided(viewlet):
+    return IOpenPage.providedBy(viewlet.context)
+
+def default_css(viewlet):
+    return viewlet.selected() and 'oc-topnav-selected' or None
+
+def join_css(viewlet):
+    return viewlet.selected() and 'oc-topnav-selected' or 'oc-topnav-join'
+
+def not_part_of_project(viewlet):
+    proj = contained_within(viewlet)
+    if proj is None:
+        return False
+    mstool = getToolByName(viewlet.context, 'portal_membership')
+    if mstool.isAnonymousUser():
+        return True
+    mem = mstool.getAuthenticatedMember()
+    team = proj.getTeams()[0]
+    filter_states = tuple(team.getActiveStates()) + ('pending',)
+    if mem.getId() in team.getMemberIdsByStates(filter_states):
+        return False
+    return True
