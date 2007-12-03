871daff1dbb4bfd2e6cc82658421d01a52ba2d21
Paul Winkler <slinkp@gmail.com>
2007-12-03 22:58:20 +0000

    don't blow up if form doesn't have a title; leave that up to validation.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11594 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/view.py
+++ opencore/project/browser/view.py
@@ -718,6 +718,8 @@ class SubProjectAddView(ProjectAddView):
 
 whitespace_pattern = re.compile('\s+')
 def strip_extra_whitespace(title):
+    if title is None:
+        return ''
     title = whitespace_pattern.sub(' ', title).strip()
     return title.strip()
 
