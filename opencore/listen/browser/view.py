43972493322b4bb4189a60f3ca27119b78b4a91c
Douglas Mayle <douglas@mayle.org>
2007-12-10 02:23:40 +0000

    View and template for list creation
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/listen-without-formlib@11737 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/browser/view.py
+++ opencore/listen/browser/view.py
@@ -1,27 +1,177 @@
-from Products.Five.browser import metaconfigure
-from Products.Five.browser import pagetemplatefile
 from Acquisition import aq_inner
-from zope.app.annotation.interfaces import IAnnotations
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import metaconfigure, pagetemplatefile
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.PageTemplates.PageTemplate import PageTemplate
-from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView
-from Products.listen.browser.mail_archive_views import ArchiveNewTopicView, SubFolderDateView
-from Products.listen.browser.mail_archive_views import ArchiveSearchView
-from Products.listen.browser.mail_message_views import ForumMailMessageView, ThreadedMailMessageView
-from Products.listen.browser.mail_message_views import MessageReplyView, SearchDebugView
+from Products.listen.browser.mail_archive_views import ArchiveForumView, ArchiveDateView, \
+                                                       ArchiveNewTopicView, SubFolderDateView, \
+                                                       ArchiveSearchView
+from Products.listen.browser.mail_message_views import ForumMailMessageView, ThreadedMailMessageView, \
+                                                       MessageReplyView, SearchDebugView
 from Products.listen.browser.manage_membership import ManageMembersView
 from Products.listen.browser.moderation import ModerationView
 from Products.listen.interfaces import IMailingList
+from Products.listen.interfaces.list_types import PublicListTypeDefinition, \
+                                                  PostModeratedListTypeDefinition, \
+                                                  MembershipModeratedListTypeDefinition
+from Products.listen.interfaces import IPublicList
+from Products.listen.interfaces import IMembershipModeratedList
+from Products.listen.interfaces import IPostModeratedList
 from Products.listen.utilities.list_lookup import ListLookupView
 from lxml.html.clean import Cleaner
-from opencore.browser.base import BaseView
+from opencore.browser.formhandler import OctopoLite, action
+from opencore.browser.base import BaseView, _
+from opencore.listen.mailinglist import OpenMailingList
 from opencore.listen.mailinglist_views import MailingListAddForm, MailingListEditForm, MailingListView
 from plone.app.form import _named
 from plone.memoize.view import memoize as req_memoize
+from zope.app.annotation.interfaces import IAnnotations
+from zope.app.component.hooks import getSite
 from zope.formlib.namedtemplate import INamedTemplate
 from zope.interface import implements
+from zExceptions import BadRequest
 import cgi
+import re
 import new
 import os.path
+
+class ListAddView(BaseView, OctopoLite):
+
+    template = ZopeTwoPageTemplateFile('create.pt')
+
+    @req_memoize
+    def list_title(self):
+        obj = self.context
+        while not IMailingList.providedBy(obj):
+            try:
+                obj = obj.aq_parent
+            except AttributeError:
+                return ''
+        return obj.Title()            
+        
+    def getSuffix(self):
+        """
+        Retrieves the FQDN that is the list address suffix for a site from
+        the opencore_properties PropertySheet.  Requires a context object
+        from inside the site so the properties tool can be retrieved.
+        """
+        # use threadlocal site to hook into acquisition context
+        site = getSite()
+        ptool = getToolByName(site, 'portal_properties')
+        ocprops = ptool._getOb('opencore_properties')
+        return '@' + str(ocprops.getProperty('mailing_list_fqdn').strip())
+
+    @action('validate')
+    def validate(self, target=None, fields=None):
+        putils = getToolByName(self.context, 'plone_utils')
+        errors = {}
+        mailto = self.request.form.get('mailto')
+        mailto = putils.normalizeString(mailto)
+        if (self.context.has_key(mailto)):
+            errors['oc-mailto-error'] = {
+                'html': 'The requested list prefix is already taken.',
+                'action': 'copy',
+                'effects': 'highlight'
+                }
+        else:
+            errors['oc-mailto-error'] = {
+                'html': '',
+                'action': 'copy',
+                'effects': ''
+                }
+        return errors
+
+
+    @action('add')
+    def handle_request(self, target=None, fields=None):
+        # Get the tool used to normalize strings
+        putils = getToolByName(self.context, 'plone_utils')
+
+        # Create an empty dictionary to hold any eventual errors.
+        self.errors = {}
+
+        # Let's do some form validation
+        # Get and clean up title from request
+        title = self.request.form.get('title', '')
+        title = re.compile('\s+').sub(' ', title).strip()
+        if not isinstance(title, unicode):
+            title = unicode(title, 'utf-8')
+        self.request.form['title'] = title
+
+        # Get and check the list policies
+        workflow = self.request.form.get('workflow_policy')
+        if workflow not in ('policy_open', 'policy_moderated', 'policy_closed'):
+            self.errors['workflow_policy'] = _(u'list_create_invalid_workflow_error', u'The mailing list security must be set to open, moderated, or closed.')
+
+        archive = self.request.form.get('archival_policy')
+        if archive not in ('policy_all', 'policy_text', 'policy_none'):
+            self.errors['archive'] = _(u'list_create_invalid_archive_error', u'The mailing list archival method must be set to all, text-only, or none.')
+
+        mailto = self.request.form.get('mailto')
+        if not re.match('[a-zA-Z][-\w]+', mailto):
+            self.errors['mailto'] = _(u'list_create_invalid_prefix_error', u'Only the following characters are allowed in list address prefixes: alpha-numerics, underscores, hyphens, and periods (i.e. A-Z, a-z, 0-9, and _-. symbols)')
+        else:
+            mailto = putils.normalizeString(mailto)
+            if hasattr(self.context, mailto):
+                self.errors['mailto'] = _(u'list_create_duplicate_error', u'The requested list prefix is already taken.')
+
+        # If we don't pass sanity checks by this point, abort and let the user correct their errors.
+        if self.errors:
+            self.add_status_message(_(u'psm_correct_errors_below', u'Please correct the errors indicated below.'))
+            return
+
+        # Try to create an OpenMember using the mailto address to see if it's going to be valid
+        lists_folder = self.context
+        try:
+            lists_folder.invokeFactory(OpenMailingList.portal_type, mailto)
+        except BadRequest:
+            self.errors['mailto'] = _(u'list_create_duplicate_error', u'The requested list prefix is already taken.')
+            self.add_status_message(_(u'psm_correct_errors_below', u'Please correct the errors indicated below.'))
+            return
+
+        list = lists_folder._getOb(mailto)
+
+        if self.errors:
+            self.add_status_message(_(u'psm_correct_errors_below', u'Please correct the errors indicated below.'))
+            return 
+
+        list.managers = (unicode(self.loggedinmember.getId()),)
+        list.setDescription(unicode(self.request.form.get('description','')))
+
+        if workflow == 'policy_open':
+            list.list_type = PublicListTypeDefinition
+            if not IPublicList.providedBy(list):
+                list.setDescription(u'PublicListTypeDefinition  error')
+            list.setDescription(u'PublicListTypeDefinition')
+        elif workflow == 'policy_moderated':
+            list.list_type = PostModeratedListTypeDefinition
+            if not IMembershipModeratedList.providedBy(list):
+                list.setDescription(u'PostModeratedListTypeDefinition  error')
+            list.setDescription(u'PostModeratedListTypeDefinition')
+        else:
+            list.list_type = MembershipModeratedListTypeDefinition
+            if not IPostModeratedList.providedBy(list):
+                list.setDescription(u'MembershipModeratedListTypeDefinition  error')
+            list.setDescription(u'MembershipModeratedListTypeDefinition')
+
+        if archive == 'policy_all':
+            list.archived = 0
+        elif archive == 'policy_text':
+            list.archived = 1
+        else:
+            list.archived = 2
+
+        self.template = None
+
+        s_message_mapping = {'title': title}
+        s_message = _(u'list_created',
+                      u'"${title}" has been created.',
+                      mapping=s_message_mapping)
+        
+        self.add_status_message(s_message)
+
+        self.redirect(list.absolute_url())
+
 
 class ListenBaseView(BaseView):
     @req_memoize
