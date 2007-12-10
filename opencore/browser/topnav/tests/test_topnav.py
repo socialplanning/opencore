08893fe7590a62f6ebb4c6ff01fcfea8c1f9aaef
Rob Marianski <rob@marianski.com>
2007-12-10 03:19:55 +0000

    update more tests for viewlets
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/viewlet_menuitems@11740 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/browser/topnav/tests/test_topnav.py
+++ opencore/browser/topnav/tests/test_topnav.py
@@ -6,6 +6,7 @@ from zope.component import getMultiAdapter
 
 from Products.CMFCore.utils import getToolByName
 
+from opencore.browser.topnav.tests import parse_topnav_context_menu
 from opencore.testing.layer import OpencoreContent
 from opencore.interfaces.member import IMemberFolder
 
@@ -23,14 +24,31 @@ class TestTopNav(OpenPlansTestCase):
     def test_contextmenu(self):
         req = self.request
         topnav = getMultiAdapter((self.portal, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-default-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(3, len(lis))
+        for li in lis:
+            self.assertEqual(False, li['selected'])
+        people, projects, start = [l['name'] for l in links]
+        self.assertEqual(people, u'People')
+        self.assertEqual(projects, u'Projects')
+        self.assertEqual(start, u'Start A Project')
 
         self.clearMemoCache()
         proj = self.portal.projects.p1
         topnav = getMultiAdapter((proj, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-project-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(5, len(lis))
+        for li in lis[:4]:
+            self.assertEqual(False, li['selected'])
+        self.assertEqual(u'oc-topnav-join', lis[4]['selected'])
+        wiki, lists, team, contents, join = [l['name'] for l in links]
+        self.assertEqual(wiki, u'Wiki')
+        self.assertEqual(lists, u'Mailing Lists')
+        self.assertEqual(team, u'Team')
+        self.assertEqual(contents, u'Contents')
+        self.assertEqual(join, u'Join Project')
 
         self.clearMemoCache()
         mtool = getToolByName(self.portal, 'portal_membership')
@@ -38,16 +56,31 @@ class TestTopNav(OpenPlansTestCase):
         memhome = self.portal.people.m1
         alsoProvides(memhome, IMemberFolder)
         topnav = getMultiAdapter((memhome, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-member-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(2, len(lis))
+        self.assertEqual(False, lis[0]['selected'])
+        self.assertEqual(False, lis[1]['selected'])
+        self.assertEqual(u'Wiki', links[0]['name'])
+        self.assertEqual(u'Profile', links[1]['name'])
 
         # test switch to project context with
         # X-OpenPlans-Project header set
         self.clearMemoCache()
         req.environ['X_OPENPLANS_PROJECT'] = 'p1'
         topnav = getMultiAdapter((self.portal, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-project-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(5, len(lis))
+        for li in lis[:4]:
+            self.assertEqual(False, li['selected'])
+        self.assertEqual(u'oc-topnav-join', lis[4]['selected'])
+        wiki, lists, team, contents, join = [l['name'] for l in links]
+        self.assertEqual(wiki, u'Wiki')
+        self.assertEqual(lists, u'Mailing Lists')
+        self.assertEqual(team, u'Team')
+        self.assertEqual(contents, u'Contents')
+        self.assertEqual(join, u'Join Project')
         del req.environ['X_OPENPLANS_PROJECT']
 
         # test switch to member context with
@@ -55,8 +88,13 @@ class TestTopNav(OpenPlansTestCase):
         self.clearMemoCache()
         req.environ['X_OPENPLANS_PERSON'] = 'm1'
         topnav = getMultiAdapter((self.portal, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-member-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(2, len(lis))
+        self.assertEqual(False, lis[0]['selected'])
+        self.assertEqual(False, lis[1]['selected'])
+        self.assertEqual(u'Wiki', links[0]['name'])
+        self.assertEqual(u'Profile', links[1]['name'])
         del req.environ['X_OPENPLANS_PERSON']
 
         # test X-OpenPlans-Person overrides
@@ -65,8 +103,13 @@ class TestTopNav(OpenPlansTestCase):
         req.environ['X_OPENPLANS_PROJECT'] = 'p1'
         req.environ['X_OPENPLANS_PERSON'] = 'm1'
         topnav = getMultiAdapter((self.portal, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-member-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(2, len(lis))
+        self.assertEqual(False, lis[0]['selected'])
+        self.assertEqual(False, lis[1]['selected'])
+        self.assertEqual(u'Wiki', links[0]['name'])
+        self.assertEqual(u'Profile', links[1]['name'])
         del req.environ['X_OPENPLANS_PERSON']
         del req.environ['X_OPENPLANS_PROJECT']
 
@@ -76,13 +119,19 @@ class TestTopNav(OpenPlansTestCase):
         req.environ['X_OPENPLANS_PROJECT'] = 'GlUrT'
         req.environ['X_OPENPLANS_PERSON'] = 'PhL00m'
         topnav = getMultiAdapter((self.portal, req), name='oc-topnav')
-        self.assertEqual(topnav.contextmenu.name,
-                         'topnav-default-menu')
+        html = topnav.contextmenu
+        lis, links = parse_topnav_context_menu(html)
+        self.assertEqual(3, len(lis))
+        for li in lis:
+            self.assertEqual(False, li['selected'])
+        people, projects, start = [l['name'] for l in links]
+        self.assertEqual(people, u'People')
+        self.assertEqual(projects, u'Projects')
+        self.assertEqual(start, u'Start A Project')
         del req.environ['X_OPENPLANS_PERSON']
         del req.environ['X_OPENPLANS_PROJECT']
 
 
-        
     def test_usermenu(self):
         req = self.request
         self.login('m1')
