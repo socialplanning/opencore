af0e63dad2421959d10a4399e13c585a5338712c
Rob Marianski <rob@marianski.com>
2007-12-10 04:03:05 +0000

    remove leftover menu item featurelet code (only need to specify configuration)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/viewlet_menuitems@11744 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/browser/topnav_menuitem.py
+++
@@ -1,7 +0,0 @@
-from opencore.browser.topnav.topnav_menuitem import BaseFeatureletMenuItem
-from opencore.featurelets.interfaces import IListenFeatureletInstalled
-
-class ListenMenuItem(BaseFeatureletMenuItem):
-    name = u'Mailing Lists'
-    supp_must_provide = IListenFeatureletInstalled
-    item_url = 'lists'
