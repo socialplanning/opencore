5f4b59b129e0c8725094129b74e091aeb22d52c1
Rob Miller <rob@kalistra.com>
2007-12-12 04:21:11 +0000

    remove all vestigial Fate-related code
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/plone3@11828 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/__init__.py
+++ opencore/project/browser/__init__.py
@@ -1,104 +1,12 @@
-#import Products.OpenPlans.content.project as project
 from Acquisition import aq_parent
 from Products.CMFCore.utils import getToolByName
-from Products.Fate.meta.view import AddView as BaseAddView
-from Products.Five.traversable import FiveTraversable
 from Products.Five.traversable import Traversable
 from memojito import memoizedproperty, memoize
 from opencore import redirect
-from opencore.interfaces import IProject
-from opencore.interfaces.adding import IAddSubProject, IAddProject 
-from opencore.interfaces.event import AfterProjectAddedEvent
-from opencore.interfaces.event import AfterSubProjectAddedEvent
-from opencore.project import PROJ_HOME
+from opencore.interfaces.adding import IAddSubProject
 from projectlisting import ProjectListingView
-from zope import event
-from zope.app.traversing.interfaces import ITraversable
-from zope.component import adapts
-from zope.interface import implements, Interface
-import cgi
-import os.path
+from zope.interface import implements
 
-# the home page id is used in a lot of places, put it higher in the
-# package path to prevent circular imports
-HOME_PAGE_ID = PROJ_HOME
-HOME_PAGE_TITLE = 'Project Home'
-HOME_PAGE_FILE = 'project_index.html'
-
-
-# == project add views == #
-
-class ProjectAddView(BaseAddView):
-    """
-    Perform initialization after creation.
-    """
-    def __init__(self, context, request):
-        """
-        Hide the tabs.
-        """
-        self.context = context
-        self.request = request
-        request.set('disable_border', 1)
-
-    @memoize
-    def projects_container(self):
-        """climb acquisition and find the proper container for project
-        adding"""
-        obj = self
-        while obj is not None and not IAddProject.providedBy(obj):
-            obj = aq_parent(obj)
-        return obj
-
-    @memoize
-    def instance(self):
-        projects = self.projects_container() 
-        name = projects.invokeFactory(self._portal_type, self._content_name)
-        if name is None:
-            name = self._content_name
-        return projects._getOb(name)
-
-    def createAndAdd(self):
-        """
-        Perform some post-creation initialization.
-        """
-        self.request.set('__initialize_project__', True)
-        instance = self.instance()
-        event.notify(AfterProjectAddedEvent(instance, self.request))
-        return instance
-
-    def redirect(self):
-        """
-        Redirect to login page if not logged in.
-        """
-        mtool = getToolByName(self.context, 'portal_membership')
-        final_url = "%s/do_add_project" % self.context.absolute_url()
-        if mtool.isAnonymousUser():
-            final_url = cgi.urllib.quote(final_url)
-            portal_url = getToolByName(self.context, 'portal_url')()
-            redirect = "%s/login_form?came_from=%s" % (portal_url,
-                                                       final_url)
-            self.request.response.redirect(redirect)
-        else:
-            self.request.response.redirect(final_url)
-
-class SubProjectAddView(ProjectAddView):
-    """add view for creating projects as subprojects of themed areas
-    """
-    
-    def __init__(self, view, request):
-        self.context = view.context
-        self.request = request
-        self.context_view = view
-
-
-    def createAndAdd(self):
-        instance = self.instance()
-        event.notify(AfterSubProjectAddedEvent(instance, self.context, 
-                                               self.request))
-        return instance 
-
-
-# == other views == #
 
 class SubProjectListingView(ProjectListingView, Traversable): 
     """mandatory docstring
@@ -130,9 +38,7 @@ class SubProjectListingView(ProjectListingView, Traversable):
     @memoize
     def blocked_content(self):
         name = self.__name__
-        parent = self.aq_parent
+        parent = aq_parent(self)
         if name in parent.objectIds():
             return getattr(parent, name)
         return False
-
-
