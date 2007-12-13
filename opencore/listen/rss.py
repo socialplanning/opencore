092bbd15c23954b294eb9c20ed659e013e94f028
Rob Marianski <rob@marianski.com>
2007-12-13 08:44:00 +0000

    add rss for lists folder across all mailing lists underneath it
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/massrss@11879 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/listen/rss.py
@@ -0,0 +1,73 @@
+from Products.CMFCore.utils import getToolByName
+from Products.listen.interfaces import ISearchableArchive
+from opencore.featurelets.interfaces import IListenContainer
+from opencore.rss.base import BaseFeedAdapter
+from opencore.rss.interfaces import IFeedData
+from opencore.rss.interfaces import IFeedItem
+from zope.component import adapts
+from zope.component import getUtility
+from zope.interface import alsoProvides
+from zope.interface import implements
+
+class ListsFeedAdapter(BaseFeedAdapter):
+    implements(IFeedData)
+    adapts(IListenContainer)
+
+    @property
+    def items(self):
+        """we aggregate the 10 most recent messages across all mailing lists,
+           then sort those by date and take the top 10
+
+           XXX this is potentially an expensive operation
+           we may want a separate catalog on the lists level
+           or store the mail messages in the main portal catalog as well
+           in which case we would be able to search for messages
+           in the site wide search"""
+
+        MSG_BODY_LENGTH = 300
+
+        brains = []
+        for ml_id in self.context.objectIds():
+            sa = getUtility(
+                ISearchableArchive,
+                context=self.context._getOb(ml_id))
+            ml_brains = sa.searchResults(sort_on='modification_date',
+                                         sort_order='descending',
+                                         sort_limit=10)
+            brains.extend(ml_brains)
+
+        brains = sorted(brains,
+                        key=lambda x:x.modification_date,
+                        reverse=True)
+        brains = brains[:10]
+
+        for brain in brains:
+            #XXX
+            # we get the object for now to see the body of the message
+            # we can either add it to the metadata, or forget about it
+            # or maybe something more clever?
+            msg = brain.getObject()
+            
+            title = brain.subject
+            # only put the first part of the body of long messages
+            body = msg.body
+            if len(body) > MSG_BODY_LENGTH:
+                body = '%s ...' % body[:MSG_BODY_LENGTH-4]
+            description = body
+            link = brain.getURL()
+            pubDate = brain.modification_date
+
+            # returning a dictionary would also work
+            # this is just more formal
+            feed_item = type('ProjectFeedItem',
+                             (object,),
+                             dict(title=title,
+                                  description=description,
+                                  link=link,
+                                  pubDate=pubDate,
+                                  ))
+
+            # to be technically correct
+            alsoProvides(feed_item, IFeedItem)
+            
+            yield feed_item
