438a88b559ebc04e3faf77b0f71e53032150eeaf
Rob Miller <rob@kalistra.com>
2007-12-14 22:51:19 +0000

    need to enumerate all of the args since they're sometimes used positionally
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11919 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/utility/http.py
+++ opencore/utility/http.py
@@ -1,4 +1,5 @@
 from httplib2 import Http
+from httplib2 import DEFAULT_MAX_REDIRECTS
 from opencore.utility.interfaces import IHTTPClient
 from zope.interface import implements
 
@@ -9,12 +10,12 @@ class HTTPClient(Http):
     """
     implements(IHTTPClient)
     
-    def request(self, uri, **kw):
+    def request(self, uri, method="GET", body=None, headers={},
+                redirections=DEFAULT_MAX_REDIRECTS, connection_type=None):
         """
         Inject Connection=close into the HTTP request headers.
         """
-        if not kw.has_key('headers'):
-            kw['headers'] = {'Connection': 'close'}
-        else:
-            kw['headers']['Connection'] = 'close'
-        return Http.request(self, uri, **kw)
+        if not headers.has_key('Connection'):
+            headers['Connection'] = 'close'
+        return Http.request(self, uri, method, body, headers, redirections,
+                            connection_type)
