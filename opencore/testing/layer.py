fee56f3d9b01c02f714c77ce71b598364f5feabe
Rob Miller <rob@kalistra.com>
2007-12-15 06:26:51 +0000

    register the MailHostMock as a utility to account for changes in getToolByName
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/plone3@11932 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/testing/layer.py
+++ opencore/testing/layer.py
@@ -1,11 +1,12 @@
 from Products.CMFCore.utils  import getToolByName
 from Testing import ZopeTestCase
-#from Products.Five.site.localsite import enableLocalSiteHook
 from Products.PloneTestCase.layer import PloneSite, ZCML
 from Products.PloneTestCase.setup import setupPloneSite
 from Testing import ZopeTestCase
 from opencore.project.handler import add_redirection_hooks 
 from opencore.testing.utility import setup_mock_http
+from opencore.testing.utility import setup_mock_mailhost
+from opencore.testing.utility import teardown_mock_mailhost
 from opencore.utils import set_opencore_properties
 from topp.utils import introspection
 from topp.utils.testing import layer_factory
@@ -16,24 +17,6 @@ import random
 import transaction as txn
 from opencore.configuration.setuphandlers import Z_DEPS, DEPS
 from five.localsitemanager import make_objectmanager_site
-
-class MailHostMock(object):
-    """
-    mock up the send method so that emails do not actually get sent
-    during automated tests
-    """
-    def __init__(self):
-        self.messages = []
-    def send(self, msg, mto=None, mfrom=None, subject=None):
-        msg = {'msg': msg,
-               'mto': mto,
-               'mfrom': mfrom,
-               'subject': subject,
-               }
-        self.messages.append(msg)
-    secureSend = send
-    def validateSingleEmailAddress(self, email):
-        return True
 
 
 class BrowserIdManagerMock(object):
@@ -79,8 +62,7 @@ class OpenPlansLayer(Install, PloneSite):
         portal.clearCurrentSkin()
         portal.setupCurrentSkin(portal.REQUEST)
 
-        portal.oldMailHost = portal.MailHost
-        portal.MailHost = MailHostMock()
+        setup_mock_mailhost(portal)
 
         portal.browser_id_manager = BrowserIdManagerMock()
         txn.commit()
@@ -88,16 +70,17 @@ class OpenPlansLayer(Install, PloneSite):
     @classmethod
     def tearDown(cls):
         portal = get_portal_as_owner()
-        del portal.MailHost
-        portal.MailHost = portal.oldMailHost
-        del portal.oldMailHost
+        teardown_mock_mailhost(portal)
         del portal.browser_id_manager
-
 
 class OpencoreContent(OpenPlansLayer):
     @classmethod
     def setUp(cls):
         portal = get_portal_as_owner()
+
+        portal.clearCurrentSkin()
+        portal.setupCurrentSkin(portal.REQUEST)
+
         create_test_content(portal)
         add_redirection_hooks(portal.projects)
         txn.commit()
--- opencore/testing/utility.py
+++ opencore/testing/utility.py
@@ -2,6 +2,26 @@ from minimock import Mock, HTTPMock
 from opencore.testing import alsoProvides
 from zope.component import provideUtility
 from opencore.utility.interfaces import IHTTPClient
+from Products.MailHost.interfaces import IMailHost
+
+class MailHostMock(object):
+    """
+    mock up the send method so that emails do not actually get sent
+    during automated tests
+    """
+    def __init__(self, name):
+        self.messages = []
+        self.name = name
+    def send(self, msg, mto=None, mfrom=None, subject=None):
+        msg = {'msg': msg,
+               'mto': mto,
+               'mfrom': mfrom,
+               'subject': subject,
+               }
+        self.messages.append(msg)
+    secureSend = send
+    def validateSingleEmailAddress(self, email):
+        return True
 
 def mock_utility(name, provides, cls=Mock):
     """
@@ -15,3 +35,16 @@ def mock_utility(name, provides, cls=Mock):
 def setup_mock_http():
     http = mock_utility('httplib2.Http', IHTTPClient, cls=HTTPMock)
     provideUtility(http, provides=IHTTPClient)
+
+def setup_mock_mailhost(portal):
+    portal._oldMailHost = portal.MailHost
+    mailhost = mock_utility('Products.SecureMailHost.SecureMailHost.SecureMailHost',
+                            IMailHost, cls=MailHostMock)
+    portal.MailHost = mailhost
+    sm = portal.getSiteManager()
+    sm.registerUtility(mailhost, provided=IMailHost)
+
+def teardown_mock_mailhost(portal):
+    del portal.MailHost
+    portal.MailHost = portal._oldMailHost
+    del portal._oldMailHost
