c191f8c2795879ed4c7374d076297ea8d173797b
Chris Abraham <cabraham@openplans.org>
2007-12-17 21:19:19 +0000

    merged stats branch -r9357:9406 to trunk
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@11937 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/stats/stats.py
@@ -0,0 +1,72 @@
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+import DateTime
+from Products.listen.interfaces import ISearchableArchive
+from zope.component import queryUtility
+
+
+class StatsView(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.catalog = getToolByName(self.context, 'portal_catalog')
+        self.membrane_tool = getToolByName(self.context, 'membrane_tool')
+        self.expiry_date = DateTime.now()-30
+
+    def get_projects(self):
+        query = dict(portal_type='OpenProject')
+        brains = self.catalog(**query)
+        return brains
+
+    def get_active_projects(self):    
+        # "active" is defined as having been modified in the last 30 days
+        projects = self.get_projects()
+        filtered_projects = [project for project in projects if project.modified > self.expiry_date]
+        return filtered_projects
+
+    def get_members(self):
+        query = dict()
+        brains = self.membrane_tool(**query)
+        return brains
+
+    def get_active_members(self):    
+        # "active" is defined as having logged in the last 30 days
+        members = self.get_members()
+        filtered_members = []
+        for mem in members:
+            mem_obj = mem.getObject()
+            if mem_obj.getLast_login_time() > self.expiry_date:
+                filtered_members.append(mem)
+        return filtered_members
+
+    def get_mailing_lists(self):
+        query = dict(portal_type='Open Mailing List')
+        brains = self.catalog(**query)
+        return brains
+
+    def get_active_mailing_lists(self):    
+        # "active" is defined as having a message in the last 30 days
+        lists = self.get_mailing_lists()
+        filtered_lists = []
+        for lst in lists:
+            mail_catalog = queryUtility(ISearchableArchive, context=lst)
+            latest_date = 0
+            if mail_catalog:
+                query = dict(sort_on='date',
+                             sort_order='descending')
+                brains = mail_catalog(**query)
+                latest_date = brains[0].date
+            if lst.modified > latest_date:
+                latest_date = lst.modified
+            if latest_date > self.expiry_date:
+                filtered_lists.append(lst)
+        
+        return filtered_lists
+
+    
+
+
+        
