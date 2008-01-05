979dea1a057700ebe0b8911d45ca7e9f94cbb98d
Rob Marianski <rob@marianski.com>
2008-01-05 23:09:53 +0000

    rss for wiki pages (show page history)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/massrss@12317 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/rss/page.py
@@ -0,0 +1,38 @@
+from Products.CMFCore.utils import getToolByName
+from opencore.interfaces import IOpenPage
+from opencore.rss.base import BaseFeedAdapter
+from opencore.rss.interfaces import IFeedData
+from opencore.rss.interfaces import IFeedItem
+from zope.component import adapts
+from zope.interface import alsoProvides
+from zope.interface import implements
+
+class PageFeedAdapter(BaseFeedAdapter):
+    """rss for wiki page modifications"""
+    
+    implements(IFeedData)
+    adapts(IOpenPage)
+
+    @property
+    def items(self):
+        pr = getToolByName(self.context, 'portal_repository')
+        for version in pr.getHistory(self.context, countPurged=False):
+            description = version.comment
+            page = version.object
+            title = page.Title()
+            link = page.absolute_url()
+            pubDate = page.modified()
+            #body = page.getText()
+            feed_item = type('PageFeedItem',
+                             (object,),
+                             dict(title=title,
+                                  description=description,
+                                  link=link,
+                                  pubDate=pubDate,
+                                  #body=body,
+                                  ))
+
+            # to be technically correct
+            alsoProvides(feed_item, IFeedItem)
+            
+            yield feed_item
