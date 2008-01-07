ecbad6b81b6ac1d398ac2ec25f704349b5267694
Rob Marianski <rob@marianski.com>
2008-01-07 01:42:15 +0000

    separate projects/project adapters into separate files
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/massrss@12321 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/rss/project.py
+++ opencore/rss/project.py
@@ -8,41 +8,6 @@ from zope.component import adapts
 from zope.interface import alsoProvides
 from zope.interface import implements
 
-class ProjectsFeedAdapter(BaseFeedAdapter):
-    """feed for new projects"""
-    
-    implements(IFeedData)
-    adapts(IAddProject)
-
-    @property
-    def items(self):
-        cat = getToolByName(self.context, 'portal_catalog')
-        #XXX put in max depth 1 to not search subfolders
-        for brain in cat(portal_type='OpenProject',
-                              sort_on='created',
-                              sort_order='descending',
-                              sort_limit=10):
-
-            title = brain.Title
-            description = brain.Description
-            link = brain.getURL()
-            pubDate = brain.created
-
-            # returning a dictionary would also work
-            # this is just more formal
-            feed_item = type('ProjectFeedItem',
-                             (object,),
-                             dict(title=title,
-                                  description=description,
-                                  link=link,
-                                  pubDate=pubDate,
-                                  ))
-
-            # to be technically correct
-            alsoProvides(feed_item, IFeedItem)
-            
-            yield feed_item
-
 class ProjectFeedAdapter(BaseFeedAdapter):
     """rss for wiki page modifications in project
        XXX or should this be only for new pages?
---
+++ opencore/rss/projects.py
@@ -0,0 +1,44 @@
+from Products.CMFCore.utils import getToolByName
+from opencore.interfaces.adding import IAddProject
+from opencore.interfaces import IProject
+from opencore.rss.base import BaseFeedAdapter
+from opencore.rss.interfaces import IFeedData
+from opencore.rss.interfaces import IFeedItem
+from zope.component import adapts
+from zope.interface import alsoProvides
+from zope.interface import implements
+
+class ProjectsFeedAdapter(BaseFeedAdapter):
+    """feed for new projects"""
+    
+    implements(IFeedData)
+    adapts(IAddProject)
+
+    @property
+    def items(self):
+        cat = getToolByName(self.context, 'portal_catalog')
+        #XXX put in max depth 1 to not search subfolders
+        for brain in cat(portal_type='OpenProject',
+                              sort_on='created',
+                              sort_order='descending',
+                              sort_limit=10):
+
+            title = brain.Title
+            description = brain.Description
+            link = brain.getURL()
+            pubDate = brain.created
+
+            # returning a dictionary would also work
+            # this is just more formal
+            feed_item = type('ProjectFeedItem',
+                             (object,),
+                             dict(title=title,
+                                  description=description,
+                                  link=link,
+                                  pubDate=pubDate,
+                                  ))
+
+            # to be technically correct
+            alsoProvides(feed_item, IFeedItem)
+            
+            yield feed_item
