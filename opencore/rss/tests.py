27ced1b976638f47f4cac56b422f3afa129f7f98
Rob Marianski <rob@marianski.com>
2008-01-07 01:56:17 +0000

    add tests for project rss
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/massrss@12322 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/rss/tests.py
+++ opencore/rss/tests.py
@@ -8,6 +8,8 @@ class DummyContext(object):
         return 'Dummy description'
     def absolute_url(self):
         return 'http://dummy/context/url'
+    def getPhysicalPath(self):
+        return ['dummy', 'context', 'url']
     def modified(self):
         from datetime import datetime
         return datetime.now()
@@ -21,7 +23,13 @@ def test_suite():
                                         optionflags=doctest.ELLIPSIS)
     projects_suite = doctest.DocFileSuite('projects.txt',
                                           optionflags=doctest.ELLIPSIS)
-    return unittest.TestSuite((base_suite,people_suite, projects_suite))
+    project_suite = doctest.DocFileSuite('project.txt',
+                                         optionflags=doctest.ELLIPSIS)
+    return unittest.TestSuite((base_suite,
+                               people_suite,
+                               projects_suite,
+                               project_suite,
+                               ))
 
 
 if __name__ == '__main__':
