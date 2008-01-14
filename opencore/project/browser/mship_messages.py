f711a55f6815cef91e88ded2cf2b1633dfae63c6
Chris Abraham <cabraham@openplans.org>
2008-01-14 16:05:04 +0000

    fixed reminders sent to member invites
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/new-team-manage-2@12633 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/mship_messages.py
+++ opencore/project/browser/mship_messages.py
@@ -1,8 +1,22 @@
 from opencore.i18n import _
 
-invite_member = _(u'email_invite_member')
+invite_member = _(u'email_invite_member', u"""Subject: You've been invited to join "${project_title}"
 
-remind_invitee = _(u'email_remind_invitee')
+Hi ${user_name},
+
+You have been invited to join "${project_title}" by ${inviter_name}.
+Please visit your account page to accept or decline the invitation: ${account_url}
+
+Cheers,
+The ${portal_name} Team
+${portal_url}
+""")
+
+remind_invitee = _(u'email_remind_invitee', u"""Subject: ${portal_title} - Invitation to join ${project_title}
+
+This is a reminder that you've been invited to join "${project_title}" on ${portal_title}.
+Please visit your account page to accept or decline the invitation: ${account_url}
+""")
 
 request_approved = _(u'email_request_approved', u"""Subject: ${portal_title} - Welcome to ${project_title}
 
