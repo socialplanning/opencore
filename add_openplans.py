3ffc284c1bc9428c155469ebdcc89f0e678014a3
Joshua Bronson <jabronson@gmail.com>
2008-02-08 22:23:14 +0000

    oops again
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@13269 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- add_openplans.py
+++ add_openplans.py
@@ -5,11 +5,10 @@ import transaction
 from Testing.makerequest import makerequest
 app=makerequest(app)
 
-
 admin_file = get_config('general', 'admin_info_filename', default='admin')
 admin_id = open(admin_file).read().split(':')[0]
 site_id = get_config('general', 'opencore_site_id', default='openplans')
-site_id = get_config('general', 'opencore_site_title', default='OpenCore Site')
+site_title = get_config('general', 'opencore_site_title', default='OpenCore Site')
 
 if not site_id in app.objectIds():
     user = app.acl_users.getUser(admin_id)
