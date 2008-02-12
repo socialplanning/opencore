f898646d7213ad4a4ce8bfc2b8447101181b5a7e
Whit Morriss <d.w.morriss@gmail.com>
2008-02-12 22:36:00 +0000

    adding a preliminary migration for history caching
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/0.9.7.7@13417 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ migrations/cache_version_history.py
@@ -0,0 +1,51 @@
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SpecialUsers import system
+from opencore.nui.wiki.interfaces import IWikiHistory
+from Products.CMFEditions.interfaces.IArchivist import ArchivistRetrieveError
+from opencore.interfaces import IOpenPage
+from DateTime.DateTime import DateTime
+
+import sys
+import transaction as txn
+
+newSecurityManager(None, system)
+
+try:
+    portal = sys.argv[1]
+except IndexError:
+    portal = 'openplans'
+
+portal = getattr(app, portal)
+pc = portal.portal_catalog
+pr = portal.portal_repository
+
+def cache_history(page):
+    if not IOpenPage.providedBy(page):
+        return
+    
+    try:
+        history = pr.getHistory(page, countPurged=False)
+    except ArchivistRetrieveError, e:
+        print e
+        return
+
+    history_cache = IWikiHistory(page)
+
+    # clear any old history ui info
+    history_cache.annot.clear()
+
+    for vd in history:
+        new_history_item = dict(
+            version_id=vd.version_id,
+            comment=vd.comment,
+            author=vd.sys_metadata['principal'],
+            modification_date=DateTime(vd.sys_metadata['timestamp']),
+            )
+        history_cache.annot[vd.version_id] = new_history_item        
+    txn.commit()
+    
+for brain in pc(portal_type="Document"):
+    app._p_jar.sync()
+    page = brain.getObject()
+    cache_history(page)
+
