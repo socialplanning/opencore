586b545413e42944e501c38ebabd44569d2c1675
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-02-26 21:55:16 +0000

    don't use @property for expensive functions
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@13755 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/contexthijack.py
+++ opencore/nui/contexthijack.py
@@ -26,17 +26,15 @@ class HeaderHijackable(BaseView):
     def _get_context(self):
         if self.request.get_header('HTTP_X_OPENPLANS_APPLICATION') == 'zope':
             return self.original_context
-        return self.context_from_headers or self.original_context
+        return self.context_from_headers() or self.original_context
     
     def _set_context(self, ctx):
         self.original_context = ctx
     context = property(_get_context, _set_context)
 
-    @property
     def context_from_headers(self):
-        return self.person_folder_from_headers or self.project_from_headers
+        return self.person_folder_from_headers() or self.project_from_headers()
 
-    @property
     def project_from_headers(self):
         name = self.request.get_header(OP_PROJECT_HEADER)
         if not name:
@@ -46,7 +44,6 @@ class HeaderHijackable(BaseView):
                                'portal_url').getPortalObject()
         return getattr(portal.projects, name, None)
 
-    @property
     def person_folder_from_headers(self):
         name = self.request.get_header(OP_PERSON_HEADER)
 
