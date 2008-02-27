d00d368d523a243ecee0dc0f84ac6bf67c1aba24
Rob Marianski <rob@marianski.com>
2008-02-27 22:41:09 +0000

    move project preferences psm's into separate translation file
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@13802 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/preferences.py
+++ opencore/project/browser/preferences.py
@@ -147,11 +147,11 @@ class ProjectPreferencesView(ProjectBaseView, OctopoLite):
 
         #store change status of flet, security, title, description, logo...
         changed = {
-            _(u'psm_project_title_changed', u"The title has been changed.") : self.context.title != self.request.form.get('project_title', self.context.title),
-            _(u'psm_project_desc_changed', u"The description has been changed.") : self.context.Description() != self.request.form.get('description', self.context.description),
-            _(u'psm_project_logo_changed', u"The project image has been changed.") : logochanged,
-            _(u'psm_security_changed', u"The security policy has been changed.") : old_workflow_policy != self.request.form.get('workflow_policy'),
-            _(u'psm_location_changed', u"The location has been changed."): bool(locationchanged),
+            _(u'psm_project_title_changed') : self.context.title != self.request.form.get('project_title', self.context.title),
+            _(u'psm_project_desc_changed') : self.context.Description() != self.request.form.get('description', self.context.description),
+            _(u'psm_project_logo_changed') : logochanged,
+            _(u'psm_security_changed') : old_workflow_policy != self.request.form.get('workflow_policy'),
+            _(u'psm_location_changed'): bool(locationchanged),
             }
         
         supporter = IFeatureletSupporter(self.context)
