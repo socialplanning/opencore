a6337f3e6234b2165ade1eefa17a9cf05eedecf0
Joshua Bronson <jabronson@gmail.com>
2008-03-03 20:59:55 +0000

    add cache_version_history migration
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@13930 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- migrations/migrate-woonerf.py
+++ migrations/migrate-woonerf.py
@@ -1,13 +1,18 @@
-username = 'admin'
 from AccessControl.SecurityManagement import newSecurityManager
+from Testing.makerequest import makerequest
+from itertools import count
+from opencore.nui.setup import set_method_aliases
+from opencore.nui.wiki import utils
+from opencore.streetswiki.utils import add_wiki
+from pprint import pprint 
+import sys
+import transaction
+
+username = 'admin'
 user = app.acl_users.getUser(username)
 user = user.__of__(app.acl_users)
 newSecurityManager(app, user)
-from Testing.makerequest import makerequest
 app=makerequest(app)
-import transaction
-from opencore.streetswiki.utils import add_wiki
-from opencore.nui.setup import set_method_aliases
 
 def print_status_messages(import_result_dict):
     print "Import status messages:"
@@ -94,6 +99,65 @@ print "Installing portal_geocoder"
 qi = portal.portal_quickinstaller
 qi.installProducts(['PleiadesGeocoder'])
 print "done"
+
+
+## from cache_version_history.py
+pc = portal.portal_catalog
+pr = portal.portal_repository
+path = '/'.join(portal.getPhysicalPath() + ('projects',))
+brains = pc(portal_type="Document", path=path)
+
+print "\nattempting to migrate %s @ %s" %(len(brains), path)
+
+counter = count()
+skipcounter = count()
+_ghosts = []
+entries = 0
+
+try:
+    for brain in brains:
+        try:
+            page = brain.getObject()
+        except AttributeError:
+            # kill ghost
+            skipcounter.next()
+            _ghosts.append(brain.getPath())
+            pc.uncatalog_object(brain.getPath())
+            # transaction.commit()
+            continue
+        
+        page._p_jar.sync()
+        if getattr(page, '__HISTORY_MIGRATED__', False):
+            skipcounter.next()
+            continue
+
+        result = utils.cache_history(page, pr)
+        mcount = scount = 0
+        
+        if result:
+            # transaction.commit()
+            mcount = counter.next()
+            entries += result
+        elif result is False:
+            scount = skipcounter.next()
+        elif result == 0:
+            print "no history: %s" %brain.getPath()
+            
+        i = mcount + scount
+        if i and not i % 100:
+            print "%s docs migrated" %i
+            
+except KeyboardInterrupt, e:
+    print e
+
+print
+print "Total pages migrated: % 3s" %(counter.next() - 1)
+print "Total entries migrated: % 1s" %(entries) 
+print "Total skipped: % 10s" %(skipcounter.next() - 1)
+print "Total ghosts removed: % 3s" %len(_ghosts)
+if _ghosts:
+    pprint(_ghosts)
+
 
 
 print "Comitting transaction..."
