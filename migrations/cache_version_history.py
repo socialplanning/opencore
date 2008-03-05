c3bdf0cb593fcb357c227eb73890e495a0cbbb48
Whit Morriss <d.w.morriss@gmail.com>
2008-03-05 23:41:45 +0000

    use new function
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@14068 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- migrations/cache_version_history.py
+++ migrations/cache_version_history.py
@@ -22,59 +22,11 @@ except IndexError:
     noskip = False
 
 portal = getattr(app, portal)
-
-pc = portal.portal_catalog
-pr = portal.portal_repository
 path = '/'.join(portal.getPhysicalPath() + ('projects',))
-brains = pc.unrestrictedSearchResults(portal_type="Document", path=path)
 
-print "\nattempting to migrate %s @ %s" %(len(brains), path)
+print utils.migrate_history(portal, path, noskip=noskip).getvalue()
 
-counter = count()
-skipcounter = count()
-_ghosts = []
-entries = 0
 
-try:
-    for brain in brains:
-        try:
-            page = brain.getObject()
-        except AttributeError:
-            # kill ghost
-            skipcounter.next()
-            _ghosts.append(brain.getPath())
-            pc.uncatalog_object(brain.getPath())
-            txn.commit()
-            continue
-        
-        page._p_jar.sync()
-        if getattr(page, '__HISTORY_MIGRATED__', False) and not noskip:
-            skipcounter.next()
-            continue
 
-        result = utils.cache_history(page, pr)
-        mcount = scount = 0
-        
-        if result:
-            txn.commit()
-            mcount = counter.next()
-            entries += result
-        elif result is False:
-            scount = skipcounter.next()
-        elif result == 0:
-            print "no history: %s" %brain.getPath()
-            
-        i = mcount + scount
-        if i and not i % 100:
-            print "%s docs migrated" %i
-            
-except KeyboardInterrupt, e:
-    print e
 
-print
-print "Total pages migrated: % 3s" %(counter.next() - 1)
-print "Total entries migrated: % 1s" %(entries) 
-print "Total skipped: % 10s" %(skipcounter.next() - 1)
-print "Total ghosts removed: % 3s" %len(_ghosts)
-if _ghosts:
-    pprint(_ghosts)
+
