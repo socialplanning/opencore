ed486ab8416d3596d92ccaa87bf8c8843595be66
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-03-06 23:10:22 +0000

    don't need oc-cab, oc-tt took it
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@14136 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
                         'http://feedparser.googlecode.com/files/feedparser-4.1.zip',
                         'https://svn.openplans.org/svn/vendor/geopy/openplans/dist',
                         'https://svn.openplans.org/svn/topp.utils/trunk#egg=topp.utils-dev',
-                        'https://svn.openplans.org/svn/oc-cab/trunk#egg=oc-cab',
                         'https://svn.openplans.org/svn/oc-tt/trunk#egg=oc-tt',
                         ],
 
@@ -60,7 +59,6 @@ setup(
           'uuid',
           'geopy==0.93-20071130',  # forces our vendor branch.
           'flunc>=0.1.2',
-          'oc-cab',
           'oc-tt',
           ],
       extras_require=dict(ubuntu=['hprof']),
