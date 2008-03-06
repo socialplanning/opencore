23448a6244d4722d6b88ecb147dff95a7f562094
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-03-06 01:57:24 +0000

    moved base_tt_setup
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@14076 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/testing/setup.py
+++ opencore/testing/setup.py
@@ -4,6 +4,7 @@ some basic opencore setups
 from Products.Five.site.localsite import enableLocalSiteHook
 from opencore.testing import utils
 from zope.app.component.hooks import setSite, setHooks
+from opencore.tasktracker.testing.setup import base_tt_setup
 
 def simple_setup(tc):
     tc._refreshSkinData()
@@ -11,14 +12,6 @@ def simple_setup(tc):
     tc.response = tc.request.RESPONSE
     tc.homepage = getattr(tc.portal, 'site-home')
     tc.projects = tc.portal.projects
-
-def base_tt_setup(tc):
-    tc.new_request = utils.new_request()
-    import opencore.tasktracker
-    from zope.app.annotation.interfaces import IAttributeAnnotatable
-    from zope.testing.loggingsupport import InstalledHandler
-    tc.log = InstalledHandler(opencore.tasktracker.LOG)
-    setSite(tc.app.plone)
 
 def extended_tt_setup(tc):
     base_tt_setup(tc)
