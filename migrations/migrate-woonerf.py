44709a68ec3a5af6d75a140c63e3c0881cdb9ab0
Rob Miller <rob@kalistra.com>
2008-03-20 19:12:45 +0000

    slight refactoring, add txn notes and more txn commits, include the team view migration stuff
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@14565 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- migrations/migrate-woonerf.py
+++ migrations/migrate-woonerf.py
@@ -34,27 +34,33 @@ tmt = n.portal_teams
 # to make it clearer below
 portal = n
 
-# first, let's set up the default profile
+# for use later
+def run_import_step(setup_tool, step_id, run_deps=False):
+    """ run an import step via the setup tool """
+    result = setup_tool.runImportStep(step_id, run_dependencies=run_deps)
+    print_status_messages(result)
 
+# first, let's set up the default profile
 # here's where we get the id of the profile site configuration
 ps = n.portal_setup
-
 context_infos = [x['id'] for x in ps.listContextInfos()
                  if 'opencore' in x['id'].lower()]
 context_infos = [x for x in context_infos if 'default' not in x]
 assert len(context_infos) == 1
 context_id = context_infos[0]
-
+    
 # now we set it as the default profile
 ps.setImportContext(context_id=context_id)
 
 # and we can import the right steps now
 print 'importing selected steps'
-result = ps.runImportStep('propertiestool', run_dependencies=1)
-print_status_messages(result)
-result = ps.runImportStep('workflow', run_dependencies=1)
-print_status_messages(result)
+step_ids = ('propertiestool', 'properties', 'workflow')
+for step_id in step_ids:
+    run_import_step(ps, step_id)
 print 'done importing selected steps'
+transaction.get().note('Ran import steps: %s' % ', '.join(step_ids))
+transaction.commit()
+
 
 woonerf_migrations = [
     'Initialize Project BTrees',
@@ -71,24 +77,14 @@ for migration in woonerf_migrations:
     migration_fn(portal)
     print 'successfully ran migration:', migration
 
+transaction.get().note('Ran woonerf setup widgets')
+transaction.commit()
+
 print 'updating role mappings'
 wft.updateRoleMappings()
 print 'done updating role mappings'
-
-new_site_title = u'Livable Streets'
-print 'updating the site title to "%s"' % new_site_title
-n.setTitle(new_site_title)
-print 'site title updated'
-
-email_from_address = 'greetings@livablestreets.com'
-print 'setting email_from_address to "%s"' % email_from_address
-n.manage_changeProperties(email_from_address=email_from_address)
-print 'done'
-
-mailing_list_fqdn = 'lists.livablestreets.com'
-print 'setting mailing_list_fqdn to "%s"' % mailing_list_fqdn
-n.portal_properties.opencore_properties.mailing_list_fqdn = mailing_list_fqdn
-print 'done'
+transaction.get().note('Updated role mappings')
+transaction.commit()
 
 ## StreetsWiki deferred from Woonerf release
 #print "Adding streetswiki folder"
@@ -99,22 +95,29 @@ print 'done'
 #set_method_aliases(portal)
 #print "done"
 
-
 print "Installing portal_geocoder"
 qi = portal.portal_quickinstaller
 qi.installProducts(['PleiadesGeocoder'])
 print "done"
-
-print "Comitting transaction..."
+transaction.get().note('Installed portal_geocoder')
 transaction.commit()
-print "All migrations done"
 
 #this needs to run after the other transaction has been run
 print "Running migrate_history..."
 path = '/'.join(portal.getPhysicalPath() + ('projects',))
 utils.migrate_history(portal, path, out=sys.stdout, save=False)
 print "done"
-
-print "Comitting transaction..."
+transaction.get().note('wiki history migrated')
 transaction.commit()
 print "migrate_history transaction done"
+
+print "Updating membrane catalog for 'team' view optimizations"
+run_import_step(ps, 'membranetool')
+print "done"
+transaction.get().note('membrane_tool reconfigured')
+transaction.commit()
+
+print "Reindexing membrane catalog"
+portal.membrane_tool.refreshCatalog()
+transaction.get().note('membrane_tool reindexed')
+transaction.commit()
