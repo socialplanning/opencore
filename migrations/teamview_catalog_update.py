1bc5b72e41cce34b6ce92054c14f28f1c42f20af
Joshua Bronson <jabronson@gmail.com>
2008-03-24 14:58:40 +0000

    move teamview_catalog_update into a function and call it from migrate-woonerf
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@14619 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- migrations/teamview_catalog_update.py
+++ migrations/teamview_catalog_update.py
@@ -4,26 +4,32 @@ from AccessControl.SecurityManagement import newSecurityManager
 from AccessControl.SpecialUsers import system
 from Testing.makerequest import makerequest
 
-newSecurityManager(None, system)
-app=makerequest(app)
+def teamview_catalog_update(commit=True):
+    newSecurityManager(None, system)
+    app=makerequest(app)
+    
+    try:
+        portal = sys.argv[1]
+    except IndexError:
+        portal = 'openplans'
+    
+    assert hasattr(app, portal), NameError("No portal '%s' exists" % portal)
+    portal = getattr(app, portal)
+    
+    print "updating membrane catalog"
+    setup_tool = portal.portal_setup
+    setup_tool.manage_updateToolProperties("profile-opencore.configuration:default",
+                                           app.REQUEST.RESPONSE)
+    setup_tool.runImportStep('membranetool')
+    transaction.get().note('Run membrane_tool profile import step')
+    if commit:
+        transaction.commit()
+    
+    print "reindexing membranetool"
+    portal.membrane_tool.refreshCatalog()
+    transaction.get().note('Reindex membrane_tool ZCatalog')
+    if commit:
+        transaction.commit()
 
-try:
-    portal = sys.argv[1]
-except IndexError:
-    portal = 'openplans'
-
-assert hasattr(app, portal), NameError("No portal %s exists" %portal)
-portal = getattr(app, portal)
-
-print "updating membrane catalog"
-setup_tool = portal.portal_setup
-setup_tool.manage_updateToolProperties("profile-opencore.configuration:default",
-                                       app.REQUEST.RESPONSE)
-setup_tool.runImportStep('membranetool')
-transaction.get().note('Run membrane_tool profile import step')
-transaction.commit()
-
-print "reindexing membranetool"
-portal.membrane_tool.refreshCatalog()
-transaction.get().note('Reindex membrane_tool ZCatalog')
-transaction.commit()
+if __name__ == '__main__':
+    teamview_catalog_update()
