e5b98ee85a45255d33c7e25e3436f73915b5ff2a
Douglas Mayle <douglas@mayle.org>
2008-04-17 19:01:55 +0000

    Move relevant tests over from oc-feed
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@15907 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/listen/feed/tests.py
@@ -0,0 +1,41 @@
+# This subpackage depends on opencore.feed, so don't run the tests if it's not
+# installed...
+try:
+    __import__('opencore.feed')
+    from opencore.feed.base import BaseFeedAdapter
+    run_tests = True
+except ImportError:
+    run_tests = False
+
+import doctest
+import unittest
+
+if run_tests:
+    BaseFeedAdapter.memberURL = lambda x, y: 'http://www.openplans.org'
+
+class DummyContext(object):
+    def Title(self):
+        return 'Dummy title'
+    def Description(self):
+        return 'Dummy description'
+    def absolute_url(self):
+        return 'http://dummy/context/url'
+    def getPhysicalPath(self):
+        return ['dummy', 'context', 'url']
+    def modified(self):
+        from datetime import datetime
+        return datetime.now()
+    def Creator(self):
+        return 'Dummy creator'
+
+def test_suite():
+    listen_suite = doctest.DocFileSuite('listen.txt',
+                                      optionflags=doctest.ELLIPSIS)
+
+    if run_tests:
+        return unittest.TestSuite((listen_suite,))
+    return unittest.TestSuite()
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
