2a7b34f8a66fafba6bb42eb2a7d290f9ea9a6f59
Jeff Hammel <jhammel@mozilla.com>
2008-04-18 18:59:31 +0000

    get the threads in the same way as for all mailing lists
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@15970 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/feed/listen.py
+++ opencore/listen/feed/listen.py
@@ -8,16 +8,24 @@ from opencore.featurelets.interfaces import IListenContainer
 from opencore.feed.base import BaseFeedAdapter
 from opencore.feed.base import FeedItemResponses
 from opencore.feed.interfaces import IFeedData
-from opencore.feed.interfaces import IFeedItem
 from zope.component import adapts
 from zope.component import getUtility
-from zope.component import createObject
-from zope.interface import alsoProvides
 from zope.interface import implements
 
 # regular expression to strip names from an email
 # address in standard format
 email_re = re.compile(r' *"(.*)" *<.*@.*>')
+
+def latest_reply(archive, message):
+    """given a message, get the latest reply in the thread"""
+    # XXX this should really go in listen
+    prev = next = message
+    while next:
+        if next.date > prev.date:
+            prev = next
+        next = archive.getNextInThread(next.message_id)
+    return prev
+
 
 class ListsFeedAdapter(BaseFeedAdapter):
     implements(IFeedData)
@@ -56,9 +64,6 @@ class ListsFeedAdapter(BaseFeedAdapter):
             # regenerate it.
             return self._items
 
-        MSG_BODY_LENGTH = 300
-
-        items = []
         messages = []
         mlists = self.mlists
         n_lists = len(mlists)
@@ -66,34 +71,18 @@ class ListsFeedAdapter(BaseFeedAdapter):
             mlist = self.context._getOb(ml_id)
             archive = getUtility(ISearchableArchive, context=mlist)
             threads = archive.getToplevelMessages()
-
-            def latest_reply(message):
-                """given a message, get the latest reply in the thread"""
-                # XXX this should really go in listen
-                prev = next = message
-                while next:
-                    if next.date > prev.date:
-                        prev = next
-                    next = archive.getNextInThread(next.message_id)
-                return prev
-
             threads = [ dict(message=message, 
-                             reply=latest_reply(message), 
-                             mlist=mlist)
-                        for message in threads ]
+                              reply=latest_reply(archive, message), 
+                              mlist=mlist)
+                         for message in threads ]
             messages.extend(threads)
             
         date = lambda x: x['reply'].modification_date
         messages.sort(key=date, reverse=True)
         messages = messages[:n_items]
 
-        for message in messages:
-            #XXX
-            # we get the object for now to see the body of the message
-            # we can either add it to the metadata, or forget about it
-            # or maybe something more clever?
-            #             msg = brain.getObject()
-            
+        self._items = []
+        for message in messages:            
             reply = message['reply']
             mlist = message['mlist']
             message = message['message']
@@ -146,53 +135,71 @@ class ListsFeedAdapter(BaseFeedAdapter):
 
 #XXX duplication between this class and lists class above
 # can factor out common behavior to a separate function/superclass
-# OR (better) can make the MailingListsFeed consume the MailingListFeed
+# OR (better) can make the MailingListsFeed consume and aggregate the MailingListFeed
 class MailingListFeedAdapter(BaseFeedAdapter):
     implements(IFeedData)
     adapts(IOpenMailingList)
 
     @property
     def items(self, n_items=5):
-        """feed for first messages in a mailing list
+        """feed for latest messages in a mailing list"""
 
-           XXX somewhat expensive in that we get n_items mailing list objects
-           but I think that these are somewhat lightweight in that they
-           inherit from OFS.Folder
-           """
+        if hasattr(self,'_items'):
+            # If the property already contains something, there's no need to
+            # regenerate it.
+            return self._items
 
-        MSG_BODY_LENGTH = 300
+        archive = getUtility(ISearchableArchive, context=self.context)
+        messages = archive.getToplevelMessages()
+        messages = [ dict(message=message, 
+                          reply=latest_reply(archive, message), 
+                          mlist=self.context)
+                     for message in messages ]
 
-        items = []
-        brains = []
-        sa = getUtility(
-            ISearchableArchive,
-            context=self.context)
-        brains = sa.searchResults(sort_on='modification_date',
-                                  sort_order='descending',
-                                  sort_limit=n_items)
+        date = lambda x: x['reply'].modification_date
+        messages.sort(key=date, reverse=True)
+        messages = messages[:n_items]
 
-        for brain in brains:
-            #XXX
-            # we get the object for now to see the body of the message
-            # we can either add it to the metadata, or forget about it
-            # or maybe something more clever?
-            msg = brain.getObject()
+        self._items = []
+        for message in messages:            
+            reply = message['reply']
+            mlist = message['mlist']
+            message = message['message']
+
+            structure = messageStructure(message,
+                                         sub_mgr=mlist)
+            reply_structure = messageStructure(reply,
+                                               sub_mgr=mlist)
             
-            title = brain.subject
-            # only put the first part of the body of long messages
-            body = msg.body
-            if len(body) > MSG_BODY_LENGTH:
-                body = '%s ...' % body[:MSG_BODY_LENGTH-4]
-            description = body
-            link = brain.getURL()
-            pubDate = brain.modification_date
-            author = msg.owner_info()['id']
+            link = '%s/forum_view' % structure['url'].rstrip('/')
+            reply_url = '%s#%s' % ( link, reply_structure['id'] )
 
-            feed_item = createObject('opencore.feed.feeditem',
-                                     title,
-                                     description,
-                                     link,
-                                     author,
-                                     pubDate)
-            items.append(feed_item)
-        return items
+            pubDate = reply.modification_date
+            
+            author = reply_structure['from_id']
+            if not author:
+                author = reply_structure['mail_from']
+                match = re.match(email_re, author)
+                if match:
+                    author = match.groups()[0]
+
+            title = structure['subject']
+            description = structure['subject']
+
+            # deal with responses
+            responses = message.responses
+            if responses:
+                byline = 'latest by'
+                response = FeedItemResponses(responses, reply_url)
+            else:
+                response = None
+                byline = 'by'
+
+            self.add_item(title=title,
+                          description=description,
+                          link=link,
+                          author=author,
+                          pubDate=pubDate,
+                          byline=byline,
+                          responses=response)
+        return self._items
