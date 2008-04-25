07aa95ace6d272d81f9c021432ced9fc2ca1c3da
Jeff Hammel <jhammel@mozilla.com>
2008-04-25 20:23:28 +0000

    replace the listen feed adapters;  this new one is very simple and faster: ~5s vs ~30s;  this disables the specced UI, but gives a cleaner slate for optimization once we figure out priority
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@16188 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/feed/listen.py
+++ opencore/listen/feed/listen.py
@@ -6,6 +6,7 @@ from opencore.feed.base import FeedItemResponses
 from opencore.feed.interfaces import IFeedData
 from opencore.listen.interfaces import IOpenMailingList
 from Products.listen.interfaces import ISearchableArchive
+from Products.listen.lib.browser_utils import getAddressInfo
 from Products.listen.lib.browser_utils import messageStructure
 from zope.component import adapts
 from zope.component import getUtility
@@ -38,87 +39,128 @@ class ListsFeedAdapter(BaseFeedAdapter):
 
     @property
     def items(self, n_items=5):
-        """we aggregate the most recent messages across all mailing lists,
-           then sort those by date and take the top n_items
-
-           XXX this is potentially an expensive operation
-           we may want a separate catalog on the lists level
-           or store the mail messages in the main portal catalog as well
-           in which case we would be able to search for messages
-           in the site wide search"""
-
         if hasattr(self,'_items'):
             # If the property already contains something, there's no need to
             # regenerate it.
             return self._items
 
-        messages = []
-        mlists = self.mlists
-        n_lists = len(mlists)
-        for ml_id in mlists:
+        all_msgs = []
+        for ml_id in self.mlists:
             mlist = self.context._getOb(ml_id)
-            archive = getUtility(ISearchableArchive, context=mlist)
-            threads = archive.getToplevelMessages()
-            threads = [ dict(message=message, 
-                             reply=latest_reply(archive, message), 
-                             mlist=mlist)
-                         for message in threads ]
-            messages.extend(threads)
-            
-        date = lambda x: x['reply'].modification_date
-        messages.sort(key=date, reverse=True)
-        messages = messages[:n_items]
-
-        self._items = []
-        for message in messages:            
-            reply = message['reply']
-            mlist = message['mlist']
-            message = message['message']
-
-            structure = messageStructure(message,
-                                         sub_mgr=mlist)
-            reply_structure = messageStructure(reply,
-                                               sub_mgr=mlist)
-            
-            link = '%s/forum_view' % structure['url'].rstrip('/')
-            reply_url = '%s#%s' % ( link, reply_structure['id'] )
-
-            pubDate = reply.modification_date
-            
-            author = reply_structure['from_id']
-            if not author:
-                author = reply_structure['mail_from']
-                match = re.match(email_re, author)
-                if match:
-                    author = match.groups()[0]
-
-            title = structure['subject']
-            description = structure['subject']
-
-            # if more than one mailing list, it should be noted
-            context = None
-            if n_lists > 1:
-                context = { 'title': mlist.title,
-                            'link': mlist.absolute_url }
-
-            # deal with responses
-            responses = message.responses
-            if responses:
-                byline = 'latest by'
-                response = FeedItemResponses(responses, reply_url)
+            cat = getUtility(ISearchableArchive, context=mlist)
+            msgs = cat(sort_limit=n_items,
+                       sort_order='descending',
+                       sort_on='modification_date')
+            all_msgs.extend(msgs)
+        all_msgs.sort(key=lambda x:x.modification_date, reverse=True)
+        all_msgs = all_msgs[:n_items]
+        for msg in all_msgs:
+            user_id, user_email = getAddressInfo(msg, mlist)
+            if user_id is None:
+                author = user_email
             else:
-                response = None
-                byline = 'by'
-
-            self.add_item(title=title,
-                          description=description,
-                          link=link,
+                author = user_id
+            self.add_item(title=msg.subject,
+                          description=msg.subject,
+                          link='%s/forum_view' % msg.getURL(),
                           author=author,
-                          pubDate=pubDate,
-                          context=context,
-                          byline=byline,
-                          responses=response)
+                          pubDate=msg.modification_date,
+#                          context=mlist,
+#                              byline='by',
+#                              responses=response
+                              )
+
         return self._items
+
+    def memberURL(self, id):
+        if '@' in id: # XXX HACK!!!
+            return None
+        return BaseFeedAdapter.memberURL(self, id)
+
+
+#     @property
+#     def items(self, n_items=5):
+#         """we aggregate the most recent messages across all mailing lists,
+#            then sort those by date and take the top n_items
+
+#            XXX this is potentially an expensive operation
+#            we may want a separate catalog on the lists level
+#            or store the mail messages in the main portal catalog as well
+#            in which case we would be able to search for messages
+#            in the site wide search"""
+
+#         if hasattr(self,'_items'):
+#             # If the property already contains something, there's no need to
+#             # regenerate it.
+#             return self._items
+
+#         messages = []
+#         mlists = self.mlists
+#         n_lists = len(mlists)
+#         for ml_id in mlists:
+#             mlist = self.context._getOb(ml_id)
+#             archive = getUtility(ISearchableArchive, context=mlist)
+#             threads = archive.getToplevelMessages()
+#             threads = [ dict(message=message, 
+#                              reply=latest_reply(archive, message), 
+#                              mlist=mlist)
+#                          for message in threads ]
+#             messages.extend(threads)
+            
+#         date = lambda x: x['reply'].modification_date
+#         messages.sort(key=date, reverse=True)
+#         messages = messages[:n_items]
+
+#         self._items = []
+#         for message in messages:            
+#             reply = message['reply']
+#             mlist = message['mlist']
+#             message = message['message']
+
+#             structure = messageStructure(message,
+#                                          sub_mgr=mlist)
+#             reply_structure = messageStructure(reply,
+#                                                sub_mgr=mlist)
+            
+#             link = '%s/forum_view' % structure['url'].rstrip('/')
+#             reply_url = '%s#%s' % ( link, reply_structure['id'] )
+
+#             pubDate = reply.modification_date
+            
+#             author = reply_structure['from_id']
+#             if not author:
+#                 author = reply_structure['mail_from']
+#                 match = re.match(email_re, author)
+#                 if match:
+#                     author = match.groups()[0]
+
+#             title = structure['subject']
+#             description = structure['subject']
+
+#             # if more than one mailing list, it should be noted
+#             context = None
+#             if n_lists > 1:
+#                 context = { 'title': mlist.title,
+#                             'link': mlist.absolute_url }
+
+#             # deal with responses
+#             responses = message.responses
+#             if responses:
+#                 byline = 'latest by'
+#                 response = FeedItemResponses(responses, reply_url)
+#             else:
+#                 response = None
+#                 byline = 'by'
+
+#             self.add_item(title=title,
+#                           description=description,
+#                           link=link,
+#                           author=author,
+#                           pubDate=pubDate,
+#                           context=context,
+#                           byline=byline,
+#                           responses=response)
+#         return self._items
 
 
 #XXX duplication between this class and lists class above
