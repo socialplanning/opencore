cbdd492fa491f83b1afc1c0f19a72351e1086562
Rob Miller <rob@kalistra.com>
2008-04-26 02:29:42 +0000

    more aq chain cleanup crap
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/plone3@16205 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/browser/viewletmanagers.py
+++ opencore/browser/viewletmanagers.py
@@ -1,9 +1,29 @@
+from AccessControl.ZopeGuards import guarded_hasattr
+from Acquisition import aq_inner
 from Products.Five.viewlet.manager import ViewletManagerBase
 from opencore.browser.interfaces import IJSViewlets
 
 from zope.interface import implements
 
 class SortedViewletManager(ViewletManagerBase):
+
+    def filter(self, viewlets):
+        """Sort out all content providers
+
+        ``viewlets`` is a list of tuples of the form (name, viewlet).
+
+        use the physical path's acquisition context.
+        """
+        results = []
+        # Only return viewlets accessible to the principal
+        # We need to wrap each viewlet in its context to make sure that
+        # the object has a real context from which to determine owner
+        # security.
+        for name, viewlet in viewlets:
+            viewlet = viewlet.__of__(aq_inner(viewlet.context))
+            if guarded_hasattr(viewlet, 'render'):
+                results.append((name, viewlet))
+        return results
 
     def sort(self, viewlets):
         """Sort the viewlets according to their sort_order attribute"""
