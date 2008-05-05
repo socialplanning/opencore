db75c4ecddf2fa677e65babf33c6a829a9ec74a9
Jeff Hammel <jhammel@mozilla.com>
2008-05-05 17:48:48 +0000

    allow embedded tags to fix #690.  Not very helpful info from  lxmls docstring: ``embedded``: |      Removes any embedded objects (flash, iframes)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@16540 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/wiki/view.py
+++ opencore/nui/wiki/view.py
@@ -98,8 +98,8 @@ class WikiEdit(WikiBase, OctopoLite):
         except TypeError:
             raise TypeError("Bad value for portal_properties.embed_whitelist: %r" % whitelist)
 
-        cleaner = Cleaner(host_whitelist=whitelist, safe_attrs_only=False)
-
+        cleaner = Cleaner(host_whitelist=whitelist, safe_attrs_only=False, embedded=False)
+        
         # stolen from lxml.html.clean
         if isinstance(html, basestring):
             return_string = True
