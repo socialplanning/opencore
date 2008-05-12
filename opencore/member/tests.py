043e0c95bc18070e896cdb6108155af3cdcd0271
Paul Winkler <slinkp@gmail.com>
2008-05-12 19:46:10 +0000

    Clean up more convenience imports of alsoProvides & noLongerProvides
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@16792 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/member/tests.py
+++ opencore/member/tests.py
@@ -24,7 +24,8 @@ def test_suite():
     from opencore.interfaces.message import ITransientMessage
     from opencore.interfaces.message import ITransientMessage
     from opencore.interfaces.pending_requests import IPendingRequests
-    from opencore.testing import alsoProvides, noLongerProvides
+    from Products.Five.utilities.marker import erase as noLongerProvides
+    from zope.interface import alsoProvides
     from zope.component import getUtility
 
     setup.setupPloneSite()
--- opencore/testing/__init__.py
+++ opencore/testing/__init__.py
@@ -1,7 +1,1 @@
-from zope.interface import alsoProvides
-try:
-    from zope.interface import noLongerProvides
-except ImportError:
-    from Products.Five.utilities.marker import erase as noLongerProvides
-
 from opencore.testing.utility import mock_utility
