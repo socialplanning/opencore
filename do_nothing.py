b7ff4c702174bffe779cd23229eb5a344f1d7cc2
Paul Winkler <slinkp@gmail.com>
2008-05-20 15:47:42 +0000

    just a comment; also, hopefully buildbot will build this.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@17099 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- do_nothing.py
+++ do_nothing.py
@@ -2,4 +2,7 @@
 Run this script once via zopectl run and you should be good to go.
 """
 
+# XXX Will we still need this after moving to plone 3, or will things
+# finally just work?
+
 print "successfully did nothing"
