70fcea30806153c5a42e39a993e4952de2e455a3
Paul Winkler <slinkp@gmail.com>
2008-05-26 21:42:10 +0000

    Add new viewlet manager for opencore member profile edit sidebar. oc-geotagging will put a map here.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@17313 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/member/browser/interfaces.py
+++ opencore/member/browser/interfaces.py
@@ -2,3 +2,7 @@ from zope.viewlet.interfaces import IViewletManager
 
 class IMemberProfilePrefs(IViewletManager):
     """Settings you can modify on a member profile"""
+
+
+class IMemberProfileSidebar(IViewletManager):
+    """More member profile viewlets"""
--- opencore/member/browser/manager.py
+++ opencore/member/browser/manager.py
@@ -1,8 +1,5 @@
 from opencore.browser.viewletmanagers import SortedViewletManager
-from zope.interface import implements
-from opencore.member.browser import interfaces
 
 class MemberPrefsManager(SortedViewletManager):
     """sorted viewlet manager for project preferences """
 
-    implements(interfaces.IMemberProfilePrefs)
