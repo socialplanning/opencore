b48ea6332f904ef0499570c20001d9d47469bd33
Paul Winkler <slinkp@gmail.com>
2008-05-28 20:44:13 +0000

    CHANGED: rmarianski broke the build with a mispelled test case argument.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@17429 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/wiki/tests.py
+++ opencore/nui/wiki/tests.py
@@ -34,7 +34,7 @@ def test_suite():
                                          optionflags=optionflags,
                                          package='opencore.nui.wiki',
                                          test_class=FunctionalTestCase,
-                                         setup=readme_setup,
+                                         setUp=readme_setup,
                                          globs = globs,
                                          layer = OpencoreContent,
                                          )
