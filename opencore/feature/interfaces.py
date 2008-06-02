955c93879ae8a89680dccf1210983521f82077b2
Rob Marianski <rob@marianski.com>
2008-06-02 04:25:58 +0000

    be more specific with feature interface
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/featured_project@17572 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feature/interfaces.py
+++ opencore/feature/interfaces.py
@@ -1,5 +1,4 @@
 from zope.interface import Interface
-from zope.interface import Attribute
 from zope import schema
 
 class IFeature(Interface):
@@ -7,8 +6,8 @@ class IFeature(Interface):
 
     title = schema.TextLine(title=u'Featured title', description=u'title of featured item')
     description = schema.Text(title=u'Featured description', description=u'description of featured item')
-    link = Attribute('optional link to featured item')
-    image_url = Attribute('optional url image of featured item')
+    link = schema.TextLine(title=u'Link to featured item', description=u'optional link to featured item')
+    image_url = schema.TextLine(title=u'Url to feature image', description=u'optional url image of featured item')
 
 class IProjectFeature(IFeature):
     """marker interface for project features specifically"""
