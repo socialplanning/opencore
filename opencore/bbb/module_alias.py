f81711e6c660d106dd3571fcde8c43d803320549
Rob Miller <rob@kalistra.com>
2008-06-17 02:35:43 +0000

    add more aliases to sys.modules so persistent objects w/ marker interfaces or class definitions that have moved will still work
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/plone3@18431 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/bbb/module_alias.py
+++ opencore/bbb/module_alias.py
@@ -11,6 +11,7 @@ import opencore.project
 import opencore.project.browser
 import opencore.bbb.transient_messages
 import wicked
+import Products.SimpleAttachment.content.file
 
 def do_aliases():
     sys.modules['opencore.siteui'] = opencore.browser
@@ -21,6 +22,15 @@ def do_aliases():
     sys.modules['opencore.nui.member.interfaces'] = opencore.interfaces.bbb
     sys.modules['opencore.nui.project.interfaces'] = opencore.interfaces.bbb
     sys.modules['opencore.siteui.interfaces'] = opencore.interfaces.member
+
+    # wicked BBB crap
     sys.modules['Products.wicked'] = wicked
+    sys.modules['Products.wicked.lib'] = wicked
+    sys.modules['Products.wicked.lib.cache'] = wicked.cache # for CacheStore obs
+    sys.modules['Products.wicked.interfaces'] = wicked.interfaces
+    sys.modules['Products.wicked.example'] = wicked.atcontent
+    sys.modules['Products.wicked.example.wickeddoc'] = wicked.atcontent.wickeddoc
 
-
+    # RichDocument -> SimpleAttachment
+    sys.modules['Products.RichDocument.content.attachments'] = \
+            Products.SimpleAttachment.content.file
