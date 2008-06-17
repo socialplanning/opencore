eb338610af245aa0f64a1de23fb52ae8ec16c0f7
Rob Miller <rob@kalistra.com>
2008-06-17 02:38:18 +0000

    add upgrade steps to reindex the membrane tool and migrate all of the mailing list local component registries
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/plone3@18433 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/upgrades/to_0_12_0.py
+++ opencore/upgrades/to_0_12_0.py
@@ -1,7 +1,13 @@
 from Products.CMFCore.utils import getToolByName
+from Products.Five.site.localsite import disableLocalSiteHook
 from opencore.upgrades.utils import run_import_step
 from opencore.upgrades.utils import logger
 from opencore.upgrades.utils import default_profile_id
+from zope.app.component.hooks import setSite
+from zope.component import getSiteManager
+from zope.component import queryMultiAdapter
+from zope.interface import Interface
+from zope.interface import providedBy
 
 def import_opencore_profile(context):
     """
@@ -19,3 +25,49 @@ def fixup_list_lookup_utility(context):
                              profile_id='Products.listen:listen',
                              )
     logger.info(result)
+
+def reindex_membrane_tool(context):
+    """
+    Triggers a reindex of the membrane_tool catalog.
+    """
+    mbtool = getToolByName(context, 'membrane_tool')
+    mbtool.refreshCatalog()
+    logger.info('Membrane tool reindexed')
+
+def migrate_mlist_component_registries(context):
+    """
+    Upgrades all of the mailing lists' local component registries to
+    the Five 1.5 format.
+    """
+    site = getToolByName(context, 'portal_url').getPortalObject()
+    cat = getToolByName(context, 'portal_catalog')
+    list_brains = cat(portal_type='Open Mailing List')
+
+    for lbrain in list_brains:
+        lst = lbrain.getObject()
+        utilities = lst.utilities.objectItems()
+
+        disableLocalSiteHook(lst)
+        components_view = queryMultiAdapter((lst, lst.REQUEST),
+                                            Interface, 'components.html')
+        components_view.makeSite()
+        setSite(lst)
+        lst.manage_delObjects(['utilities'])
+
+        site_manager = getSiteManager()
+        for id, utility in utilities:
+            info = id.split('-')
+            if len(info) == 1:
+                name = ''
+            else:
+                name = info[1]
+            interface_name = info[0]
+
+            for iface in providedBy(utility):
+                if iface.getName() == interface_name:
+                    site_manager.registerUtility(utility, iface, name=name)
+
+        logger.info('%s mailing list component registry migrated'
+                    % list.getId())
+
+    logger.info('Mailing list local component registry migration complete')
