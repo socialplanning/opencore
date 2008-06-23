d83c784ec7c4e94b0a1662119f56accf109d1837
Chris Abraham <cabraham@openplans.org>
2008-06-23 19:45:10 +0000

    modified listen migration to deassign local roles on lists folders and archive folders
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@18622 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- Products/OpenPlans/Extensions/migrate_listen_permissions.py
+++ Products/OpenPlans/Extensions/migrate_listen_permissions.py
@@ -5,6 +5,7 @@ from AccessControl.interfaces import IRoleManager
 
 from Products.listen.interfaces import IListLookup
 from Products.listen.lib.common import assign_local_role
+from Acquisition import aq_parent, aq_inner
 
 OWNER_ROLE = 'Owner'
 
@@ -24,6 +25,12 @@ def migrate_listen_permissions(self):
         managers = ml.managers
         list_role_manager = IRoleManager(ml)
         assign_local_role(OWNER_ROLE, managers, list_role_manager)
+        # here we delete roles on the 'lists' folder so that they don't interfere
+        # with roles on the list
+        parent = aq_parent(aq_inner(ml))
+        assign_local_role(OWNER_ROLE, [], IRoleManager(parent))
+        # we also need to delete the roles on the archives folder
+        assign_local_role(OWNER_ROLE, [], IRoleManager(ml.archive))
         out.write("Assigned local roles for list: '%s'\n" % ml.Title())
 
     return out.getvalue()
