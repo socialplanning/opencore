063684434e08403173ec4683da7097ebcc8901f7
Rob Marianski <rob@marianski.com>
2008-06-24 23:47:47 +0000

    for features, use a plain old news item instead of creating a new content type and refer to the project
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/featured_project@18684 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feature/browser.py
+++ opencore/feature/browser.py
@@ -33,24 +33,13 @@ class FeatureAddView(formbase.Form, BaseView):
     def handle_add(self, action, data):
         proj_id = data['proj_id']
         project_feature = createObject('opencore.feature.project',
+                                       self.context,
+                                       data['proj_id'],
                                        data['title'],
                                        data['description'],
-                                       proj_id,
+                                       data['proj_id'],
                                        )
-        #XXX this doesn't work because we didn't set up the fti
-        #(it sets it correctly, but getLayout asks the fti, which isn't set)
-        project_feature.setLayout('view')
-
-        self.context[proj_id] = project_feature
-
-        #XXX this also doesn't work because something isn't set right
-        # getPhysicalPath on the project_feature object is empty
-        #self.request.response.redirect(project_feature.absolute_url())
-
-        #XXX we redirect to /view because the default view on features
-        #isn't set up right
-        self.request.response.redirect(
-            '%s/%s/view' % (self.context.absolute_url(), proj_id))
+        self.request.response.redirect(project_feature.absolute_url())
 
 class FeatureEditView(formbase.EditForm, BaseView):
     """Edit a featured project"""
@@ -73,9 +62,7 @@ class FeatureEditView(formbase.EditForm, BaseView):
 
     @form.action('Save changes', prefix='')
     def handle_save(self, action, data):
-        self.context.title = data['title']
-        self.context.description = data['description']
-        #XXX this is what it can be when we work out the urls
-        #self.request.response.redirect(self.context.absolute_url())
-        self.request.response.redirect(
-            self.request.ACTUAL_URL.replace('edit', 'view'))
+        self.context.setTitle(data['title'])
+        self.context.setDescription(data['description'])
+        self.context.reindexObject()
+        self.request.response.redirect(self.context.absolute_url())
--- opencore/feature/feature.py
+++ opencore/feature/feature.py
@@ -1,40 +1,6 @@
-from OFS.SimpleItem import SimpleItem
-from OFS.PropertyManager import PropertyManager
-from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
-from Products.CMFCore.DynamicType import DynamicType
 from Products.CMFCore.utils import getToolByName
-from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
-from interfaces import IProjectFeature
 from zope.app.component.hooks import getSite
-from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary
-
-#XXX all these base classes, and things aren't working right
-# must be an easier way :)
-class ProjectFeature(SimpleItem,
-                     DynamicType,
-                     CMFCatalogAware,
-                     PropertyManager,
-                     BrowserDefaultMixin):
-    """implementation of a featured item geared towards projects"""
-
-    implements(IProjectFeature)
-
-    def __init__(self, title, description, proj_id):
-        self.proj_id = proj_id
-        self.title = title
-        self.description = description
-
-    @property
-    def link(self):
-        site = getSite()
-        return '%s/projects/%s' % (
-            getToolByName(site, 'portal_url')(),
-            self.proj_id)
-
-    @property
-    def image_url(self):
-        return '%s/logo_square_thumb' % self.link
 
 def projectsVocabulary(self):
     """return back all project ids"""
