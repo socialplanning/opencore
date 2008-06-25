3f10c566092db721d46a3de461d65ceba5ceea07
Rob Marianski <rob@marianski.com>
2008-06-25 21:16:24 +0000

    add text field, and allow pegging an existing feature to a different project
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/featured_project@18699 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feature/browser.py
+++ opencore/feature/browser.py
@@ -1,9 +1,12 @@
+from Products.CMFCore.utils import getToolByName
 from Products.Five.formlib import formbase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from opencore.browser.base import BaseView
 from zope.formlib import form
 from zope.component import createObject
 from zope import schema
+
+projfeature_prefix = 'projfeature-'
 
 class FeatureAddView(formbase.Form, BaseView):
     """add a new featured project"""
@@ -22,6 +25,10 @@ class FeatureAddView(formbase.Form, BaseView):
                     description=u'Feature Description',
                     __name__='description',
                     required=True),
+        schema.Text(title=u'Text',
+                    description=u'Feature Text',
+                    __name__='text',
+                    required=True),
         schema.Choice(title=u'Featured project',
                       description=u'Featured project',
                       __name__='proj_id',
@@ -34,17 +41,18 @@ class FeatureAddView(formbase.Form, BaseView):
         proj_id = data['proj_id']
         project_feature = createObject('opencore.feature.project',
                                        self.context,
-                                       data['proj_id'],
+                                       projfeature_prefix + data['proj_id'],
                                        data['title'],
                                        data['description'],
+                                       data['text'],
                                        data['proj_id'],
                                        )
         self.request.response.redirect(project_feature.absolute_url())
 
-class FeatureEditView(formbase.EditForm, BaseView):
+class FeatureEditView(formbase.Form, BaseView):
     """Edit a featured project"""
 
-    template = ViewPageTemplateFile('feature_form.pt')
+    template = ViewPageTemplateFile('feature_edit_form.pt')
 
     prefix = ''
     label = u'Edit a featured project'
@@ -58,11 +66,35 @@ class FeatureEditView(formbase.EditForm, BaseView):
                     description=u'Feature Description',
                     __name__='description',
                     required=True),
+        schema.Text(title=u'Text',
+                    description=u'Feature Text',
+                    __name__='text',
+                    required=True),
+        schema.Choice(title=u'Featured project',
+                      description=u'Featured project',
+                      __name__='proj_id',
+                      vocabulary='opencore.projects',
+                      required=True),
         )
 
     @form.action('Save changes', prefix='')
     def handle_save(self, action, data):
+        cur_proj_id = self.context.getId()[len(projfeature_prefix):]
+        new_proj_id = data['proj_id']
+        if cur_proj_id != new_proj_id:
+            #we need to rename the object
+            #XXX this will give us copy errors if we try to set the id to something that already exists
+            parent = self.context.aq_inner.aq_parent
+            parent.manage_renameObject(projfeature_prefix + cur_proj_id, projfeature_prefix + new_proj_id)
+            self.context = parent._getOb(projfeature_prefix + new_proj_id)
+            # and we need to also update the reference
+            self.context.deleteReferences(relationship='opencore.featured.project')
+            portal = getToolByName(self.context, 'portal_url').getPortalObject()
+            proj = portal.projects._getOb(new_proj_id)
+            self.context.addReference(proj, relationship='opencore.featured.project')
+
         self.context.setTitle(data['title'])
         self.context.setDescription(data['description'])
+        self.context.setText(data['text'])
         self.context.reindexObject()
         self.request.response.redirect(self.context.absolute_url())
--- opencore/feature/factory.py
+++ opencore/feature/factory.py
@@ -3,12 +3,14 @@ from interfaces import IProjectFeature
 from zope.component.factory import Factory
 from zope.interface import alsoProvides
 
-def create_project_feature(context, id, title, description, proj_id):
+def create_project_feature(context, id, title, description, text, proj_id):
+    # XXX this will generate errors if the project has already been featured
     context.invokeFactory('News Item', id, title=title)
     ni = context._getOb(id)
     alsoProvides(ni, IProjectFeature)
     ni.setTitle(title)
     ni.setDescription(description)
+    ni.setText(text)
     portal = getToolByName(context, 'portal_url').getPortalObject()
     proj = portal.projects._getOb(proj_id)
     ni.addReference(proj, relationship='opencore.featured.project')
