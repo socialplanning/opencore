9566efdb1a87e47d55c72b500a5f29afd4f126db
Rob Miller <rob@kalistra.com>
2008-07-11 21:20:54 +0000

    adding unmake-sites migration (to be run BEFORE a p3 upgrade)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@19014 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ migrations/unmake-sites.py
@@ -0,0 +1,34 @@
+"""
+Migration to unmake the local sites at the app root and the Plone site
+root.  Run this BEFORE updating to Plone 3-based opencore, version 0.13.
+"""
+
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SpecialUsers import system
+import sys
+import transaction
+
+newSecurityManager(None, system)
+
+from Testing.makerequest import makerequest
+app=makerequest(app)
+
+from zope.component import getMultiAdapter
+from zope.interface import Interface
+
+try:
+    portal = sys.argv[1]
+except IndexError:
+    portal = 'openplans'
+
+app_view = getMultiAdapter((app, app.REQUEST), Interface,
+                           name='manage_site.html')
+app_view.unmakeSite()
+
+portal = getattr(app, portal)
+portal_view = getMultiAdapter((portal, portal.REQUEST), Interface,
+                              name='manage_site.html')
+portal_view.unmakeSite()
+
+transaction.get().note('Unmade app and portal as local sites')
+transaction.commit()
