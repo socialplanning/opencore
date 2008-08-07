37130d76e198fcea1ec7deb5f3c8dc0daff28985
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-08-07 20:00:28 +0000

    don't subclass ViewletBase (http://www.openplans.org/projects/opencore/lists/opencore-dev/archive/2008/08/1218138858079/forum_view)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/viewlet-refactor@19693 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/main/view.py
+++ opencore/nui/main/view.py
@@ -1,8 +1,6 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.Five.viewlet.viewlet import ViewletBase
 from opencore.project.utils import project_noun
 
-class GetStarted(ViewletBase):
+class GetStarted(object):
 
     def project_noun(self):
         return project_noun()
