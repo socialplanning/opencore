d50432a4ef74d8804467b9eff03d8fff2597ffab
Paul Winkler <slinkp@gmail.com>
2008-08-12 17:32:38 +0000

    Merged some changelog entries from trunk that are actually on this branch already.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/0.13@19758 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pkg_resources as pkr
 
 import sys, os
 
-version = '0.13.0.dev'
+version = '0.13.0'
 
 f = open('README.txt')
 readme = f.read()
@@ -11,12 +11,23 @@ f.close()
 
 name='opencore'
 
+# Get strings from http://www.python.org/pypi?:action=list_classifiers
+classifiers=[
+    "Framework :: Plone",
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Natural Language :: English",
+    "Operating System :: Unix",
+    "Programming Language :: Python",
+    ]
+
 setup(
     name=name,
       version=version,
       description="Software that drives http://openplans.org",
       long_description=readme,
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=classifiers,
       keywords='openplans openplans.org topp',
       author='The Open Planning Project',
       author_email='opencore-dev@lists.openplans.org',
