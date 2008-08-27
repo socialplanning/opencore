bfd84d43c94f60363dc524cafc27ea8f37309717
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-08-27 16:09:22 +0000

    cleanup project add view, make it look more like prefs
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/viewlet-sandbox@20093 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/add.py
+++ opencore/project/browser/add.py
@@ -60,24 +60,24 @@ class ProjectAddView(ProjectBaseView, OctopoLite):
             return False
         return True
 
-    def validate(self):
+    def validate(self, request):
         errors = {}
 
-        title = self.request.form.get('project_title')
+        title = request.form.get('project_title')
         title = text.strip_extra_whitespace(title)
         if not isinstance(title, unicode):
             title = unicode(title, 'utf-8')
-        self.request.form['project_title'] = title
+        request.form['project_title'] = title
         if not self.valid_title(title):
             errors['project_title'] = 'The name must contain 2 or more characters.'
 
-        id_ = self.request.form.get('projid')
+        id_ = request.form.get('projid')
         if not self.valid_id(id_):
             errors['id'] = 'The url must contain 2 or more characters.'
         else:
             putils = getToolByName(self.context, 'plone_utils')
             id_ = putils.normalizeString(id_)
-            self.request.form['projid'] = id_
+            request.form['projid'] = id_
             if self.context.has_key(id_):
                 errors['id'] = 'The requested url is already taken.'
         if id_ in self.reserved_names():
@@ -86,23 +86,15 @@ class ProjectAddView(ProjectBaseView, OctopoLite):
                                       u'The name "${project_name}" is reserved. Please try a different name.',
                                       mapping={u'project_name':id_}))
 
-        # Give plugin viewlets a chance to validate. We don't have a
-        # project yet, so they'll have to tolerate validating with the
-        # project container as the context.
-        from opencore.framework.editform import edit_form_manager
-        manager = edit_form_manager(self)
-        errors.update(manager.validate(self.request))
-        return errors
+    def save(self, request):
+        proj = self.context[request.form['projid']]
 
-    def save(self):
-        proj = self.context[self.request.form['projid']]
-
-        logo = self.request.form.get('logo')
+        logo = request.form.get('logo')
         if logo:
             if not self.check_logo(proj, logo):
                 # the above call actually sets the logo. yuck.
                 return
-            del self.request.form['logo']
+            del request.form['logo']
 
         hpcontext = IHomePage(proj)
         hpcontext.home_page = 'summary'
@@ -111,25 +103,32 @@ class ProjectAddView(ProjectBaseView, OctopoLite):
         # a project for them to use as the context to save to.
         from opencore.framework.editform import edit_form_manager
         manager = edit_form_manager(self, context=proj)
-        manager.save(self.request)
+        manager.save(request)
+
+    def error_handler(self, errors):
+        self.errors = errors
+        self.add_status_message(_(u'psm_correct_errors_below',
+                                  u'Please correct the errors indicated below.'))
 
     @action('add')
     def handle_request(self, target=None, fields=None):
         #XXX all of the errors that are reported back here are not going
         # through the translation machinery
 
-        # @@ what is this for?
-        self.request.set('__initialize_project__', True)
+        from opencore.framework.editform import edit_form_manager
+        plugins = edit_form_manager(self)
+        request = self.request
+        
+        request.set('__initialize_project__', True) # @@ what is this for?
+        errors = self.validate(request)
 
-        self.errors = self.validate()
-
-        # XXX TO DO: handle featurelets, just like in preferences.py
-
-        if self.errors:
-            self.add_status_message(_(u'psm_correct_errors_below',
-                                      u'Please correct the errors indicated below.'))
-            return
-
+        # Give plugin viewlets a chance to validate. We don't have a
+        # project yet, so they'll have to tolerate validating with the
+        # project container as the context.
+        errors.update(plugins.validate(request))
+        if errors:
+            return self.error_handler(errors)
+        
         # Aarrgghh!! #*!&% plone snoops into the request, and reads the form variables directly,
         # so we have to set the form variables with the same names as the schema
         self.request.form['title'] = self.request.form['project_title']
@@ -144,7 +143,7 @@ class ProjectAddView(ProjectBaseView, OctopoLite):
         proj = self.context[proj.getId()]
         self.notify(proj)
 
-        self.save()  # instead i think it would be much preferable to invoke a save
+        self.save(request)  # instead i think it would be much preferable to invoke a save
         # ............ on the newly created project directly:
         # ............ >>> IEditForm(proj).save(request.form) 
         # this is hard, though, because things behave strangely here;
@@ -152,19 +151,6 @@ class ProjectAddView(ProjectBaseView, OctopoLite):
         # are actually set on the content...
 
         self.template = None  # Don't render anything before redirect.
-        #site_url = getToolByName(self.context, 'portal_url')()
-        #proj_edit_url = '%s/projects/%s/project-home/edit' % (site_url, id_)
-        #
-        #s_message_mapping = {'title': title, 'proj_edit_url': proj_edit_url,
-        #                     'project_noun': self.project_noun,}
-        #
-        #
-        #s_message = _(u'project_created',
-        #              u'"${title}" has been created. Create a team by searching for other members to invite to your ${project_noun}, then <a href="${proj_edit_url}">edit your ${project_noun} home page</a>.',
-        #              mapping=s_message_mapping)
-        #
-        #self.add_status_message(s_message)
-
         self.redirect('%s/tour' % proj.absolute_url())
 
     def notify(self, project):
