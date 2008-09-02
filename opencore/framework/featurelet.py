d979568caf6d489752f58446644ed1db593b9d80
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-09-02 00:45:45 +0000

    we might need this?
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/viewlet-sandbox@20191 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/framework/featurelet.py
@@ -0,0 +1,6 @@
+from zope.interface import Interface
+from zope.viewlet.interfaces import IViewletManager
+
+class IAdditionalTools(IViewletManager):
+    """ a viewlet manager for additional tools to install """
+
