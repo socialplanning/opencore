ec1876712c9820aa3ae16710f665f1a03876ad70
Paul Winkler <slinkp@gmail.com>
2008-10-07 19:31:55 +0000

    CHANGED create-az.py script to work when run from any directory
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/0.14@20784 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/locales/create-az.py
+++ opencore/locales/create-az.py
@@ -1,6 +1,10 @@
 # creates the az translation from the en po file
 # az translation has all msgstr set to the corresponding msgid
 
+import sys
+import os
+
+
 header = """msgid ""
 msgstr ""
 "Project-Id-Version: opencore\\n"
@@ -18,33 +22,36 @@ msgstr ""
 "Language-Name: Azerbaijani\\n"
 """
 
-# step through the en file starting at the bottom of the header
-# copying line by line yet setting msgstr equal to the preceding
-# msgid
 
-import sys
-import os.path
-if len(sys.argv)==3:
-    file_name = sys.argv[1]
-    file_name2 = sys.argv[2]
-else:
-    file_name = os.path.join('en', 'LC_MESSAGES', 'opencore.po')
-    file_name2 = os.path.join('az', 'LC_MESSAGES', 'opencore.po')
 
-f1 = open(file_name)
-f2 = open(file_name2, 'w')
+def create_az(file_name=None, file_name2=None):
+    # step through the en file starting at the bottom of the header
+    # copying line by line yet setting msgstr equal to the preceding
+    # msgid
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    if file_name == None:
+        file_name = os.path.join(cwd, 'en', 'LC_MESSAGES', 'opencore.po')
+    if file_name2 == None:
+        file_name2 = os.path.join(cwd, 'az', 'LC_MESSAGES', 'opencore.po')
 
-for line in f1:
-    if '#' in line:
-        break
+    f1 = open(file_name, 'r')
+    f2 = open(file_name2, 'w')
 
-f2.write(header)
-f2.write(line)
-for line in f1:
-    if 'msgid' in line:
-        msgid = line.split()[1]
-    if 'msgstr' in line and not line.startswith('#'):
-        f2.write('msgstr ' + msgid)
-    else:
-        f2.write(line)
+    for line in f1:
+        if '#' in line:
+            break
 
+    f2.write(header)
+    f2.write(line)
+    for line in f1:
+        if 'msgid' in line:
+            msgid = line.split()[1]
+        if 'msgstr' in line and not line.startswith('#'):
+            f2.write('msgstr ' + msgid)
+        else:
+            f2.write(line)
+    return file_name2
+
+if __name__ == '__main__':
+    created = create_az(*sys.argv[1:3])
+    print "Created", created, "OK"
