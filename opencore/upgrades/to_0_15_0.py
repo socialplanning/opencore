bc151579669b3106a8cdf093364c21cb80dc912a
Rob Miller <rob@kalistra.com>
2008-11-07 23:23:44 +0000

    add upgrade steps to re-apply the workflow profile step and to update the w/f role mappings
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@21310 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/upgrades/to_0_15_0.py
+++ opencore/upgrades/to_0_15_0.py
@@ -3,9 +3,18 @@ This should be run after upgrading from opencore 0.14.X
 to opencore >= 0.15.0.
 """
 
+from Products.CMFCore.utils import getToolByName
 from opencore.upgrades.utils import logger
 from opencore.upgrades.utils import run_import_step
 
 def reapply_type_information(context):
     result = run_import_step(context, 'typeinfo')
     logger.info(result)
+
+def reapply_workflow_profile(context):
+    result = run_import_step(context, 'workflow')
+    logger.info(result)
+
+def update_workflow_permissions(context):
+    wftool = getToolByName(context, 'portal_workflow')
+    wftool.updateRoleMappings()
