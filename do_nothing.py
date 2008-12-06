449d5cd5b07431d69e5c7f703c74193b73ff7af6
Paul Winkler <slinkp@gmail.com>
2008-12-06 00:52:04 +0000

    REMOVED do_nothing.py: it's not needed since we upgraded to plone 3, and fassembler won't try to run it if it's not there.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@21665 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- do_nothing.py
+++
@@ -1,8 +0,0 @@
-"""This is a hack to get around the fail-on-first-startup problem.
-Run this script once via zopectl run and you should be good to go.
-"""
-
-# XXX Will we still need this after moving to plone 3, or will things
-# finally just work?
-
-print "successfully did nothing"
