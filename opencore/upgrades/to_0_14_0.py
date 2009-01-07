90c64594b496bb5cfc8c4709fe403603cbcce30b
Paul Winkler <slinkp@gmail.com>
2009-01-07 23:22:09 +0000

    FIX previous migration fix: brain.getId is an attribute, not method.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/0.15@22345 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/upgrades/to_0_14_0.py
+++ opencore/upgrades/to_0_14_0.py
@@ -27,7 +27,7 @@ def fix_member_indexes(context, commit_batchsize=200):
         try:
             mem = mship_tool.getMemberById(brain.getId)
         except AssertionError:
-            logger.error('%s Got assertion error trying to find user %r, should not happen' % (msg, brain.getId()))
+            logger.error('%s Got assertion error trying to find user %r, should not happen' % (msg, brain.getId))
             continue
         if mem is None:
             logger.info("%s Got no member for id %r, should not happen" % (msg, brain.getId))
