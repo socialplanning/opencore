309c89878e7149aaa5453c90c12439c49ffb197f
Paul Winkler <slinkp@gmail.com>
2009-01-09 21:06:10 +0000

    MERGED -r22345:22384 (migration fixes to avoid conflict errors) from opencore/branches/0.15/
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@22387 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/upgrades/to_0_15_0.py
+++ opencore/upgrades/to_0_15_0.py
@@ -6,6 +6,7 @@ to opencore >= 0.15.0.
 from Products.CMFCore.utils import getToolByName
 from opencore.upgrades.utils import logger
 from opencore.upgrades.utils import run_import_step
+import transaction
 
 def reapply_type_information(context):
     result = run_import_step(context, 'typeinfo')
@@ -16,8 +17,9 @@ def reapply_workflow_profile(context):
     logger.info(result)
 
 def update_workflow_permissions(context):
-    wftool = getToolByName(context, 'portal_workflow')
-    wftool.updateRoleMappings()
+    portal = getToolByName(context, 'portal_url').getPortalObject()
+    from utils import updateRoleMappings
+    updateRoleMappings(portal, commit_interval=50)
 
 def update_rolemap(context):
     result = run_import_step(context, 'rolemap')
@@ -29,18 +31,33 @@ def retitle_member_areas(context):
     # forward-ported from the people-cloud branch.
     portal = getToolByName(context, 'portal_url').getPortalObject()
     mdtool = getToolByName(portal, 'portal_memberdata')
-    mem_ids = dict.fromkeys(mdtool.objectIds(spec='OpenMember'))
-    pfolder = portal.people
-    for mem_id, home in pfolder.objectItems():
-        if mem_id in mem_ids:
-            member = mdtool._getOb(mem_id)
-            mem_title = member.Title() or mem_id
-            if mem_title != mem_id:
-                home.setTitle(mem_title)
-                home.reindexObject(idxs=['Title'])
-                logger.info('retitled folder for %s' % mem_id)
-            page = home._getOb('%s-home' % mem_id, None)
-            if page is not None and page.Title() == '%s Home' % mem_id:
-                page.setTitle('%s Home' % mem_title)
-                page.reindexObject(idxs=['Title'])
-                logger.info('retitled home page for %s' % mem_id)
+    mem_ids = dict.fromkeys(
+        mdtool.objectIds(spec='OpenMember'))
+    i = 0
+    changed = False
+    for mem_id, home in portal.people.objectItems():
+        if mem_id not in mem_ids:
+            continue
+        i += 1
+        member = mdtool._getOb(mem_id)
+        mem_title = member.Title() or mem_id
+        if home.Title() != mem_title:
+            # This is used in top nav.
+            # It's not directly user-controllable, so we always do the upgrade
+            changed = True
+            home.setTitle(mem_title)
+            home.reindexObject(idxs=['Title'])
+            logger.info('retitled folder for %s' % mem_id)
+        page = home._getOb('%s-home' % mem_id, None)
+        if page is not None and page.Title() == '%s Home' % mem_id and mem_id != mem_title:
+            # Pages are user-renamable so we had to check for a nonstandard title
+            changed = True
+            page.setTitle('%s Home' % mem_title)
+            page.reindexObject(idxs=['Title'])
+            logger.info('retitled home page for %s' % mem_id)
+        if changed and i % 400 == 0:
+            transaction.commit()
+            logger.info('===== COMMITTING (%d of %d) ====' %(i, len(mem_ids)))
+            changed = False
+    transaction.commit()
+    logger.info('==== COMMITTING (%d of %d) ====' %(i, len(mem_ids)))
