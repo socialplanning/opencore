15f167aa2b757abe5ae03f5d57bc7e99e751b04c
Rob Marianski <rob@marianski.com>
2009-01-26 23:06:51 +0000

    add support for a google analytics snippet that only inserts itself for open and closed projects
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/people-cloud@22572 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/browser/analytics.py
+++ opencore/browser/analytics.py
@@ -1,7 +1,11 @@
 from Products.CMFPlone.utils import safe_unicode
 from Products.Five.viewlet.viewlet import ViewletBase
+from opencore.interfaces.workflow import IReadWorkflowPolicySupport
 from opencore.utility.interfaces import IProvideSiteConfig
+from opencore.interfaces import IProject
+from topp.utils.zutils import aq_iface
 from zope.component import getUtility
+
 
 class AnalyticsSnippet(ViewletBase):
     """ include analytics engine JS snippet, if it exists in the
@@ -14,3 +18,20 @@ class AnalyticsSnippet(ViewletBase):
         if snippet:
             snippet = safe_unicode(snippet)
         return snippet
+
+
+class GoogleAnalyticsSnippet(AnalyticsSnippet):
+    """
+    include a google analytics snippet for all pages except in closed
+    projects
+    """
+
+    config_id = 'google_analytics_snippet'
+
+    def render(self):
+        proj = aq_iface(self.context, IProject)
+        if proj is not None:
+            proj_policy = IReadWorkflowPolicySupport(proj).getCurrentPolicyId()
+            if proj_policy not in ('open_policy', 'medium_policy'):
+                return u''
+        return super(GoogleAnalyticsSnippet, self).render()
