fae689e16fcec5bd8ee7b935fa7f36f3b078147b
Rob Marianski <rob@marianski.com>
2009-03-26 23:04:49 +0000

    add portal status message after featuring a project
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/people-cloud@23466 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feature/browser.py
+++ opencore/feature/browser.py
@@ -1,17 +1,21 @@
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from opencore.browser.base import BaseView
 from opencore.feature import feature_project
 from opencore.feature import get_featured_project_metadata
 
-class FeatureProjectView(BrowserView):
+class FeatureProjectView(BaseView):
     """handle posting to feature a new project"""
 
     def __call__(self):
+        portal_url = getToolByName(self.context, 'portal_url')()
         if self.request.environ['REQUEST_METHOD'] == 'POST':
             project_id = self.request.form.get('project_id', None)
             if project_id is not None:
                 feature_project(project_id)
-        portal_url = getToolByName(self.context, 'portal_url')()
+                self.add_status_message(u'Project <a href="%s">%s</a> featured' %
+                                        (portal_url + '/projects/' + project_id,
+                                         project_id))
         return self.request.response.redirect(portal_url + '/projects')
 
 class LatestFeaturedProjectView(BrowserView):
