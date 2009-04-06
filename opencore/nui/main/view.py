a9c3ebee64efb2ee1e443f4166fb80e6d04caa98
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2009-04-06 02:40:37 +0000

    rename WithProjectNoun class and merge nui main_macros with search-widget.pt per http://www.openplans.org/projects/opencore/lists/opencore-dev/archive/2009/03/1236123389092 and http://www.openplans.org/projects/opencore/lists/opencore-dev/archive/2009/03/1236128978405
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@23525 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/nui/main/view.py
+++ opencore/nui/main/view.py
@@ -2,7 +2,7 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.viewlet.viewlet import ViewletBase
 from opencore.project.utils import project_noun
 
-class WithProjectNoun(object):
+class SearchWidget(object):
     @property
     def project_noun(self):
         return project_noun()
