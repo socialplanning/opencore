f44c9ecf570dd7995ee8597ccc9a65ba7663c01d
Rob Marianski <rob@marianski.com>
2009-04-07 21:23:49 +0000

    resolve unit test failure by adding setup function to setSite on the portal
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/people-cloud@23542 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feature/tests.py
+++ opencore/feature/tests.py
@@ -7,11 +7,15 @@ import unittest
 optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
 
 def test_suite():
+    def setup(tc):
+        from zope.app.component.hooks import setSite
+        setSite(tc.portal)
     readme = dtf.ZopeDocFileSuite("README.txt",
                                   optionflags=optionflags,
                                   package='opencore.feature',
                                   test_class=OpenPlansTestCase,
                                   globs=locals(),
                                   layer=test_layer
+                                  setUp=setup,
                                   )
     return unittest.TestSuite((readme,))
