66d770cf7a5ed21b6c7fae739fbcc6f704c97579
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2009-05-05 18:33:55 +0000

    update oc-feed dependency, use 0.4 oc-feed tag
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/0.17@23940 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -47,14 +47,14 @@ setup(
                         'http://feedparser.googlecode.com/files/feedparser-4.1.zip',
                         'https://svn.openplans.org/svn/topp.utils/trunk#egg=topp.utils-dev',
                         'https://svn.openplans.org/svn/ZCMLLoader/trunk#egg=ZCMLLoader',
-                        'https://svn.openplans.org/svn/opencore/plugins/oc-feed/trunk/#egg=oc_feed-dev',
+                        'https://svn.openplans.org/svn/opencore/plugins/oc-feed/tags/0.4#egg=oc_feed-0.4',
                         'https://svn.plone.org/svn/collective/borg/components/borg.localrole/trunk#egg=borg.localrole-dev',
                         ],
 
       install_requires=[
           # -*- Extra requirements: -*-
           "oc-js==dev,>=0.5",
-          'oc-feed>=0.2,==dev',
+          'oc-feed>=0.4',
           'simplejson',
           'decorator',
           'feedparser',
