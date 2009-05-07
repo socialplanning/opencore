ac4bb8365bef0ab29b8c62244218ffa651a931a0
Rob Marianski <rob@marianski.com>
2009-05-07 15:57:58 +0000

    do not peg oc-js to a version, but list as a dependency
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/people-cloud@23996 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -30,13 +30,12 @@ setup(
       zip_safe=False,
       dependency_links=["http://svn.openplans.org/eggs/",
                         'http://download.savannah.nongnu.org/releases/pyprof/hprof-0.1.1.tar.gz#egg=hprof',
-                        'https://svn.openplans.org/svn/oc-js/tags/0.6.3#egg=oc-js-0.6.3',
                         'http://feedparser.googlecode.com/files/feedparser-4.1.zip',
                         ],
 
       install_requires=[
           # -*- Extra requirements: -*-
-          "oc-js==0.6.3",
+          "oc-js",
           'oc-feed==0.1',
           "ClockQueue==0.1.1",
           'simplejson==1.7.3',
