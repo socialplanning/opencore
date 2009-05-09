88957181239ae14b585680a20cbf405c9a249966
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2009-05-09 14:53:03 +0000

    merge bookkeeping changes from 0.17rb
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@24038 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -24,60 +24,58 @@ classifiers=[
 
 setup(
     name=name,
-      version=version,
-      description="Software that drives http://openplans.org",
-      long_description=readme,
-      classifiers=classifiers,
-      keywords='openplans openplans.org topp',
-      author='The Open Planning Project',
-      author_email='opencore-dev@lists.openplans.org',
-      url='http://www.openplans.org/projects/opencore',
-      license='GPLv3',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      package_data={
+    version=version,
+    description="Software that drives http://openplans.org",
+    long_description=readme,
+    classifiers=classifiers,
+    keywords='openplans openplans.org topp',
+    author='The Open Planning Project',
+    author_email='opencore-dev@lists.openplans.org',
+    url='http://www.openplans.org/projects/opencore',
+    license='GPLv3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    package_data={
         '': ['copy/*', 'ftests/*', '*py', '*zcml', '*txt'],
-      },
-      zip_safe=False,
-      dependency_links=['https://svn.openplans.org/svn/topp.featurelets/trunk#egg=topp.featurelets-0.3.0',
-                        'https://svn.openplans.org/svn/OpencoreRedirect/trunk#egg=OpencoreRedirect-dev',
-                        'http://svn.red-bean.com/bob/simplejson/trunk/#egg=simplejson-dev',
-                        'http://download.savannah.nongnu.org/releases/pyprof/hprof-0.1.1.tar.gz#egg=hprof',
-                        'https://svn.openplans.org/svn/oc-js/tags/0.6.4#egg=oc-js-0.6.4',
-                        'http://feedparser.googlecode.com/files/feedparser-4.1.zip',
-                        'https://svn.openplans.org/svn/topp.utils/trunk#egg=topp.utils-dev',
-                        'https://svn.openplans.org/svn/ZCMLLoader/trunk#egg=ZCMLLoader',
-                        'https://svn.openplans.org/svn/opencore/plugins/oc-feed/trunk/#egg=oc_feed-dev',
-                        'https://svn.plone.org/svn/collective/borg/components/borg.localrole/trunk#egg=borg.localrole-dev',
-                        'https://svn.openplans.org/svn/flunc/trunk#egg=flunc-0.5dev',
-                        ],
-
-      install_requires=[
-          # -*- Extra requirements: -*-
-          "oc-js==0.6.4",
-          'oc-feed>=0.2,==dev',
-          'simplejson',
-          'decorator',
-          'feedparser',
-          'topp.featurelets>=0.3.0',
-          'topp.utils>=0.3.1dev,==dev',
-          'OpencoreRedirect==dev,>=0.5dev',
-          'httplib2',
-          'lxml>=2.0alpha5',
-          'plone.mail',
-          'borg.localrole==2.0.1',
-          'uuid',
-          'flunc>=0.5dev',
-          'zcmlloader',
-          'Products.CacheSetup==1.2',
-          'Products.GenericSetup==1.4.1',
-          'zc.queue',
-          ],
-      extras_require=dict(ubuntu=['hprof']),
-
-      # the opencore.versions are the names of the packages
-      # these are what show up in the openplans-versions view
-      entry_points="""
+        },
+    zip_safe=False,
+    dependency_links=[
+        'https://svn.openplans.org/svn/OpencoreRedirect/trunk#egg=OpencoreRedirect-dev',
+        'http://svn.red-bean.com/bob/simplejson/trunk/#egg=simplejson-dev',
+        'http://download.savannah.nongnu.org/releases/pyprof/hprof-0.1.1.tar.gz#egg=hprof',
+        'https://svn.openplans.org/svn/oc-js/tags/0.6.4#egg=oc-js-0.6.4',
+        'http://feedparser.googlecode.com/files/feedparser-4.1.zip',
+        'https://svn.openplans.org/svn/ZCMLLoader/trunk#egg=ZCMLLoader',
+        'https://svn.openplans.org/svn/opencore/plugins/oc-feed/tags/0.4#egg=oc_feed-0.4',
+        'https://svn.plone.org/svn/collective/borg/components/borg.localrole/trunk#egg=borg.localrole-dev',
+        ],
+    install_requires=[
+        # -*- Extra requirements: -*-
+        "oc-js==0.6.4",
+        'oc-feed>=0.4',
+        'simplejson',
+        'decorator',
+        'feedparser',
+        'topp.featurelets>=0.3.0',
+        'topp.utils>=0.5',
+        'OpencoreRedirect==dev,>=0.5dev',
+        'httplib2',
+        'lxml>=2.0alpha5',
+        'plone.mail',
+        'borg.localrole==2.0.1',
+        'uuid',
+        'flunc>=0.5',
+        'zcmlloader',
+        'Products.CacheSetup==1.2',
+        'Products.GenericSetup==1.4.1',
+        'zc.queue',
+        ],
+    
+    extras_require=dict(ubuntu=['hprof']),
+    
+    # the opencore.versions are the names of the packages
+    # these are what show up in the openplans-versions view
+    entry_points="""
       [distutils.commands]
       zinstall = topp.utils.setup_command:zinstall
       [opencore.versions]
