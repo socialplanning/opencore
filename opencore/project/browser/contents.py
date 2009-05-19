3c06d20c2e034db9f2788fc7b8bb621510e1acb1
Paul Winkler <slinkp@gmail.com>
2009-05-19 16:44:03 +0000

    DELETE crufty RecentUpdatesView.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@24134 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/contents.py
+++ opencore/project/browser/contents.py
@@ -356,14 +356,3 @@ class ContentsCollection(list):
 
     def extend(self, items):
         list.extend(self, items)
-
-
-class RecentUpdatesView(ProjectBaseView):
-    """
-    things we need:
-    first:
-    recent wikipage updates (just query catalog sorted on last_modified? check rollie's wireframes to see if multiple updates per page show up, in which case this is harder)
-    listen threads with recent messages (same as above)
-    then: recent blog posts (pull wordpress rss feed -- use some sort of utility/tool [what is the difference?] in opencore.wordpress?)
-    then: tasktracker .. maybe.
-    """
