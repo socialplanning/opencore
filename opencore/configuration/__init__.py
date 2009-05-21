8241bdd0e5513dd54432a303f58a4b4418d34776
Paul Winkler <slinkp@gmail.com>
2009-05-21 19:17:59 +0000

    REMOVE hacky broken COOKIE_DOMAIN thing from previous commit.   A) it wasn't used, and B) python doesn't really support dynamic module-scope attributes.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@24177 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/configuration/__init__.py
+++ opencore/configuration/__init__.py
@@ -1,4 +1,3 @@
-from opencore.configuration.utils import product_config
 from pkg_resources import Requirement
 from zope.component import queryUtility
 
@@ -8,24 +7,6 @@ OC_REQ = Requirement.parse('opencore')
 DEFAULT_ROLES          = ['ProjectMember', 'ProjectAdmin']
 DEFAULT_ACTIVE_MSHIP_STATES=['public', 'private']
 PROJECTNAME            = "OpenPlans"
-
-class __useless(object):
-    # this is here just so we can create a property
-
-    _cookie_domain = None
-
-    @classmethod
-    def cookie_domain(kls):
-        # Bootstrap this on first usage to avoid using the component
-        # architecture during package load.
-        # XXX Is this actually used anywhere??
-        if kls._cookie_domain is None:
-            from opencore.utility.interfaces import IProvideSiteConfig
-            config = queryUtility(IProvideSiteConfig)
-            kls._cookie_domain = config.get('cookie_domain')
-        return kls._cookie_domain
-
-COOKIE_DOMAIN = __useless.cookie_domain
 
 PROHIBITED_MEMBER_PREFIXES = ['openplans', 'topp', 'anon', 'admin',
                               'manager', 'webmaster', 'help', 'support']
