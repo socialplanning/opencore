cc06b6ce541f9107de71c8dd73a0ffc24983c50a
Paul Winkler <slinkp@gmail.com>
2009-05-22 14:45:49 +0000

    REMOVE apparently useless test monkeypatch w/ hardcoded domain; refs openplans:#2826
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@24188 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/feed/tests.py
+++ opencore/project/feed/tests.py
@@ -1,15 +1,7 @@
-from opencore.feed.base import BaseFeedAdapter
 from opencore.testing import dtfactory as dtf
 from opencore.testing.layer import SiteSetupLayer
 import doctest
 import unittest
-
-# XXX Monkeypatching should be scoped to just the test it's needed for,
-# and restored on teardown; but for some reason I can't make that work.
-# XXX HARDCODED DOMAIN
-BaseFeedAdapter.memberURL = lambda x, y: 'http://www.openplans.org'
-BaseFeedAdapter.member_portraitURL = lambda x, y: 'http://www.openplans.org/++resource++img/topp_logo.jpg'
-
 
     
 def test_suite():
