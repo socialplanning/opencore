3c3a461763a073a7fd7fd40c88acdea9d2f722f3
Paul Winkler <slinkp@gmail.com>
2009-05-22 15:04:50 +0000

    REMOVE hardcoded domain from tests; refs openplans:#2826
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@24191 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/listen/test_manage_event.py
+++ opencore/listen/test_manage_event.py
@@ -12,6 +12,9 @@ class TestMailingListManageEvent(OpenPlansTestCase):
 
     def afterSetUp(self):
         """create a mailing list that we can send events to"""
+        oc_props = self.portal.portal_properties.opencore_properties
+        self._old_fqdn = oc_props.getProperty('mailing_list_fqdn')
+        oc_props.manage_changeProperties(mailing_list_fqdn='lists.example.org')
         ml = _createObjectByType('Open Mailing List', self.portal, 'mylist')
         ml.setTitle('My List')
         ml.mailto = 'mylist'
@@ -19,8 +22,12 @@ class TestMailingListManageEvent(OpenPlansTestCase):
         ll.registerList(ml)
         self.mailhost = getUtility(IMailHost)
 
+
     def beforeTearDown(self):
         self.mailhost.messages = []
+        oc_props = self.portal.portal_properties.opencore_properties
+        oc_props.manage_changeProperties(mailing_list_fqdn=self._old_fqdn)
+
 
     def _getView(self):
         return getMultiAdapter((self.portal, self.portal.REQUEST),
@@ -30,7 +37,7 @@ class TestMailingListManageEvent(OpenPlansTestCase):
         view = self._getView()
         # XXX HARDCODED DOMAIN
         headers = {'from': 'johnnywalker@example.com',
-                   'to': 'mylist@lists.openplans.org',
+                   'to': 'mylist@lists.example.org',
                    }
         status_codes = [200]
         view(status_codes, headers)
@@ -38,7 +45,7 @@ class TestMailingListManageEvent(OpenPlansTestCase):
         self.assertEqual(1, len(msgs), 'No error message sent')
         msg = msgs[0]
         # XXX HARDCODED DOMAIN
-        self.assertEqual('mylist-manager@lists.openplans.org', msg['mfrom'])
+        self.assertEqual('mylist-manager@lists.example.org', msg['mfrom'])
         self.assertEqual(['johnnywalker@example.com'], msg['mto'])
         self.assertEqual('Mailing list error: message too big',  msg['subject'])
         self.failUnless("message that you're trying to send is too big" in msg['msg'])
@@ -47,7 +54,7 @@ class TestMailingListManageEvent(OpenPlansTestCase):
         view = self._getView()
         # XXX HARDCODED DOMAIN
         headers = {'from': 'mister.dewars@example.com',
-                   'to': 'mylist@lists.openplans.org',
+                   'to': 'mylist@lists.example.org',
                    }
         # any unknown status code
         status_codes = [777]
@@ -56,7 +63,7 @@ class TestMailingListManageEvent(OpenPlansTestCase):
         self.assertEqual(1, len(msgs), 'No error message sent')
         msg = msgs[0]
         # XXX HARDCODED DOMAIN
-        self.assertEqual('mylist-manager@lists.openplans.org', msg['mfrom'])
+        self.assertEqual('mylist-manager@lists.example.org', msg['mfrom'])
         self.assertEqual(['mister.dewars@example.com'], msg['mto'])
         self.assertEqual('Mailing list error', msg['subject'])
         self.failUnless('unknown error' in msg['msg'])
