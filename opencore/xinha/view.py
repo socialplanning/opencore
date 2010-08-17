0b6e74e88bcea429704c0f085ff4b7e7710b93ab
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-08-17 17:34:11 +0000

    Start to sanify xinha config view to prepare for merge to trunk:  * parallel versions of xinha will only exist in 'optinxinhaupgrade' plugin package
     * rename xinha-092.pt to xinha.pt
     * remove xinha-096.pt (now in plugin package)
     * don't register resourceDirectory for xinha upgrade (now in plugin package)
    
    
    
    git-svn-id: https://svn.socialplanning.org/svn/opencore/branches/xinha-parallel-upgrade@27395 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/xinha/view.py
+++ opencore/xinha/view.py
@@ -1,31 +1,7 @@
 from opencore.browser.base import BaseView
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from opencore.xinha.i18n import xinha_lang_code
 
 class XinhaConfig(BaseView):
-    template92 = ZopeTwoPageTemplateFile("xinha-092.pt")
-    template96 = ZopeTwoPageTemplateFile("xinha-096.pt")
 
     def xinha_lang_code(self):
-        xinha_lang_dir = None
-        if self.use_new():
-            xinha_lang_dir = "thirdparty/xinha96r1270/lang"
-        return xinha_lang_code(self.request, 
-                               xinha_lang_dir)
-
-    def __call__(self):
-        if self.use_new():
-            return self.template96()
-        return self.template92()
-
-    def use_new(self):
-        member = self.loggedinmember
-        if member is None:
-            return False
-
-        opt_ins = get_config('xinha_upgrade_optins', '').split()
-        opt_ins = [x.strip() for x in opt_ins if x.strip()]
-
-        if member.getId() in opt_ins:
-            return True
-        return False
+        return xinha_lang_code(self.request)
