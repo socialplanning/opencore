364eabe34fe9558ba45144258d4f6f839c78c5d5
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-11-25 20:59:42 +0000

    refs #2928: start moving to newest version of xinha.  first, i am pulling in the new configuration.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@27781 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/xinha/view.py
+++ opencore/xinha/view.py
@@ -5,3 +5,26 @@ class XinhaConfig(BaseView):
 
     def xinha_lang_code(self):
         return xinha_lang_code(self.request)
+
+    def in_wordpress(self):
+        return self.request["HTTP_X_OPENPLANS_APPLICATION"] == "wordpress"
+
+    def fake_context(self):
+        """
+        If we're in wordpress, then, as far as xinha's concerned,
+        the context is project-home -- this is used for figuring
+        out where to hang attachments
+        """
+        if not self.in_wordpress():
+            return self.context
+        project = self.request["HTTP_X_OPENPLANS_PROJECT"]
+        project = getattr(self.portal.projects, project)
+        return project["project-home"]
+
+    def backend_url(self):
+        url = self.fake_context().absolute_url() 
+        return url + "/internal-link"
+
+    def image_backend_url(self):
+        url = self.fake_context().absolute_url()
+        return url + "/backend?"
