ad172206d90cebdda97f3b6838d0e5de476327b8
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-12-26 18:18:05 +0000

    allow custom smtp port
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@27904 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/scripts/configure_mailhost.py
+++ opencore/scripts/configure_mailhost.py
@@ -5,7 +5,15 @@ app = setup(app)
 
 mh = app.openplans.MailHost
 
+import sys
+import pdb
+try:
+    port = int(sys.argv[2])
+except:
+    port = 25
+
 if not mh.smtp_host:
     mh.smtp_host = "localhost"
-    mh.smtp_port = 25
-    transaction.commit()
+    
+mh.smtp_port = port
+transaction.commit()
