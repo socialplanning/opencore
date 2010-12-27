5b05e3dd043ad72d0b22d411b8970f171823909f
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-12-27 16:29:13 +0000

    fix configure_mailhost script, and make it verbose
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@27909 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/scripts/configure_mailhost.py
+++ opencore/scripts/configure_mailhost.py
@@ -7,13 +7,16 @@ mh = app.openplans.MailHost
 
 import sys
 import pdb
+
 try:
-    port = int(sys.argv[2])
-except:
+    port = int(sys.argv[-1])
+except TypeError:
     port = 25
 
 if not mh.smtp_host:
     mh.smtp_host = "localhost"
+
+print "Setting MailHost to use port %s" % port
     
 mh.smtp_port = port
 transaction.commit()
