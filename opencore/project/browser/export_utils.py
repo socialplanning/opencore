bd69f604340a360d26bdbc05235ed0dd6462a6e6
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-01-05 18:02:07 +0000

    i forgot to add page title
    git-svn-id: https://svn.socialplanning.org/svn/opencore/trunk@27999 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/project/browser/export_utils.py
+++ opencore/project/browser/export_utils.py
@@ -305,8 +305,10 @@ class ContentExporter(object):
             # add the page title in a <h1> like in the wiki;
             # and add all the essential class markup
             # for the CSS rules.
-            text = ('<body class="oc-wiki">\n<h1>%s</h1>\n<div class="oc-wiki-content">\n' 
+            title = page.Title().decode("utf-8")
+            text = ('<body class="oc-wiki">\n<h1>%s</h1>\n<div class="oc-wiki-content">\n'
                     + text + "\n</div>\n</body>")
+            text = text % title
             # We want to rewrite links to other wiki pages,
             # so that the wiki will form a valid web after 
             # we export it and save the HTML files with 
