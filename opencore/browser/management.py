7a1ca2e92109a161df4887b2b0b883ea8e3e5a84
Ethan Jucovy <ethan.jucovy@gmail.com>
2013-06-03 00:30:49 +0000

    import users script - wip

--- opencore/browser/management.py
+++ opencore/browser/management.py
@@ -7,6 +7,48 @@ from cStringIO import StringIO
 from interfaces import IAddOpenPlans
 from zope.formlib import form
 
+from opencore.browser.base import BaseView
+from opencore.member.workflow import MemberWorkflowHandler
+
+from DateTime import DateTime
+
+class ImportUsers(BaseView):
+
+    def __call__(self, *args, **kwargs):
+        import csv, sys, os
+        fp = open(self.request.form.get("file"))
+        data = csv.reader(fp)
+        joinview = self.context.restrictedTraverse("@@join")
+        membertool = getToolByName(self.context, "portal_membership")
+
+        header = data.next()
+        for member in data:
+            member = dict(zip(header, member))
+            if "jucovy" not in member['email'] and member['member_id'] != "ejucovy":
+                continue
+
+            self.request.form.clear()
+            self.request.form.update({"id": member['member_id'],
+                                      "email": member['email'],
+                                      "password": "changeme",
+                                      "fullname": member['fullname'],
+                                      "confirm_password": "changeme",
+                                      })
+            mem_obj = joinview._create_member(confirmed=True) # This doesn't confirm the user; it just suppresses email notification.
+            if isinstance(mem_obj, dict):
+                print mem_obj
+                continue
+            if member['is_confirmed'] == "confirmed":
+                MemberWorkflowHandler(mem_obj).confirm()
+                membertool.createMemberArea(member['member_id'])
+
+            mem_obj.getField("password").set(mem_obj, member['password'])
+            
+            mem_obj.getField("creation_date").set(mem_obj, 
+                                                  DateTime(member['creation_date']))
+            for field in "location home_page description statement skills affiliations website background favorites".split():
+                mem_obj.getField(field).set(mem_obj, member[field])
+        return "ok"
 
 class TestContentCreator(formbase.PageForm):
     label = 'Create Dummy Projects and Members'
